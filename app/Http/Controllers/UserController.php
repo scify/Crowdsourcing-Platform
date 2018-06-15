58a512871ceb52d582588a87c7f60ce1e655b91e
Paul Isaris <paulisaris@gmail.com>
2018-06-15 12:51:55 +0300

    Edit user profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,24 +20,20 @@ class UserController extends Controller
     public function myProfile()
     {
         $userViewModel = $this->userManager->getMyProfileData(Auth::user());
-        return view('home.my-profile', ['userViewModel' => $userViewModel]);
+        return view('home.my-profile', ['viewModel' => $userViewModel]);
     }
 
     public function patch(Request $request) {
         $this->validate($request, [
-            'password' => 'required|string|min:6|confirmed',
-            'current_password' => 'required|string|min:6'
+            'password' => 'nullable|string|min:6|confirmed',
+            'current_password' => 'required_with:password|string|min:6'
         ]);
         $data = $request->all();
-        $current_password = Auth::User()->password;
-        if(Hash::check($data['current_password'], $current_password)) {
-            $user_id = Auth::User()->id;
-            $obj_user = User::find($user_id);
-            $obj_user->password = Hash::make($data['password']);
-            $obj_user->save();
+        try {
+            $this->userManager->updateUser($data);
             session()->flash('flash_message_success', 'Profile updated.');
-        } else {
-            session()->flash('flash_message_failure', 'Please enter correct current password.');
+        } catch (\Exception $e) {
+            session()->flash('flash_message_failure', 'Error: ' . $e->getMessage());
         }
         return back();
     }
