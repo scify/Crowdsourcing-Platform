fe94bbc01cac8bad846912098438f4d27457dbaa
Paul Isaris <paulisaris@gmail.com>
2018-06-15 12:51:55 +0300

    Edit user profile

--- app/Models/ViewModels/UserProfile.php
+++ app/Models/ViewModels/UserProfile.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: snik
- * Date: 4/23/18
- * Time: 1:19 PM
- */
 
 namespace App\Models\ViewModels;
 
@@ -13,12 +7,10 @@ use App\Models\User;
 
 class UserProfile
 {
-    public $fullName;
-    public $email;
+    public $user;
 
     public function __construct(User $user)
     {
-        $this->fullName = $user->name . " " . $user->surname;
-        $this->email = $user->email;
+        $this->user = $user;
     }
 }
