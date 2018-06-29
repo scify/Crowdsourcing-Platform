2a39587ce0c4972b2ab2d59107e75d3035b60e28
Paul Isaris <paulisaris@gmail.com>
2018-06-29 14:42:39 +0300

    Added placeholders for Twitter client key and secret, added correct twitter URL in form

--- resources/views/auth/social-sign-in.blade.php
+++ resources/views/auth/social-sign-in.blade.php
@@ -7,7 +7,7 @@
             <a class="socialSignIn" href="{{url('login/social/facebook')}}"><i class="fab fa-facebook-square"></i></a>
         </div>
         <div class="col-sm-6 align-left">
-            <a class="socialSignIn" href="{{url('login/social/facebook')}}"><i class="fab fa-twitter-square"></i></a>
+            <a class="socialSignIn" href="{{url('login/social/twitter')}}"><i class="fab fa-twitter-square"></i></a>
         </div>
     </div>
 </div>
