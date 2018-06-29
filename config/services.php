441f0f9dec6652ba3d9e342d009672d7dfa38536
Paul Isaris <paulisaris@gmail.com>
2018-06-29 12:44:06 +0300

    -Added provider class to change relatve urls to absolute

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => url('login/social/facebook/callback'),
+        'redirect' => 'login/social/facebook/callback',
     ],
 
 ];
