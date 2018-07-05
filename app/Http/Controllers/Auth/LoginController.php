4568f7e82490fad6c16db007d02ab910c54290b1
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2018-07-05 14:45:20 +0300

    after login redirect to my-profile

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/my-profile';
 
     private $userManager;
 
