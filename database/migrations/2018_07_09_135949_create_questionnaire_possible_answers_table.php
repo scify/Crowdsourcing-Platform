30cec856ddb4e5da665094accba283ee57b662b7
Spyros Nikolakis <nikolakispyros@gmail.com>
2018-07-13 17:15:38 +0300

    added value column in questionnaire_possible_answers table

--- database/migrations/2018_07_09_135949_create_questionnaire_possible_answers_table.php
+++ database/migrations/2018_07_09_135949_create_questionnaire_possible_answers_table.php
@@ -16,6 +16,7 @@ class CreateQuestionnairePossibleAnswersTable extends Migration
         Schema::create('questionnaire_possible_answers', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('question_id');
+            $table->string('value', 500)->nullable();
             $table->string('answer', 500);
             $table->foreign('question_id')->references('id')->on('questionnaire_questions');
             $table->timestamps();
