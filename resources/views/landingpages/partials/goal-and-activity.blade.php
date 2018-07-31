0e325272e4d93e4430bb5b85588cac9d9c1755f5
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2018-07-31 18:01:25 +0300

    change labels

--- resources/views/landingpages/partials/goal-and-activity.blade.php
+++ resources/views/landingpages/partials/goal-and-activity.blade.php
@@ -2,8 +2,14 @@
 
     <div class="goal-title">
         {{-- todo:  If 0 responses , or if target achieved display different messages --}}
-        <h2 class="info"><span class="number">{{$viewModel->totalResponses}}</span> people spoke up so far. Let's get
-            to {{$viewModel->questionnaire->goal}}!
+        <h2 class="info"><span class="number">{{$viewModel->totalResponses}}</span> people spoke up so far.
+            @if ($viewModel->totalResponses==0)
+                Be the first!
+            @else
+                Let's get
+                to {{$viewModel->questionnaire->goal}}!
+            @endif
+
         </h2>
     </div>
 
