2a9fb6b69e41663ab278b3312efbc2756d7bead3
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2018-07-31 18:42:18 +0300

    impove my dashboard

--- resources/views/my-profile.blade.php
+++ resources/views/my-profile.blade.php
@@ -19,8 +19,7 @@
                 <div class="box-body">
                     <div class="row">
                         <div class="col-md-12">
-                            Under construction: Call to action to answer the question should be displayed here and any
-                            welcome material
+                           [todo:put some introductory text here]
                         </div>
                     </div>
                 </div>
@@ -36,26 +35,53 @@
                 </div>
                 <div class="box-body">
                     <div class="row">
-
                         <div class="col-md-12">
-                            You haven't responsed so far [todo: display a list of pending projects here]
+                            <table style="margin-top:5px; width:100%;">
+                                <tr>
+                                    <th>Project</th>
+                                    <th>Status</th>
+                                    <th >You can help by:</th>
+                                </tr>
+                                <tr>
+                                    <td style="padding-top:15px"><a href="/fair-eu"> <img height="30" alt="FAIR EU" src="https://dev.ecas/images/default-project/fair-eu.png"></a></td>
+                                    <td style="padding-top:15px;">In progress </td>
+                                    <td style="padding-top:15px;margin-right:100px;"><a href="/fair-eu?open=1">Responding to a questionnaire</a></td>
+                                </tr>
+                            </table>
+
                         </div>
 
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-6 col-xs-12">
-            <div class="box box-success">
+        <div id="awards" class="col-md-6 col-xs-12">
+            <div class="box box-danger">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Gamification Elements</h3>
+                    <h3 class="box-title">You don't have any awards assigned. That's a pity!</h3>
                 </div>
                 <div class="box-body">
                     <div class="row">
 
-                        <div class="col-md-12">
-                            A summary of gamification tools can be displayed here. Badges and action required for next
-                            level
+                        <div class="col-md-12 text-center">
+
+                            <br>
+
+                            <a href="/fair-eu?open=1">Respond to FAIR-EU questionnaire <br>  and gain the Influencer award
+                                <br>
+                                <br>
+                                <img  src="{{asset("images/badges/award.png")}}">
+                            </a>
+
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+
+
                         </div>
 
                     </div>
