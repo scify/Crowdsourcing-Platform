5e6471ebd5a40d6c0d882f3827bfe672aa125855
Spyros Nikolakis <nikolakispyros@gmail.com>
2018-07-31 12:49:24 +0300

    ECP-81 integrated with google translate to autogenerate questionnaire translations

---
+++ app/Utils/Translator.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: snik
+ * Date: 6/6/18
+ * Time: 4:52 PM
+ */
+
+namespace App\Utils;
+
+use Google\Cloud\Translate\TranslateClient;
+
+class Translator
+{
+    /**
+     * Translate texts to the preferred language.
+     * @param array $texts   The texts that need translation
+     * @param string $to     The language in which the texts will be translated
+     * @return array         The translated texts
+     */
+    public static function translateTexts(array $texts, $to)
+    {
+        $translate = new TranslateClient(['key' => env('GOOGLE_TRANSLATE_KEY')]);
+        $result = $translate->translateBatch($texts, [
+            'target' => $to,
+        ]);
+        return $result;
+    }
+}
