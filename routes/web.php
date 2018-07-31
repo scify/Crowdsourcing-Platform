6b2b3345e9c0389c81f8f1582b8a1637c6597019
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2018-07-31 18:13:18 +0300

    remove contribute page

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ Route::group([ 'middleware' => 'auth' ], function () {
     Route::get('/', 'UserController@home');
     Route::get('/my-profile', 'UserController@myProfile')->name('profile');
     Route::get('/my-account', 'UserController@myAccount')->name('my-account');
-    Route::get('/contribute', 'UserController@contribute')->name('contribute');
     Route::get("/admin/manage-users", "AdminController@manageUsers")->middleware("can:manage-users");
     Route::get("/admin/edit-user/{id}", "AdminController@EditUserForm")->middleware("can:manage-users");
     Route::post("/admin/add-user", "AdminController@addUserToPlatform")->middleware("can:manage-users");
