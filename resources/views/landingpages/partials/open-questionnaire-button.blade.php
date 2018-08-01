0d22ba0a7762f909938dac6a8cf81f0e23988b85
Spyros Nikolakis <nikolakispyros@gmail.com>
2018-08-01 11:36:19 +0300

    speak up button, now does not open the questionnaire modal if it is already responded

--- resources/views/landingpages/partials/open-questionnaire-button.blade.php
+++ resources/views/landingpages/partials/open-questionnaire-button.blade.php
@@ -1,13 +1,18 @@
 @if($viewModel->questionnaire)
     @if(\Auth::user())
-        <a href="javascript:void(0)"
-           class="btn btn-primary respond-questionnaire"
-           data-open-on-load ={{$viewModel->openQuestionnaireWhenPageLoads?"1":"0"}}
-           data-questionnaire-id="{{$viewModel->questionnaire->id}}"
-           data-url="{{route('respond-questionnaire')}}"
-           data-toggle="modal" data-target="#questionnaire-modal">
-            Speak up {{--<i class="fa fa-angle-right"></i>--}}
-        </a>
+        @if(!$viewModel->userResponse)
+            <a href="javascript:void(0)"
+               class="btn btn-primary respond-questionnaire"
+               data-open-on-load={{$viewModel->openQuestionnaireWhenPageLoads?"1":"0"}}
+                       data-questionnaire-id="{{$viewModel->questionnaire->id}}"
+               data-url="{{route('respond-questionnaire')}}"
+               data-toggle="modal" data-target="#questionnaire-modal">
+                Speak up {{--<i class="fa fa-angle-right"></i>--}}
+            </a>
+        @else
+            <a href="#questionnaire"
+               class="btn btn-primary respond-questionnaire">Speak up</a>
+        @endif
     @else
         <a href="/login?submitQuestionnaire=1&redirectTo={{urlencode($viewModel->project->slug."?open=1")}}"
            class="btn btn-primary respond-questionnaire">
