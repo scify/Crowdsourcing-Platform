10f506c5b02e564b0346c861b2ad8a5d0ec778bc
Spyros Nikolakis <nikolakispyros@gmail.com>
2018-08-03 12:21:16 +0300

    ECP-96 created the generic ecas crowdsourcing platform landing page

--- resources/views/crowdsourcing-landingpage/partials/projects.blade.php
+++ resources/views/crowdsourcing-landingpage/partials/projects.blade.php
@@ -3,12 +3,11 @@
         <h2>Projects</h2>
         <div class="content-container">
             <p>In the present platform, a lot of different projects are hosted. Below, there is a list of all the
-                currently active ones. You may click on any item of the list to visit the project's page and
-                contribute.</p>
+                currently active ones. Please visit a project's page and make an impact, by just answering a couple of questions!</p>
             <div class="projects-container row">
                 @foreach($projects as $project)
-                    <div class="col-md-4">
-                        <div class="project-wrapper" title="{{$project->name}}">
+                    <div class="col-md-4 col-sm-6 col-xs-10 col-xs-offset-1 col-sm-offset-0 col-md-offset-0">
+                        <div class="project-wrapper">
                             <div class="project-logo">
                                 <img src="{{$project->logo_path}}" alt="">
                             </div>
@@ -16,7 +15,7 @@
                                 {!! $project->about !!}
                             </div>
                             <div class="project-visit-btn">
-                                <a href="/{{$project->slug}}" class="btn btn-block btn-primary">Visit project's page</a>
+                                <a href="/{{$project->slug}}" class="btn btn-block btn-primary">Contribute</a>
                             </div>
                         </div>
                     </div>
