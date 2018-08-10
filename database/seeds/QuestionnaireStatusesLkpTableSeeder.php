245e9b07b5d0161d87f4fa277088ec1c28a33312
Spyros Nikolakis <nikolakispyros@gmail.com>
2018-08-10 12:10:34 +0300

    typo fix

--- database/seeds/QuestionnaireStatusesLkpTableSeeder.php
+++ database/seeds/QuestionnaireStatusesLkpTableSeeder.php
@@ -14,7 +14,7 @@ class QuestionnaireStatusesLkpTableSeeder extends Seeder
         DB::table('questionnaire_statuses_lkp')->delete();
         DB::table('questionnaire_statuses_lkp')->insert(array(
             array('id' => 1, 'title' => 'Draft', 'description' => 'The questionnaire is still under development.'),
-            array('id' => 2, 'title' => 'Published', 'description' => 'The questionnaire is released and citizen can respond to it.'),
+            array('id' => 2, 'title' => 'Published', 'description' => 'The questionnaire is released and citizens can respond to it.'),
             array('id' => 3, 'title' => 'Finalized', 'description' => 'The questionnaire should not accept any new responses.'),
             array('id' => 4, 'title' => 'Unpublished', 'description' => 'The questionnaire is not available online anymore.'),
             array('id' => 5, 'title' => 'Deleted', 'description' => 'The questionnaire has been archived.')
