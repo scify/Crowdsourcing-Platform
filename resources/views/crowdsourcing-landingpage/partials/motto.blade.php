318c34a2a9a05224425835d83e4020389aa20ea4
Paul Isaris <paulisaris@gmail.com>
2018-08-23 16:33:09 +0300

    - Updated site texts according to feedback

--- resources/views/crowdsourcing-landingpage/partials/motto.blade.php
+++ resources/views/crowdsourcing-landingpage/partials/motto.blade.php
@@ -3,9 +3,9 @@
          style="background-image: url({{asset('images/active_participation.png')}});">
         <div class="gray-filter"></div>
         <div class="text">
-            <h2>Towards a crowdsourcing pilot at the EU level:<br>Taking decisions with citizens and not for them</h2>
+            <h2>Let's crowdsource!<br>Taking decisions with citizens and not for them</h2>
             <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1 call-to-action">
-                <a href="/login" class="btn btn-block">Start contributing</a>
+                <a href="/login" class="btn btn-block">Share your ideas</a>
             </div>
         </div>
     </div>
--- resources/views/crowdsourcing-landingpage/partials/projects.blade.php
+++ resources/views/crowdsourcing-landingpage/partials/projects.blade.php
@@ -1,9 +1,10 @@
 <div class="row">
     <div class="col-md-12">
-        <h2>Projects</h2>
+        <h2>What are we crowdsourcing?</h2>
         <div class="content-container">
-            <p>In the present platform, a lot of different projects are hosted. Below, there is a list of all the
-                currently active ones. Please visit a project's page and make an impact, by just answering a couple of questions!</p>
+            <p>ECAS crowdsourcing platform host various projects. Please check below those
+                that are the currently active and waiting for your contribution.
+                Please visit a project's page and make an impact by answering just a couple of questions!</p>
             <div class="projects-container row">
                 @foreach($projects as $project)
                     <div class="col-md-4 col-sm-6 col-xs-10 col-xs-offset-1 col-sm-offset-0 col-md-offset-0">
