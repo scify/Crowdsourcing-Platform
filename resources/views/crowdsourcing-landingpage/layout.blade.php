03264b671761e578fcfc705bf761affbc7be8ee4
Paul Isaris <paulisaris@gmail.com>
2018-08-24 10:08:57 +0300

    - Changes in texts and footer

--- resources/views/crowdsourcing-landingpage/layout.blade.php
+++ resources/views/crowdsourcing-landingpage/layout.blade.php
@@ -175,6 +175,19 @@
         </div>
     </div>
     <div class="row">
+        <div class="col-md-6 col-sm-offset-3">
+            <p style="font-size:13px; line-height:20px;"><img
+                        src="https://faireu.ecas.org/wp-content/uploads/2018/02/big-eu-flag.png" alt=""
+                        style="width:120px; float:left; margin:5px 30px 0 0;">
+                Project Co-funded by the JUSTICE, EQUALITY AND CITIZENSHIP PROGRAMME (2014-2020) OF THE EUROPEAN
+                UNION.<br>
+                The content of this website represents the views of the author only and is his/her sole responsibility.
+                The European Commission does not accept any responsibility for use that may be made of the information
+                it contains.
+            </p>
+        </div>
+    </div>
+    <div class="row">
         <div class="col-md-12">
             <div class="lower-footer">
                 Copyright &copy; {{ date("Y") }} <a target=" _blank"
