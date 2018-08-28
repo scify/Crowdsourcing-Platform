ec0d9c7cec500328f21d4165b9524685ee03da96
Paul Isaris <paulisaris@gmail.com>
2018-08-28 17:09:48 +0300

    - UI Design of dashboard page

--- resources/assets/js/pages/landingPage.js
+++ resources/assets/js/pages/landingPage.js
@@ -40,42 +40,6 @@ let ProgressBar = require('progressbar.js');
         displayTranslation.apply($('#questionnaire-lang-selector'));
     };
 
-    let displayProgressBar = function () {
-
-        let progressBar = $("#progress-bar-circle");
-        let bar = new ProgressBar.Circle(progressBar[0], {
-            color: '#0063aa',
-            // This has to be the same size as the maximum width to
-            // prevent clipping
-            strokeWidth: 4,
-            trailWidth: 2,
-            easing: 'easeInOut',
-            duration: 1400,
-            text: {
-                autoStyleContainer: false
-            },
-            from: {color: '#008AE5', width: 2},
-            to: {color: '#008AE5', width: 4},
-            // Set default step function for all animate calls
-            step: function (state, circle) {
-                circle.path.setAttribute('stroke', state.color);
-                circle.path.setAttribute('stroke-width', state.width);
-
-                let value = Math.round(circle.value() * 100);
-                if (value === 0) {
-                    circle.setText('0%');
-                } else {
-                    circle.setText(value + "%");
-                }
-
-            }
-        });
-
-        bar.text.style.fontSize = '2rem';
-        bar.animate(progressBar.data("target") / 100);  // Number from 0.0 to 1.0
-
-    };
-
     let displayTranslation = function () {
         survey.locale = $(this).val();
         survey.render();
@@ -101,7 +65,6 @@ let ProgressBar = require('progressbar.js');
 
     let init = function () {
         displayQuestionnaire();
-        displayProgressBar();
         initEvents();
         openQuestionnaireIfNeeded();
     };
--- resources/views/my-dashboard.blade.php
+++ resources/views/my-dashboard.blade.php
@@ -9,66 +9,49 @@
 @endpush
 
 @section('content')
-    {{--<div class="row">--}}
-        {{--<div class="col-md-12 col-xs-12">--}}
-            {{--<div class="box box-warning">--}}
-                {{--<div class="box-header with-border">--}}
-                    {{--<h3 class="box-title">Welcome</h3>--}}
-                {{--</div>--}}
-                {{--<div class="box-body">--}}
-                    {{--<div class="row">--}}
-                        {{--<div class="col-md-12">--}}
-                            {{--[todo:put some introductory text here, this is generic and relates to the crowdsourcing--}}
-                            {{--platform]--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
     <div class="row">
         <div class="col-md-12">
-            <div class="col-md-6 projects">
                 <div class="box box-success">
                     <div class="box-header with-border">
                         <h3 class="box-title">Active Projects</h3>
                     </div>
                     <div class="box-body">
                         <div class="row">
-                            <div class="col-md-4">
-
+                            <div class="col-md-6 text-center">
                                 @if($viewModel->projects->count() === 0)
-
                                     <div class="no-projects-found">There are currently no active projects.
                                     </div>
                                 @else
                                     @foreach($viewModel->projects as $project)
-                                        <div style="padding-top:15px"><a href="{{$project->slug}}"> <img height="30" alt="{{$project->name}}"
+                                        <div style="padding-top:15px"><a href="{{$project->slug}}"> <img height="70" alt="{{$project->name}}"
                                                                                                          src="{{asset($project->logo_path)}}"></a>
                                         </div>
                                     @endforeach
                                 @endif
+                                <div class="row">
+                                    <div class="col-xs-3 progress-container">
+                                        @if($viewModel->projectGoalVM)
+                                            @include('landingpages.partials.project-goal', ['projectGoalVM' => $viewModel->projectGoalVM])
+                                        @endif
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-md-4">
-                                @if($viewModel->projectGoalVM)
-                                    @include('landingpages.partials.project-goal', ['projectGoalVM' => $viewModel->projectGoalVM])
-                                @endif
+                            <div class="col-md-6">
+                                @include('gamification.next-step', ['nextStepVM' => $viewModel->gamificationNextStepVM])
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
 
     </div>
     <div class="row gamification-box">
-        <div class="col-md-6">
-            @include('gamification.next-step', ['nextStepVM' => $viewModel->gamificationNextStepVM])
-        </div>
-        <div class="col-md-6">
+        <div class="col-md-12" style="position: relative">
+        <div class="col-md-6" style="margin: 10px auto; float: none !important;">
             <div id="awards">
                 @include('gamification.user-badges', ['badgesVM' => $viewModel->badgesVM])
             </div>
+        </div>
         </div>
     </div>
 @stop
