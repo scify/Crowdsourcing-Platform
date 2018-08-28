cbd91a653ad1c3e28ec74f9f15fb9e67545f1b7f
Paul Isaris <paulisaris@gmail.com>
2018-08-28 10:01:20 +0300

    - Redesigned Gamification next steps box in dashboard page

--- resources/views/gamification/next-step.blade.php
+++ resources/views/gamification/next-step.blade.php
@@ -1,9 +1,19 @@
-<div class="row nextStepContainer">
-    <div class="col-md-12">
-        <h4>{{ $nextStepVM->title }}</h4>
-        <h5>{{ $nextStepVM->subtitle }}</h5>
-        <div class="nextStepImgContainer">
-            <img class="nextStepImg" src="{{asset("images/badges/" . $nextStepVM->imgFileName)}}">
+@push('css')
+    <link rel="stylesheet" href="{{ asset('dist/css/next-step.css') }}">
+@endpush
+<div class="box box-primary">
+    <div class="box-header with-border">
+        <h3 class="box-title">What to do next?</h3>
+    </div>
+    <div class="box-body">
+        <div class="row nextStepContainer">
+            <div class="col-md-12">
+                <h4>{{ $nextStepVM->title }}</h4>
+                <h5>{{ $nextStepVM->subtitle }}</h5>
+                <div class="nextStepImgContainer">
+                    <img class="nextStepImg" src="{{asset("images/badges/" . $nextStepVM->imgFileName)}}">
+                </div>
+            </div>
         </div>
     </div>
 </div>
