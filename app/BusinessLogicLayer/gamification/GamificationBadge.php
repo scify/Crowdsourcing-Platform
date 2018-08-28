bea5210b40c15f0a174d2d2a88b1c0ff8e758659
Paul Isaris <paulisaris@gmail.com>
2018-08-28 11:20:43 +0300

    - Refactored gamification badge classes to have less repetitive code and to be more coherent

--- app/BusinessLogicLayer/gamification/GamificationBadge.php
+++ app/BusinessLogicLayer/gamification/GamificationBadge.php
@@ -6,17 +6,24 @@ abstract class GamificationBadge {
 
     protected $pointsPerAction = 1;
     public $badgeID;
-    protected $numberOfActionsPerformed = -1;
+    public $numberOfActionsPerformed = -1;
+    public $level = 0;
+    public $name;
+    public $messageForLevel;
+    public $imageFileName;
 
-    abstract public function getBadgeMessageForLevel(int $level);
+    public function __construct($name, $imageFileName, $numberOfActionsPerformed, $pointsPerAction = 1) {
+        $this->name = $name;
+        $this->imageFileName = $imageFileName;
+        $this->numberOfActionsPerformed = $numberOfActionsPerformed;
+        $this->pointsPerAction = $pointsPerAction;
+        $this->level = $this->calculateLevel();
+        $this->messageForLevel = $this->getBadgeMessageForLevel();
+    }
 
-    abstract public function getBadgeName();
+    abstract protected function getBadgeMessageForLevel();
 
-    abstract public function getBadgeImageName();
-
-    abstract public function getNumberOfActionsPerformed(int $userId);
-
-    public function getLevel(int $userId) {
-        return $this->getNumberOfActionsPerformed($userId) * $this->pointsPerAction;
+    protected function calculateLevel() {
+        return $this->numberOfActionsPerformed * $this->pointsPerAction;
     }
 }
