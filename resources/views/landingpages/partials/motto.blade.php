65bd98ba9649b63026e19ecd655cc3489e08616b
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2018-08-28 12:02:25 +0300

    fix title on project moto

--- resources/views/landingpages/partials/motto.blade.php
+++ resources/views/landingpages/partials/motto.blade.php
@@ -3,7 +3,7 @@
          style="background-image: url({{asset($viewModel->project->img_path)}});">
         <div class="gray-filter"></div>
         <div class="text">
-            <h2>{{$viewModel->project->motto}}</h2>
+            <h1 id="project-moto">{!! $viewModel->project->motto !!}</h1>
             <div class="col-md-2 col-md-offset-5 col-xs-6 col-xs-offset-3 call-to-action">
 
                 @include("landingpages.partials.open-questionnaire-button")
