4ef71577ed9238045af9817e3d195396a5abde91
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2018-08-30 12:35:33 +0300

    fix: webpack does not procude source maps

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,16 @@ mix.js('resources/assets/js/common.js', 'public/dist/js/')
     .js('resources/assets/js/questionnaireSocialShare.js', 'public/dist/js')
     .js('resources/assets/js/partials/newsletter-signup.js', 'public/dist/js')
     .js('resources/assets/js/UsersListController.js', 'public/dist/js')
-    .sass('resources/assets/sass/common.scss', 'public/dist/css')
+    .extract([
+        'jquery-slimscroll', 'fastclick', 'admin-lte', 'bootstrap-sweetalert', 'select2', 'bootstrap', 'jquery-toast-plugin'
+    ])
+    .sourceMaps()
+    .webpackConfig({
+        devtool: 'source-map'
+    })
+    .version();
+
+mix.sass('resources/assets/sass/common.scss', 'public/dist/css')
     .sass('resources/assets/sass/gamification/badges.scss', 'public/dist/css')
     .sass('resources/assets/sass/gamification/next-step.scss', 'public/dist/css')
     .sass('resources/assets/sass/questionnaire/social-share.scss', 'public/dist/css')
@@ -27,10 +36,10 @@ mix.js('resources/assets/js/common.js', 'public/dist/js/')
     .sass('resources/assets/sass/pages/manage-questionnaires.scss', 'public/dist/css')
     .sass('resources/assets/sass/pages/translate-questionnaire.scss', 'public/dist/css')
     .sass('resources/assets/sass/pages/home.scss', 'public/dist/css')
-    .extract([
-        'jquery-slimscroll', 'fastclick', 'admin-lte', 'bootstrap-sweetalert', 'select2', 'bootstrap', 'jquery-toast-plugin'
-    ])
     .sourceMaps()
+    .webpackConfig({
+        devtool: 'source-map'
+    })
     .version();
 
 // move sweetalert.css to public/dist/css
