333d15b2f35f6566518d47e0075217900b5a7785
Paul Isaris <paulisaris@gmail.com>
2018-08-31 10:18:39 +0300

    - Removed unused parsed variable - Changed message for "Influencer" badge

--- app/BusinessLogicLayer/gamification/InfluencerBadge.php
+++ app/BusinessLogicLayer/gamification/InfluencerBadge.php
@@ -11,18 +11,18 @@ class InfluencerBadge extends GamificationBadge {
 
     public function __construct(QuestionnaireResponseReferralManager $questionnaireResponseReferralManager, int $userId) {
         $this->questionnaireResponseReferralManager = $questionnaireResponseReferralManager;
-        $pointsPerAction = 5;
+        $this->pointsPerAction = 5;
         $this->badgeID = GamificationBadgeIdsEnum::INFLUENCER_BADGE_ID;
         $this->color = '#f44336';
         $numberOfActionsPerformed = $this->questionnaireResponseReferralManager->getQuestionnaireReferralsForUser($userId)->count();
         parent::__construct("Influencer",
             "influencer.png",
             "In order to gain this badge, people have to respond to your social posts and contribute!",
-            $numberOfActionsPerformed, $userId, $pointsPerAction);
+            $numberOfActionsPerformed, $userId);
     }
 
     public function getBadgeMessageForLevel() {
-        return 'You have invited ' . $this->numberOfActionsPerformed . ' people to answer';
+        return $this->numberOfActionsPerformed . ' people have anwered to questionnaires you shared';
     }
 
     public function getEmailBody() {
