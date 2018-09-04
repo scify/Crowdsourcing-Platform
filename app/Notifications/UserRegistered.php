bb32d1201460e79daf0be5ddb3e3312bee2c0ead
Paul Isaris <paulisaris@gmail.com>
2018-09-04 15:25:53 +0300

    Added a new email notification that is sent to a user that joins the platform

---
+++ app/Notifications/UserRegistered.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class UserRegistered extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('ECAS | Welcome!')
+            ->greeting('Thanks for joining the ECAS Crowdsourcing Platform!')
+            ->line('<br><p style="text-align: center"><b>Are you ready to be a part of our community?</b><br></p>')
+            ->line('<br><p style="text-align: center"><b>Visit your dashboard to see what\'s next</b><br></p>')
+            ->action('Go to Dashboard', url('/my-dashboard'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
