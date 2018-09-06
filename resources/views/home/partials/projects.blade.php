31ccc3c81c1b7a416b5fd5b817f6985497d6e52f
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2018-09-06 18:02:42 +0300

    fix labels

--- resources/views/home/partials/projects.blade.php
+++ resources/views/home/partials/projects.blade.php
@@ -2,7 +2,7 @@
     <div class="col-md-12">
         <h2>What are we crowdsourcing?</h2>
         <div class="content-container">
-            <p>ECAS crowdsourcing platform host various projects. Please check below those
+            <p>ECAS crowdsourcing platform hosts various projects. Please check below those
                 that are the currently active and waiting for your contribution.
                 Please visit a project's page and make an impact by answering just a couple of questions!</p>
             <div class="projects-container row">
