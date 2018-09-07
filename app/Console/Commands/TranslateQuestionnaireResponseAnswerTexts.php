e4533e786f27d89780609d49a7eba43e68443932
Paul Isaris <paulisaris@gmail.com>
2018-09-07 16:33:37 +0300

    Added laravel command to automatically translate questionnaire answer texts

---
+++ app/Console/Commands/TranslateQuestionnaireResponseAnswerTexts.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\BusinessLogicLayer\QuestionnaireResponseAnswerTranslator;
+use Illuminate\Console\Command;
+
+class TranslateQuestionnaireResponseAnswerTexts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'translate:questionnaire_answer_texts';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command translates any questionnaire response answer texts that have not been translated yet.';
+
+    private $questionnaireResponseAnswerTranslator;
+
+    public function __construct(QuestionnaireResponseAnswerTranslator $questionnaireResponseAnswerTranslator)
+    {
+        parent::__construct();
+        $this->questionnaireResponseAnswerTranslator = $questionnaireResponseAnswerTranslator;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->questionnaireResponseAnswerTranslator->translateAllNonTranslatedAnswerTexts();
+    }
+}
