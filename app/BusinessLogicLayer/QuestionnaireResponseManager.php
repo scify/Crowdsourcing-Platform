87a66875265c75e95c48c093332b8df7195b7067
Paul Isaris <paulisaris@gmail.com>
2018-09-10 11:18:09 +0300

    Added basic view for showing all questionnaire responses in user dashboard

---
+++ app/BusinessLogicLayer/QuestionnaireResponseManager.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\BusinessLogicLayer;
+
+
+use App\Models\User;
+use App\Repository\QuestionnaireRepository;
+
+class QuestionnaireResponseManager {
+
+    private $questionnaireRepository;
+
+    public function __construct(QuestionnaireRepository $questionnaireRepository) {
+        $this->questionnaireRepository = $questionnaireRepository;
+    }
+
+    public function getQuestionnaireResponsesForUser(User $user) {
+        return $this->questionnaireRepository->getAllResponsesGivenByUser($user->id);
+    }
+}
