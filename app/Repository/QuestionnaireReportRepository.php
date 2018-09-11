edbff88ab53b705471cf9d6a65be542e179c583a
Paul Isaris <paulisaris@gmail.com>
2018-09-11 16:18:55 +0300

    - Questionnaire responses answer texts are now fetched in a separate DB query. - The answer text data is shown in a new datatable, attached in a modal. - Deleted unused js file

--- app/Repository/QuestionnaireReportRepository.php
+++ app/Repository/QuestionnaireReportRepository.php
@@ -26,21 +26,36 @@ class QuestionnaireReportRepository {
 
     public function getReportDataForAnswers($questionnaireId) {
         return DB::select('
-            select  qra.question_id, qra.answer_id, count(*) as num_of_times, qpa.answer, qrat.answer as text_answer,
-             qrat.english_translation as answer_english_translation, qrat.initial_language_detected as answer_initial_language_detected, qq.question
-
-            from questionnaire_responses as qr
-            
-                inner join questionnaires q on q.id = qr.questionnaire_id
-                inner join questionnaire_response_answers qra on qra.questionnaire_response_id = qr.id
-                inner join questionnaire_questions qq on qq.id = qra.question_id
-                left outer join questionnaire_possible_answers qpa on qpa.question_id = qq.id and qpa.id = qra.answer_id
-                left outer join questionnaire_response_answer_texts qrat on qrat.questionnaire_response_answer_id = qra.id
-            
-            where qr.questionnaire_id = ' . $questionnaireId . ' and qr.deleted_at is null
-            
-            group by qra.question_id, qra.answer_id, qpa.answer, qrat.answer, qq.question, qrat.english_translation, qrat.initial_language_detected
-            order by qq.id, qpa.id;
+                select
+                q.id as questionnaire_id,
+                qq.id as question_id, 
+                qq.question,
+                qpa.id as answer_id,
+                qpa.answer,
+                IFNULL(ResponseCounts.num_of_responses,0) as num_of_responses
+                
+                from 
+                    questionnaires q 
+                    inner join questionnaire_questions qq on qq.questionnaire_id = q.id
+                    left outer join questionnaire_possible_answers qpa on qpa.question_id = qq.id
+                
+                    left outer join
+                        (
+                        select 
+                            question_id,answer_id, count(*) as num_of_responses
+                            from questionnaire_response_answers  qra
+                                inner join questionnaire_questions qq on qq.id = qra.question_id
+                                where qq.questionnaire_id = ' . $questionnaireId . '
+                                group by question_id,answer_id
+                        ) as ResponseCounts on  ResponseCounts.question_id = qq.id and ResponseCounts.answer_id <=>  qpa.id
+                   
+                where 
+                q.id = 1 
+                and qq.type <> "html"
+                
+                and qq.deleted_at is null
+                and qpa.deleted_at is null 
+                order by qq.id, qpa.id
         ');
     }
 }
