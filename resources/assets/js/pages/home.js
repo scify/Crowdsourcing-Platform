fa31302499ef5ddc47325e7adba03e025c44f2fc
Paul Isaris <paulisaris@gmail.com>
2018-09-24 12:28:14 +0300

    Added github repo badges Code style in home js file

--- resources/assets/js/pages/home.js
+++ resources/assets/js/pages/home.js
@@ -1,11 +1,8 @@
-(function() {
+'use strict';
 
-    $(document).on('click', '#top-menu-content a[href^="#"]', function (event) {
-        event.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top-50
-        }, 500);
-    });
-
+(function () {
+  $(document).on('click', '#top-menu-content a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('html, body').animate({ scrollTop: $($.attr(this, 'href')).offset().top - 50 }, 500);
+  });
 })();
