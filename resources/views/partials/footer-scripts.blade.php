f6d2af055f0bcde5f346ac95c2fb61efe1b7a6bc
Paul Isaris <paulisaris@gmail.com>
2018-09-27 10:01:34 +0300

    Updated local to production when including script for google analytics

--- resources/views/partials/footer-scripts.blade.php
+++ resources/views/partials/footer-scripts.blade.php
@@ -1,7 +1,7 @@
 <script src="{{ mix('dist/js/manifest.js') }}?{{ env("APP_VERSION") }}"></script> {{-- The Webpack manifest runtime--}}
 <script src="{{ mix('dist/js/vendor.js') }}?{{env("APP_VERSION")}}"></script> {{-- Vendor libraries like jQuery, bootstrap --}}
 <script src="{{ mix('dist/js/common.js')}}?{{env("APP_VERSION")}}"></script> {{-- our application common code --}}
-@if (App::environment('local'))
+@if (App::environment('production'))
     @include('analytics')
 @endif
 @stack('scripts')
