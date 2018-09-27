d81d9694df5cca2644e7392b73aab3c35c4ffb58
Paul Isaris <paulisaris@gmail.com>
2018-09-27 10:01:07 +0300

    Google Analytics ID is now part of the .env file

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -4,7 +4,7 @@
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-54374180-5', 'auto');
+    ga('create', '{{ env('GA_ID') }}', 'auto');
     ga('send', 'pageview');
 
 </script>
