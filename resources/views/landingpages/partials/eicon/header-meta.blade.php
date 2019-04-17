b42b8559f9672efc39bee78b8de6678e5ce134d1
Paul Isaris <paulisaris@gmail.com>
2019-04-17 15:08:25 +0300

    Updated social sharing text

--- resources/views/landingpages/partials/eicon/header-meta.blade.php
+++ resources/views/landingpages/partials/eicon/header-meta.blade.php
@@ -1,6 +1,6 @@
 <meta name="keywords" content="EICON, crowdsourcing, Europe, EU, democracy">
 <meta property="og:title" content="Are you an expert in VET?">
 <meta property="og:site_name" content="EICON Crowdsourcing Platform">
-<meta property="twitter:title" content="Are you an expert in VET?">
+<meta property="twitter:title" content="Are you an expert / practicioner in Inclusive Education?">
 <meta property="twitter:description" content="Take part in our questionnaire!">
 <meta property="twitter:creator" content="eicon_europe">
