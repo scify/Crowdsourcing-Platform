ce0ffa4fa9c97800a87234b82955b4e95c7f1201
Paul Isaris <paulisaris@gmail.com>
2019-09-05 12:24:08 +0300

    query fix

--- app/Repository/QuestionnaireReportRepository.php
+++ app/Repository/QuestionnaireReportRepository.php
@@ -14,7 +14,7 @@ class QuestionnaireReportRepository {
 
                 inner join users as u on u.id = qr.user_id
                 inner join questionnaires q on q.id = qr.questionnaire_id
-                inner join questionnaire_response_answers qra on qra.questionnaire_response_id = qr.id
+                left outer join questionnaire_response_answers qra on qra.questionnaire_response_id = qr.id
                 inner join questionnaire_questions qq on qq.id = qra.question_id
                 left outer join questionnaire_possible_answers qpa on qpa.question_id = qq.id and qpa.id = qra.answer_id
                 left outer join questionnaire_response_answer_texts qrat on qrat.questionnaire_response_answer_id = qra.id
