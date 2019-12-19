da55f00a9bb02238f1aa1fcb832f50a22af01116
Paul Isaris <paulisaris@gmail.com>
2019-12-19 15:41:28 +0200

    Added check for project status when trying to access it's landing page

---
+++ resources/views/landingpages/project-unavailable.blade.php
@@ -0,0 +1,21 @@
+@extends('landingpages.layout')
+
+@section('content')
+    <section>
+        <div class="row" id="motto">
+            <div class="col-md-12 image-with-text-wrapper no-padding"
+                 style="background-image: url({{asset($viewModel->project->img_path)}});">
+                <div class="gray-filter"></div>
+                <div class="text">
+                    <h1 id="project-moto">{!! $viewModel->project->motto !!}</h1>
+                    <div class="col-md-2 col-md-offset-5 col-xs-6 col-xs-offset-3 call-to-action">
+                        <h2>{{ $viewModel->getProjectStatusMessage() }}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        @include('landingpages.partials.about')
+    </section>
+@endsection
