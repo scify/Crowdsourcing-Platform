e56d4c47b2a9a3c727a3fbc130f0fed1ebd36873
Paul Isaris <paulisaris@gmail.com>
2020-01-09 11:55:50 +0200

    Added separate classes for user role management and project access management

---
+++ app/BusinessLogicLayer/lkp/UserRolesLkp.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\BusinessLogicLayer\lkp;
+
+
+abstract class UserRolesLkp {
+    //ATTENTION: these values match with the db values defined in database\seeds\UsersRoleLkpTableSeeder.php
+    const ADMIN = 1;
+    const CONTENT_MANAGER = 2;
+    const REGISTERED_USER = 3;
+}
