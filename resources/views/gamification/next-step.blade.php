89bb971916b2de8f2e454b750e106bc3baff75cb
Paul Isaris <paulisaris@gmail.com>
2020-01-15 17:07:39 +0200

    Removed redundant if statement

--- resources/views/gamification/next-step.blade.php
+++ resources/views/gamification/next-step.blade.php
@@ -9,15 +9,13 @@
             <img class="nextStepImg" src="{{asset("images/badges/" . $nextStepVM->imgFileName)}}">
         </div>
     </div>
-    @if($nextStepVM->projectHasActiveQuestionnaire)
-        @if($nextStepVM->userHasAlreadyAnsweredTheActiveQuestionnaire)
-            <div class="col-md-12">
-                @include('questionnaire.social-share', ['viewModel' => $nextStepVM->socialShareVM])
-            </div>
-        @else
-            <div class="col-md-12">
-                <a href="{{url("/" . $nextStepVM->project->slug . "?open=1")}}" class="btn btn-primary btn-lg nextStepActionBtn">Speak up</a>
-            </div>
-        @endif
+    @if($nextStepVM->userHasAlreadyAnsweredTheActiveQuestionnaire)
+        <div class="col-md-12">
+            @include('questionnaire.social-share', ['viewModel' => $nextStepVM->socialShareVM])
+        </div>
+    @else
+        <div class="col-md-12">
+            <a href="{{url("/" . $nextStepVM->project->slug . "?open=1")}}" class="btn btn-primary btn-lg nextStepActionBtn">Speak up</a>
+        </div>
     @endif
 </div>
