2de17988d92a5e6da32c665624f614d8af613e27
Paul Isaris <paulisaris@gmail.com>
2020-01-15 17:01:20 +0200

    Added a separate class to compute the gamification badges for a given questionnaire

---
+++ app/BusinessLogicLayer/gamification/QuestionnaireBadgeProvider.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace App\BusinessLogicLayer;
+
+
+use App\BusinessLogicLayer\gamification\CommunicatorBadge;
+use App\BusinessLogicLayer\gamification\ContributorBadge;
+use App\BusinessLogicLayer\gamification\GamificationBadge;
+use App\BusinessLogicLayer\gamification\InfluencerBadge;
+use App\Models\Questionnaire;
+use App\Repository\QuestionnaireRepository;
+use App\Repository\QuestionnaireResponseReferralRepository;
+use App\Repository\QuestionnaireResponseRepository;
+use App\Repository\UserQuestionnaireShareRepository;
+
+class QuestionnaireBadgeProvider {
+
+    protected $questionnaireRepository;
+    protected $questionnaireResponseRepository;
+    protected $userQuestionnaireShareRepository;
+    protected $questionnaireResponseReferralRepository;
+
+    public function __construct(QuestionnaireRepository $questionnaireRepository,
+                                QuestionnaireResponseRepository $questionnaireResponseRepository,
+                                UserQuestionnaireShareRepository $userQuestionnaireShareRepository,
+                                QuestionnaireResponseReferralRepository $questionnaireResponseReferralRepository) {
+        $this->questionnaireRepository = $questionnaireRepository;
+        $this->questionnaireResponseRepository = $questionnaireResponseRepository;
+        $this->userQuestionnaireShareRepository = $userQuestionnaireShareRepository;
+        $this->questionnaireResponseReferralRepository = $questionnaireResponseReferralRepository;
+    }
+
+
+    public function getNextUnlockableBadgeToShowForQuestionnaire(Questionnaire $questionnaire, int $userId): GamificationBadge {
+        if(!$this->userHasAchievedContributorBadgeForQuestionnaire($questionnaire, $userId))
+            return new ContributorBadge($this->questionnaireRepository->
+            getAllResponsesGivenByUser($userId)->count());
+
+        if(!$this->userHasAchievedCommunicatorBadgeForQuestionnaire($questionnaire, $userId))
+            return new CommunicatorBadge($this->userQuestionnaireShareRepository->
+            getUserQuestionnaireSharesForUserForQuestionnaire($questionnaire->id, $userId)->count(), $userId);
+
+        if(!$this->userHasAchievedInfluencerBadgeForQuestionnaire($questionnaire, $userId))
+            return new InfluencerBadge($this->questionnaireResponseReferralRepository->
+            getQuestionnaireReferralsForUserForQuestionnaire($questionnaire->id, $userId)->count());
+
+        return null;
+    }
+
+    protected function userHasAchievedContributorBadgeForQuestionnaire(Questionnaire $questionnaire, int $userId) {
+        return $this->questionnaireResponseRepository->questionnaireResponseExists($questionnaire->id, $userId);
+    }
+
+    protected function userHasAchievedCommunicatorBadgeForQuestionnaire(Questionnaire $questionnaire, int $userId) {
+        return $this->userQuestionnaireShareRepository->questionnaireShareExists($questionnaire->id, $userId);
+    }
+
+    protected function userHasAchievedInfluencerBadgeForQuestionnaire(Questionnaire $questionnaire, int $userId) {
+        return $this->questionnaireResponseReferralRepository->questionnaireReferralByUserExists($questionnaire->id, $userId);
+    }
+
+}
