e270a9b2250407778a6213359efe9b7302c04805
Paul Isaris <paulisaris@gmail.com>
2020-01-15 17:11:17 +0200

    Added View model for platform wide gamification badges list

---
+++ app/Models/ViewModels/PlatformWideGamificationBadges.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\ViewModels;
+
+class PlatformWideGamificationBadges {
+
+    public $contributorBadge;
+    public $communicatorBadge;
+    public $influencerBadge;
+
+    public function __construct(GamificationBadgeVM $contributorBadge, GamificationBadgeVM $communicatorBadge, GamificationBadgeVM $influencerBadge) {
+        $this->contributorBadge = $contributorBadge;
+        $this->communicatorBadge = $communicatorBadge;
+        $this->influencerBadge = $influencerBadge;
+    }
+}
