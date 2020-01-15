0a2b7a65863e4cc5688913e42bbd33bd28db4466
Paul Isaris <paulisaris@gmail.com>
2020-01-15 17:07:56 +0200

    Renamed view model variable

--- resources/views/landingpages/partials/project-goal.blade.php
+++ resources/views/landingpages/partials/project-goal.blade.php
@@ -1,8 +1,8 @@
 <div class="activity-title wrapper-title">
-    <p>{{ $projectGoalVM->responsesNeededToReachGoal }} answers left to reach our goal</p>
+    <p>{{ $viewModel->responsesNeededToReachGoal }} answers left to reach our goal</p>
 </div>
 <div class="progress-container">
     <div class="progress-bar-circle" id="project-progress-{{ $projectId }}"
-         data-target="{{$projectGoalVM->targetAchievedPercentage}}">
+         data-target="{{$viewModel->targetAchievedPercentage}}">
     </div>
 </div>
