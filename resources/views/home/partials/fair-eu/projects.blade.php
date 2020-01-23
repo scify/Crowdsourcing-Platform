b848cb1c6c8bd700690b2378d51ac2f837f99686
Paul Isaris <paulisaris@gmail.com>
2020-01-23 10:54:44 +0200

    UI redesign for multiple projects in home page

--- resources/views/home/partials/fair-eu/projects.blade.php
+++ resources/views/home/partials/fair-eu/projects.blade.php
@@ -1,11 +1,15 @@
-<div class="row">
-    <div class="col-md-12">
-        <h2>What are we crowdsourcing?</h2>
-        <div class="content-container">
-            <p>ECAS crowdsourcing platform hosts various projects. Please check below those
-                that are the currently active and waiting for your contribution.
-                Please visit a project's page and make an impact by answering just a couple of questions!</p>
-            @include('home.partials.projects-list-home')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>What are we crowdsourcing?</h2>
+            <div class="content-container">
+                <p>ECAS crowdsourcing platform hosts various projects. Please check below those
+                    that are the currently active and waiting for your contribution.
+                    Please visit a project's page and make an impact by answering just a couple of questions!</p>
+
+                @include('home.partials.projects-list-home')
+
+            </div>
         </div>
     </div>
 </div>
