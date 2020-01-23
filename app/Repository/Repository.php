cff1dbec03e4cc34885967ccf749213c681692c2
Paul Isaris <paulisaris@gmail.com>
2020-01-23 14:32:35 +0200

    Correct import

--- app/Repository/Repository.php
+++ app/Repository/Repository.php
@@ -3,9 +3,9 @@
 namespace App\Repository;
 
 use Illuminate\Container\Container as App;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Collection;
 
 abstract class Repository implements RepositoryInterface {
 
