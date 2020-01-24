daae1e4e25ec94606269a92d451aae0f8ee66a25
Paul Isaris <paulisaris@gmail.com>
2020-01-24 16:03:44 +0200

    Add default project layout to all error pages (404, 500 etc)

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -1,32 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    @include('partials.favicons')
-    <title>@yield('title')</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{ asset('dist/css/common.css') }}">
+@extends('home.layout')
+@push('css')
     <link rel="stylesheet" href="{{ asset('dist/css/errors.css') }}">
-    @if (App::environment('production'))
-        @include('analytics')
-    @endif
-</head>
-<body class="hold-transition background-page @yield('body_class')" style="background: url('{{ asset('images/projects/' . config('app.project_resources_dir') . '/active_participation.png') }}') no-repeat">
-@if (App::environment('staging'))
-    <div class="staging-warning">
-        <p>~~~WARING: STAGING ENVIRONMENT~~~</p>
-    </div>
-@endif
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="code">
-            @yield('code')
+@endpush
+@section('title_prefix')
+    @yield('title')
+@endsection
+@section('content')
+    <section>
+        <div class="flex-center position-ref error-container">
+            <div class="content">
+                <div class="code">
+                    @yield('code')
+                </div>
+                <div class="title">
+                    @yield('message')
+                </div>
+            </div>
         </div>
-        <div class="title">
-            @yield('message')
-        </div>
-    </div>
-</div>
-</body>
-</html>
+    </section>
+@endsection
+
