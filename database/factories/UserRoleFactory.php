d4f6c2c8ddfb0a1147a6cf789dc1d021332b0d99
Paul Isaris <paulisaris@gmail.com>
2020-01-24 14:44:16 +0200

    Added Model Factories for easier testing

---
+++ database/factories/UserRoleFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\BusinessLogicLayer\lkp\UserRolesLkp;
+use App\Models\User;
+use App\Models\UserRole;
+use Faker\Generator as Faker;
+
+$factory->define(UserRole::class, function (Faker $faker) use ($factory) {
+    return [
+        'user_id' => $factory->create(User::class)->id,
+        'role_id' => UserRolesLkp::REGISTERED_USER
+    ];
+});
