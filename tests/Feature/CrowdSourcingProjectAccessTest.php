cef0ecbd4a727e7ad8e24a1fe7799b7d68779dba
Paul Isaris <paulisaris@gmail.com>
2020-01-24 14:45:04 +0200

    Added Feature tests for crowdsourcing project landing page privacy level

---
+++ tests/Feature/CrowdSourcingProjectAccessTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\BusinessLogicLayer\lkp\CrowdSourcingProjectStatusLkp;
+use App\BusinessLogicLayer\lkp\UserRolesLkp;
+use App\Models\User;
+use App\Models\UserRole;
+use App\Repository\CrowdSourcingProjectRepository;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CrowdSourcingProjectAccessTest extends TestCase {
+    protected $crowdSourcingProjectRepository;
+
+    protected function setUp(): void {
+        parent::setUp();
+        $this->crowdSourcingProjectRepository = $this->app->make(CrowdSourcingProjectRepository::class);
+    }
+
+
+    /**
+     * @test
+     */
+    public function guestCanViewPublishedProjectLandingPage() {
+        // get a published project
+        $project = $this->crowdSourcingProjectRepository
+            ->findBy('status_id', CrowdSourcingProjectStatusLkp::PUBLISHED, 'slug')->first();
+
+        $response = $this->get(route('project.landing-page', $project->slug));
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function guestCannotViewNonPublishedProjectLandingPage() {
+        // get a non published project
+        $project = $this->crowdSourcingProjectRepository
+            ->where([['status_id', '<>', CrowdSourcingProjectStatusLkp::PUBLISHED]], 'slug')->first();
+
+        $response = $this->get(route('project.landing-page', $project->slug));
+
+        $response->assertStatus(404);
+    }
+
+    /**
+     * @test
+     */
+    public function authenticatedUserCannotViewNonPublishedProjectLandingPage() {
+        $user = factory(User::class)->make();
+
+        $this->be($user);
+        // get a non published project
+        $project = $this->crowdSourcingProjectRepository
+            ->where([['status_id', '<>', CrowdSourcingProjectStatusLkp::PUBLISHED]], 'slug')->first();
+
+        $response = $this->get(route('project.landing-page', $project->slug));
+
+        $response->assertStatus(404);
+    }
+
+    /**
+     * @test
+     */
+    public function adminCanViewNonPublishedProjectLandingPage() {
+        $user = factory(User::class, 1)
+            ->make()
+            ->each(function ($user) {
+                $user->userRoles()->save(factory(UserRole::class)->make(['user_id' => $user->id, 'role_id' => UserRolesLkp::ADMIN]));
+            })->first();
+
+        $this->be($user);
+        // get a non published project
+        $project = $this->crowdSourcingProjectRepository
+            ->where([['status_id', '<>', CrowdSourcingProjectStatusLkp::PUBLISHED]], 'slug')->first();
+
+        $response = $this->get(route('project.landing-page', $project->slug));
+
+        $response->assertStatus(200);
+    }
+}
