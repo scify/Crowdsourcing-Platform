c6a23a7b3d9a0b57029ceafd933efc124cde21e7
Paul Isaris <paulisaris@gmail.com>
2020-02-04 13:10:17 +0200

    Fixed navbar to be compatible with Bootstrap 4

--- resources/views/home/partials/fair-eu/navbar.blade.php
+++ resources/views/home/partials/fair-eu/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="main-header navbar navbar-expand navbar-white navbar-light fixed-top navbar-default navbar-fixed-top">
+<nav class="main-header navbar navbar-expand navbar-white navbar-light fixed-top navbar-default navbar-fixed-top m-0">
     @if (App::environment('staging'))
         <div class="staging-warning">
             <p>~~~WARING: STAGING ENVIRONMENT~~~</p>
@@ -9,24 +9,20 @@
             <a class="navbar-brand" href="#">
                 <img alt="ECAS" src="{{asset('images/projects/fair-eu/ecas_logo.png')}}">
             </a>
-            <button type="button" class="navbar-toggle collapsed" data-widget="collapse"
-                    data-target="#top-menu-content">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top-menu-content" aria-controls="top-menu-content" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
         </div>
         <div class="collapse navbar-collapse pull-right" id="top-menu-content">
-            <ul class="nav navbar-nav">
-                <li>
-                    <a href="#about">ABOUT US</a>
+            <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">ABOUT US</a>
                 </li>
-                <li>
-                    <a href="#features">FEATURES</a>
+                <li class="nav-item">
+                    <a class="nav-link" href="#features">FEATURES</a>
                 </li>
-                <li>
-                    <a href="#projects">WHAT ARE WE CROWDSOURCING?</a>
+                <li class="nav-item">
+                    <a class="nav-link" href="#projects">WHAT ARE WE CROWDSOURCING?</a>
                 </li>
                 @include("partials.login-menu-options")
             </ul>
--- resources/views/partials/login-menu-options.blade.php
+++ resources/views/partials/login-menu-options.blade.php
@@ -1,46 +1,47 @@
 @if (Auth::check())
-<li>
-    <a href="/my-dashboard">DASHBOARD</a>
-</li>
-<li class="dropdown user user-menu">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        @if (Auth::user()->avatar)
-        <img src="{{ Auth::user()->avatar}}" class="user-image" >
-        @endif
-        <span class="hidden-xs">{{Auth::user()->nickname}}</span>
-    </a>
-    <ul class="dropdown-menu">
-        <!-- User image -->
-        <li class="user-header">
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('my-dashboard') }}">DASHBOARD</a>
+    </li>
+    <li class="nav-item dropdown user user-menu">
+        <a class="nav-link" href="#" class="dropdown-toggle" data-toggle="dropdown">
             @if (Auth::user()->avatar)
-            <img src="{{Auth::user()->avatar}}" class="img-circle">
+                <img src="{{ Auth::user()->avatar}}" class="user-image">
             @endif
-            <p>
-                {{ Auth::user()->name }} {{ Auth::user()->nickname }}
-                <small>Member since   {{ Auth::user()->created_at }}</small>
-            </p>
-        </li>
+            <span class="hidden-xs">{{Auth::user()->nickname}}</span>
+            <i class="fas fa-caret-down"></i>
+        </a>
+        <ul class="dropdown-menu">
+            <!-- User image -->
+            <li class="user-header">
+                @if (Auth::user()->avatar)
+                    <img src="{{Auth::user()->avatar}}" class="img-circle">
+                @endif
+                <p>
+                    {{ Auth::user()->name }} {{ Auth::user()->nickname }}
+                    <small>Member since {{ Auth::user()->created_at }}</small>
+                </p>
+            </li>
 
-        <li class="user-footer">
-            <div class="pull-left">
-                <!--<a href="#" class="btn btn-default btn-flat">Profile</a>-->
-            </div>
-            <div class="pull-right">
-                <a id="log-out" href="{{ route('logout') }}"
-                   class="btn btn-default btn-flat"
-                   onclick="e">Sign out</a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    {{--                                {{ csrf_field() }}--}}
-                </form>
-            </div>
-        </li>
-    </ul>
-</li>
+            <li class="user-footer">
+                <div class="pull-left">
+                    <!--<a href="#" class="btn btn-default btn-flat">Profile</a>-->
+                </div>
+                <div class="pull-right">
+                    <a id="log-out" href="{{ route('logout') }}"
+                       class="btn btn-default btn-flat"
+                       onclick="e">Sign out</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{--                                {{ csrf_field() }}--}}
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </li>
 @else
-<li>
-    <a href="/login">LOGIN</a>
-</li>
-<li>
-    <a href="/register">REGISTER</a>
-</li>
+    <li>
+        <a href="/login">LOGIN</a>
+    </li>
+    <li>
+        <a href="/register">REGISTER</a>
+    </li>
 @endif
