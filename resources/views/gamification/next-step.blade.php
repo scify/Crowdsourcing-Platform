5b9a9248ef93baeb01e2d1463b5f6f283387531b
Paul Isaris <paulisaris@gmail.com>
2020-02-04 10:01:26 +0200

    Finished upgrading my dashboard page to Bootstrap 4 and AdminLTE 3

--- resources/views/gamification/next-step.blade.php
+++ resources/views/gamification/next-step.blade.php
@@ -2,20 +2,26 @@
     <link rel="stylesheet" href="{{ asset('dist/css/next-step.css') }}">
 @endpush
 
-<div class="row nextStepContainer">
-    <div class="col-md-12">
-        <h4 class="title">{!! $nextStepVM->title !!}</h4>
-        <div class="nextStepImgContainer">
-            <img class="nextStepImg" src="{{asset("images/badges/" . $nextStepVM->imgFileName)}}">
+<div class="container-fluid nextStepContainer h-100 p-2">
+    <div class="row justify-content-center align-self-center">
+
+        <div class="col-md-12">
+            <h4 class="title">{!! $nextStepVM->title !!}</h4>
+            <div class="nextStepImgContainer">
+                <img class="nextStepImg" src="{{asset("images/badges/" . $nextStepVM->imgFileName)}}">
+            </div>
+
+            @if($nextStepVM->userHasAlreadyAnsweredTheActiveQuestionnaire)
+
+                @include('questionnaire.social-share', ['viewModel' => $nextStepVM->socialShareVM])
+
+            @else
+
+                <a href="{{url("/" . $nextStepVM->project->slug . "?open=1")}}"
+                   class="btn btn-primary btn-lg nextStepActionBtn">Speak up</a>
+
+            @endif
         </div>
     </div>
-    @if($nextStepVM->userHasAlreadyAnsweredTheActiveQuestionnaire)
-        <div class="col-md-12">
-            @include('questionnaire.social-share', ['viewModel' => $nextStepVM->socialShareVM])
-        </div>
-    @else
-        <div class="col-md-12">
-            <a href="{{url("/" . $nextStepVM->project->slug . "?open=1")}}" class="btn btn-primary btn-lg nextStepActionBtn">Speak up</a>
-        </div>
-    @endif
 </div>
+
--- resources/views/questionnaire/social-share.blade.php
+++ resources/views/questionnaire/social-share.blade.php
@@ -2,35 +2,35 @@
     <link rel="stylesheet" href="{{ asset('dist/css/social-share.css') }}">
 @endpush
 
-<div class="row social-share">
-    <div class="col-md-12">
-        <div class="col-md-12">
-            <h4 class="title" style="margin-bottom: 20px">Share the Questionnaire:</h4>
-        </div>
-        <div class="col-lg-6">
-            <a target="_blank"
-               href="https://www.facebook.com/sharer/sharer.php?u={{ $viewModel->getSocialShareURL() }}"
-                    class="social-share-button fb-share-button facebook btn btn-lg btn-default">
-                <i class="fa fa-facebook" aria-hidden="true"></i>Facebook
-            </a>
-        </div>
-        <div class="col-lg-6">
-            <a target="_blank" href="https://twitter.com/share?url={{ $viewModel->getSocialShareURL() }}"
-               class="social-share-button twitter-share-button twitter btn btn-lg btn-default">
-                <i class="fa fa-twitter" aria-hidden="true"></i>Twitter
-            </a>
-        </div>
-    </div>
+<div class="container-fluid">
+    <div class="row social-share">
+            <div class="col-md-12">
+                <h4 class="title" style="margin-bottom: 20px">Share the Questionnaire:</h4>
+            </div>
+            <div class="col-lg-6">
+                <a target="_blank"
+                   href="https://www.facebook.com/sharer/sharer.php?u={{ $viewModel->getSocialShareURL() }}"
+                   class="social-share-button fb-share-button facebook btn btn-lg btn-default">
+                    <i class="fab fa-facebook-f"></i>Facebook
+                </a>
+            </div>
+            <div class="col-lg-6">
+                <a target="_blank" href="https://twitter.com/share?url={{ $viewModel->getSocialShareURL() }}"
+                   class="social-share-button twitter-share-button twitter btn btn-lg btn-default">
+                    <i class="fab fa-twitter"></i>Twitter
+                </a>
+            </div>
 
-    <div class="col-md-12 share-success hidden">
-        <div class="alert alert-success alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            Thank you for sharing the Questionnaire! <br>
-            You will soon receive an e-mail with your badge!
+        <div class="col-md-12 share-success d-none">
+            <div class="alert alert-success alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span>
+                </button>
+                Thank you for sharing the Questionnaire! <br>
+                You will soon receive an e-mail with your badge!
+            </div>
         </div>
     </div>
 </div>
-
 @push('scripts')
     <script src="{{ mix('dist/js/questionnaireSocialShare.js')}}?{{env("APP_VERSION")}}"></script>
 @endpush
