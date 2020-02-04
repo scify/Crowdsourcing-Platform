05672b2d7650d5a3a255c45f4c94774304bc03a6
Paul Isaris <paulisaris@gmail.com>
2020-02-04 16:24:22 +0200

    Modal close button

--- resources/views/my-history.blade.php
+++ resources/views/my-history.blade.php
@@ -65,9 +65,8 @@
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                                    aria-hidden="true">&times;</span></button>
                         <h3 class="modal-title" id="questionnaireTitle"></h3>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     </div>
                     <div class="modal-body">
                         <div id="questionnaireResponse"></div>
