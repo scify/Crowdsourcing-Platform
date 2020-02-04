688f2f0e9915d243c8ac7f907d806fd8a307974c
Paul Isaris <paulisaris@gmail.com>
2020-02-04 15:23:10 +0200

    Upgraded Home page to Bootstrap 4

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,16 +1,18 @@
 @extends('home.layout')
 
 @section('content')
-    <section id="motto">
-        @include('home.partials.' . config('app.project_resources_dir') . '.motto')
-    </section>
-    <section id="about">
-        @include('home.partials.' . config('app.project_resources_dir') . '.about-us')
-    </section>
-    <section id="features">
-        @include('home.partials.' . config('app.project_resources_dir') . '.features')
-    </section>
-    <section id="projects">
-        @include('home.partials.' . config('app.project_resources_dir') . '.projects')
-    </section>
+    <div class="row">
+        <section id="motto" class="w-100">
+            @include('home.partials.' . config('app.project_resources_dir') . '.motto')
+        </section>
+        <section id="about" class="w-100">
+            @include('home.partials.' . config('app.project_resources_dir') . '.about-us')
+        </section>
+        <section id="features" class="w-100">
+            @include('home.partials.' . config('app.project_resources_dir') . '.features')
+        </section>
+        <section id="projects" class="w-100">
+            @include('home.partials.' . config('app.project_resources_dir') . '.projects')
+        </section>
+    </div>
 @endsection
