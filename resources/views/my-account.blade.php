a652a40813680d453a3222f89de026b1c4008999
Paul Isaris <paulisaris@gmail.com>
2020-02-04 11:25:57 +0200

    Upgraded My account page to Bootstrap 4 and AdminLTE 3

--- resources/views/my-account.blade.php
+++ resources/views/my-account.blade.php
@@ -4,21 +4,16 @@
     <h1>My Account</h1>
 @stop
 
-@push('css')
-    <link rel="stylesheet" href="{{ asset('dist/css/my-dashboard.css') }}">
-@endpush
-
 @section('content')
-
     <div class="row">
         <div class="col-md-6 col-xs-12">
-            <div class="box box-info">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Personal Details</h3>
+            <div class="card card-info">
+                <div class="card-header with-border">
+                    <h3 class="card-title">Personal Details</h3>
                 </div>
-                <div class="box-body">
+                <div class="card-body">
                     <div class="row">
-                        <form id="form-change-password" role="form" method="POST" action="{{ url('/user/update') }}"
+                        <form id="form-change-password" class="w-100" role="form" method="POST" action="{{ url('/user/update') }}"
                               novalidate>
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
@@ -82,11 +77,11 @@
     </div>
     <div class="row">
         <div class="col-md-6 col-xs-12">
-            <div class="box box-info">
-                <div class="box-header with-border">
-                    <h3 class="box-title">My Data</h3>
+            <div class="card card-info">
+                <div class="card-header with-border">
+                    <h3 class="card-title">My Data</h3>
                 </div>
-                <div class="box-body">
+                <div class="card-body">
                     <div class="row">
                         <div class="col-md-3">
                             <a href="{{ route('downloadMyData') }}" data-widget="tooltip" title="This includes all your responses to questionnaires" target="_blank" class="btn btn-primary">Download my data</a>
@@ -98,14 +93,14 @@
     </div>
     <div class="row">
         <div class="col-md-6 col-xs-12">
-            <div class="box box-default">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Account Deactivation</h3>
+            <div class="card card-default">
+                <div class="card-header with-border">
+                    <h3 class="card-title">Account Deactivation</h3>
                 </div>
-                <div class="box-body">
+                <div class="card-body">
                     <div class="row">
                         <div class="col-md-3">
-                            <a href="#deactivationConfirmationModal" data-widget="modal">Deactivate my account</a>
+                            <a href="#deactivationConfirmationModal" data-toggle="modal">Deactivate my account</a>
                         </div>
                     </div>
                 </div>
@@ -113,30 +108,30 @@
         </div>
     </div>
 
-
-@stop
-
-<div id="deactivationConfirmationModal" class="modal fade">
-    <div class="modal-dialog" style="margin-top: 10%">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Are you sure?</h4>
-            </div>
-            <div class="modal-body">
-                <p class="text-danger"><b>Waring:</b> This action will delete your account.<br><br>You will lose  all your badges and will not be able to participate in the platform any more.</p>
-            </div>
-            <div class="modal-footer">
-                <form role="form" method="POST" action="{{ url('/user/deactivate') }}"
-                      novalidate>
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-danger">I understand, please deactivate my account</button>
-                </form>
+    <div id="deactivationConfirmationModal" class="modal fade">
+        <div class="modal-dialog" style="margin-top: 10%">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Are you sure?</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <p class="text-danger"><b>Waring:</b> This action will delete your account.<br><br>You will lose  all your badges and will not be able to participate in the platform any more.</p>
+                </div>
+                <div class="modal-footer">
+                    <form role="form" method="POST" action="{{ url('/user/deactivate') }}"
+                          novalidate>
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-danger">I understand, please deactivate my account</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+@stop
+
+
 
 @push('scripts')
     <script src="{{ mix('dist/js/myProfile.js')}}?{{env("APP_VERSION")}}"></script>
