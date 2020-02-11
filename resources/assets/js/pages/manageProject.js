ec422bccf221993725aa73701e1eafa2714df76b
Paul Isaris <paulisaris@gmail.com>
2020-02-11 12:34:45 +0200

    - Upgraded bootstrap-colorpicker to version 3 - Added bootstrap-colorpicker preview box that changes upon color picker change

--- resources/assets/js/pages/manageProject.js
+++ resources/assets/js/pages/manageProject.js
@@ -16,6 +16,11 @@ const Stepper = require('bs-stepper');
             $(obj).colorpicker({
                 horizontal: true
             });
+
+            $(obj).on('colorpickerChange', function(event) {
+                console.log(event.color.toString());
+                $(obj).find('.input-group-addon').css('background-color', event.color.toString());
+            });
         });
 
     };
