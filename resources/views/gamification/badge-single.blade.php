ea91e77d5a6e4979fe3140ff421f0b08c8f96c6b
Paul Isaris <paulisaris@gmail.com>
2020-02-18 15:11:19 +0200

    Fixed gamification badges layout & design for automatic emails

--- resources/views/gamification/badge-single.blade.php
+++ resources/views/gamification/badge-single.blade.php
@@ -8,15 +8,15 @@
              padding: 15px;
              border-radius: 10px;
              color: white; !important">
-    <div class="col-md-12 badgeImg">
-        <img class="badgeImg" src="{{asset("images/badges/" . $badge->badgeImageName)}}">
+    <div class="col-md-12 badgeImg" style="text-align: center; margin-bottom: 1rem;">
+        <img class="badgeImg" height="130px" src="{{asset("images/badges/" . $badge->badgeImageName)}}">
     </div>
     <div class="col-md-12 mt-2">
-        <h4 class="align-center badgeName" style="color: white; !important">{{ $badge->badgeName }}</h4>
+        <h4 class="badgeName" style="color: white; text-align: center; margin: 0.5rem 0;">{{ $badge->badgeName }}</h4>
         @if($badge->level)
-            <h6 class="align-center badgeLevel" style="color: white; !important">Level: <span
+            <h6 class="badgeLevel" style="color: white; text-align: center; margin: 0.5rem 0; ">Level: <span
                         class="points" style="font-weight: bold;">{{ $badge->level }}</span></h6>
-            <h5 class="align-center badgeMessage" style="color: white; !important">{{ $badge->badgeMessage }}</h5>
+            <h5 class="badgeMessage" style="color: white; text-align: center; margin: 0.5rem 0;">{{ $badge->badgeMessage }}</h5>
         @endif
     </div>
 </div>
