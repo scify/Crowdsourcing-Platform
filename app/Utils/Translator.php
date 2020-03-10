68bac41d7fd1333111234a835a5f1e5f53225abd
Paul Isaris <paulisaris@gmail.com>
2020-03-10 14:57:18 +0200

    Refactored tranlating service to use batch of 100 texts at a time

--- app/Utils/Translator.php
+++ app/Utils/Translator.php
@@ -1,17 +1,12 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: snik
- * Date: 6/6/18
- * Time: 4:52 PM
- */
 
 namespace App\Utils;
 
-use Google\Cloud\Translate\TranslateClient;
+use Google\Cloud\Translate\V2\TranslateClient;
 
 class Translator
 {
+    public const BATCH_SIZE = 100;
     /**
      * Translate texts to the preferred language.
      * @param array $texts   The texts that need translation
@@ -21,9 +16,15 @@ class Translator
     public static function translateTexts(array $texts, $to)
     {
         $translate = new TranslateClient(['key' => config('app.google_translate_key')]);
-        $result = $translate->translateBatch($texts, [
-            'target' => $to,
-        ]);
+        // Google translate capacity is 100 texts per request.
+        // So we need to break the texts into 100-texts batches
+        $batches = array_chunk($texts, self::BATCH_SIZE);
+        $result = [];
+        foreach ($batches as $batch) {
+            array_merge($result, $translate->translateBatch($batch, [
+                'target' => $to,
+            ]));
+        }
         return $result;
     }
 }
