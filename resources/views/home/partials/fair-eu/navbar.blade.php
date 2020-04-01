c650bb7837a80c4ce5ad059f79febf41b0439362
Paul Isaris <paulisaris@gmail.com>
2020-04-01 11:50:13 +0300

    - Fixed navbar expansion on larger screens - Fixed view composer for error pages

--- resources/views/home/partials/fair-eu/navbar.blade.php
+++ resources/views/home/partials/fair-eu/navbar.blade.php
@@ -1,18 +1,17 @@
-<nav class="main-header navbar navbar-expand navbar-white navbar-light fixed-top navbar-default navbar-fixed-top m-0">
+<nav class="main-header navbar navbar-expand-lg navbar-white navbar-light fixed-top navbar-default navbar-fixed-top m-0">
     @if (App::environment('staging'))
         <div class="staging-warning">
             <p>~~~WARING: STAGING ENVIRONMENT~~~</p>
         </div>
     @endif
-    <div class="container">
-        <div class="navbar-header">
+
             <a class="navbar-brand" href="#">
                 <img alt="ECAS" src="{{asset('images/projects/fair-eu/ecas_logo.png')}}">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top-menu-content" aria-controls="top-menu-content" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-        </div>
+
         <div class="collapse navbar-collapse pull-right" id="top-menu-content">
             <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item">
@@ -27,5 +26,4 @@
                 @include("partials.login-menu-options")
             </ul>
         </div>
-    </div>
 </nav>
--- resources/views/landingpages/landing-page.blade.php
+++ resources/views/landingpages/landing-page.blade.php
@@ -1,19 +1,21 @@
 @extends('landingpages.layout')
 
 @section('content')
-    <section>
-        @include('landingpages.partials.motto')
-    </section>
-    <section>
-        @include('landingpages.partials.about')
-    </section>
-    <section>
-        @include('landingpages.partials.questionnaire')
-    </section>
-
-    @if($viewModel->questionnaire)
-        <section id="collective-goal" style="background-color: {{ $viewModel->project->lp_questionnaire_goal_bg_color }}">
-            @include('landingpages.partials.goal-and-activity')
+    <div class="container-fluid">
+        <section>
+            @include('landingpages.partials.motto')
         </section>
-    @endif
+        <section>
+            @include('landingpages.partials.about')
+        </section>
+        <section>
+            @include('landingpages.partials.questionnaire')
+        </section>
+        @if($viewModel->questionnaire)
+            <section id="collective-goal"
+                     style="background-color: {{ $viewModel->project->lp_questionnaire_goal_bg_color }}">
+                @include('landingpages.partials.goal-and-activity')
+            </section>
+        @endif
+    </div>
 @endsection
