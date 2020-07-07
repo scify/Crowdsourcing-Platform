fe28e8288cbea3490328068222bcc0b672c719d6
Paul Isaris <paulisaris@gmail.com>
2020-07-07 15:52:04 +0300

    Staging banner

--- resources/views/home/partials/eicon/navbar.blade.php
+++ resources/views/home/partials/eicon/navbar.blade.php
@@ -1,35 +1,32 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
-    @if (App::environment('staging'))
-        <div class="staging-warning">
-            <p>~~~WARING: STAGING ENVIRONMENT~~~</p>
+<div @if (App::environment('staging')) class="header-margin-top" @endif>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img alt="ECAS" src="{{asset('images/projects/eicon/logo.png')}}">
+                </a>
+                <button type="button" class="navbar-toggle" data-widget="collapse"
+                        data-target="#top-menu-content">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse pull-right" id="top-menu-content">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#about">ABOUT US</a>
+                    </li>
+                    <li>
+                        <a href="#features">FEATURES</a>
+                    </li>
+                    <li>
+                        <a href="#projects">WHAT ARE WE CROWDSOURCING?</a>
+                    </li>
+                    @include("partials.login-menu-options")
+                </ul>
+            </div>
         </div>
-    @endif
-    <div class="container">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">
-                <img alt="ECAS" src="{{asset('images/projects/eicon/logo.png')}}">
-            </a>
-            <button type="button" class="navbar-toggle" data-widget="collapse"
-                    data-target="#top-menu-content">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
-        <div class="collapse navbar-collapse pull-right" id="top-menu-content">
-            <ul class="nav navbar-nav">
-                <li>
-                    <a href="#about">ABOUT US</a>
-                </li>
-                <li>
-                    <a href="#features">FEATURES</a>
-                </li>
-                <li>
-                    <a href="#projects">WHAT ARE WE CROWDSOURCING?</a>
-                </li>
-                @include("partials.login-menu-options")
-            </ul>
-        </div>
-    </div>
-</nav>
+    </nav>
+</div>
--- resources/views/landingpages/partials/navbar.blade.php
+++ resources/views/landingpages/partials/navbar.blade.php
@@ -1,16 +1,12 @@
-<nav class="main-header navbar navbar-expand-lg navbar-white navbar-light fixed-top navbar-default navbar-fixed-top m-0">
-    @if (App::environment('staging'))
-        <div class="staging-warning">
-            <p>~~~WARING: STAGING ENVIRONMENT~~~</p>
-        </div>
-    @endif
-
+<div @if (App::environment('staging')) class="header-margin-top" @endif>
+    <nav class="main-header navbar navbar-expand-lg navbar-white navbar-light fixed-top navbar-default navbar-fixed-top m-0">
             <a class="navbar-brand" href="#">
-                <img alt="{{$viewModel->project->name}}" src="{{asset($viewModel->project->logo_path)}}">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top-menu-content" aria-controls="top-menu-content" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <img alt="{{$viewModel->project->name}}" src="{{asset($viewModel->project->logo_path)}}">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top-menu-content"
+                aria-controls="top-menu-content" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
         <div class="collapse navbar-collapse pull-right" id="top-menu-content">
             <ul class="nav navbar-nav ml-auto">
@@ -20,4 +16,5 @@
                 @include("partials.login-menu-options")
             </ul>
         </div>
-</nav>
+    </nav>
+</div>
