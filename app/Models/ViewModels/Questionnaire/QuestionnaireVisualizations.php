eae1188ae20e3f1b2ddd1cd2998a0c35f2ca7988
Paul Isaris <paulisaris@gmail.com>
2020-07-08 10:58:42 +0300

    Added optional home url view model variable for navbar

--- app/Models/ViewModels/Questionnaire/QuestionnaireVisualizations.php
+++ app/Models/ViewModels/Questionnaire/QuestionnaireVisualizations.php
@@ -7,9 +7,11 @@ use App\Models\CrowdSourcingProject;
 
 class QuestionnaireVisualizations {
     public $project;
+    public $home_url;
 
     public function __construct(CrowdSourcingProject $project) {
         $this->project = $project;
+        $this->home_url = route('project.landing-page', $project->slug);
     }
 
 }
