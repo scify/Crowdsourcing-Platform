061d70dbaee097fe0e3458f57736def4a2d2fa6a
Paul Isaris <paulisaris@gmail.com>
2020-07-08 14:43:54 +0300

    Initialized first 2 mock charts

--- resources/views/questionnaire/visualizations.blade.php
+++ resources/views/questionnaire/visualizations.blade.php
@@ -4,7 +4,33 @@
 @endpush
 
 @section('content')
-    <div class="container-fluid">
+    <div class="container wide">
+        <div class="row my-5">
+            <div class="col-12">
+                <h1 class="text-lg-center text-md-center text-sm-left">Total Questionnaire Statistics:</h1>
+            </div>
+        </div>
+        <div class="row my-5 py-5 align-items-center bg-white">
+            <div class="col-lg-3 col-md-6 col-sm-12 offset-lg-1 offset-md-0 offset-sm-0 mb-4 mb-lg-0 mb-md-0">
+                <h2>Total Responses:</h2>
+            </div>
+            <div class="col-lg-7 col-md-6 col-sm-12">
+                <canvas id="responsesChart"></canvas>
+            </div>
+        </div>
+        <div class="row my-5 py-5 align-items-center bg-white">
+            <div class="col-lg-3 col-md-6 col-sm-12 offset-lg-1 offset-md-0 offset-sm-0 mb-4 mb-lg-0 mb-md-0">
+                <h2>Responses per language:</h2>
+            </div>
+            <div class="col-lg-7 col-md-6 col-sm-12">
+                <canvas id="responsesPerLanguageChart"></canvas>
+            </div>
+        </div>
+        <div class="row my-5">
+            <div class="col-12">
+                <h1 class="text-lg-center text-md-center text-sm-left">Statistics per question:</h1>
+            </div>
+        </div>
     </div>
 @endsection
 
