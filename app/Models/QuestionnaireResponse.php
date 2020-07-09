92930d72d63224130f966d3b35c01531961caf75
Paul Isaris <paulisaris@gmail.com>
2020-07-09 12:42:24 +0300

    - Added Language id to questionnaire responses table

--- app/Models/QuestionnaireResponse.php
+++ app/Models/QuestionnaireResponse.php
@@ -10,6 +10,7 @@ namespace App\Models;
 
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -19,23 +20,24 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property int $id
  * @property int $questionnaire_id
  * @property int $user_id
+ * @property int $language_id
  * @property string $response_json
- * @property \Carbon\Carbon|null $created_at
- * @property \Carbon\Carbon|null $updated_at
- * @property \Carbon\Carbon|null $deleted_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property Carbon|null $deleted_at
  * @property-read \App\Models\User $user
  * @method static bool|null forceDelete()
- * @method static \Illuminate\Database\Query\Builder|\App\Models\QuestionnaireResponse onlyTrashed()
+ * @method static \Illuminate\Database\Query\Builder|QuestionnaireResponse onlyTrashed()
  * @method static bool|null restore()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\QuestionnaireResponse whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\QuestionnaireResponse whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\QuestionnaireResponse whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\QuestionnaireResponse whereQuestionnaireId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\QuestionnaireResponse whereResponseJson($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\QuestionnaireResponse whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\QuestionnaireResponse whereUserId($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\QuestionnaireResponse withTrashed()
- * @method static \Illuminate\Database\Query\Builder|\App\Models\QuestionnaireResponse withoutTrashed()
+ * @method static Builder|QuestionnaireResponse whereCreatedAt($value)
+ * @method static Builder|QuestionnaireResponse whereDeletedAt($value)
+ * @method static Builder|QuestionnaireResponse whereId($value)
+ * @method static Builder|QuestionnaireResponse whereQuestionnaireId($value)
+ * @method static Builder|QuestionnaireResponse whereResponseJson($value)
+ * @method static Builder|QuestionnaireResponse whereUpdatedAt($value)
+ * @method static Builder|QuestionnaireResponse whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|QuestionnaireResponse withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|QuestionnaireResponse withoutTrashed()
  * @mixin \Eloquent
  */
 class QuestionnaireResponse extends Model
---
+++ database/migrations/2020_07_09_093125_add_language_id_to_questionnaire_responses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddLanguageIdToQuestionnaireResponsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questionnaire_responses', function (Blueprint $table) {
+            $table->unsignedInteger('language_id')->after('user_id')->nullable();
+            $table->foreign('language_id')->references('id')->on('languages_lkp');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questionnaire_responses', function (Blueprint $table) {
+            $table->dropForeign('questionnaire_responses_language_id_foreign');
+            $table->dropColumn('language_id');
+        });
+    }
+}
