99081231ae8b2366e558d9e368c552f8e2a0f9b7
Paul Isaris <paulisaris@gmail.com>
2020-07-09 12:28:31 +0300

    - Added timestamps in languages lookup table - Refactored Languages Lookup Seeder class to update or create - Added update or create method in Repository.php base class

--- database/seeds/LanguagesLkpTableSeeder.php
+++ database/seeds/LanguagesLkpTableSeeder.php
@@ -1,56 +1,60 @@
 <?php
 
+use App\Repository\LanguageRepository;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
-class LanguagesLkpTableSeeder extends Seeder
-{
-    // languages found here: https://en.wikipedia.org/wiki/Languages_of_the_European_Union
-    private static $LANGUAGES = [
-        'bg' => 'Bulgarian',
-        'hr' => 'Croatian',
-        'cs' => 'Czech',
-        'da' => 'Danish',
-        'nl' => 'Dutch',
-        'en' => 'English',
-        'et' => 'Estonian',
-        'fi' => 'Finnish',
-        'fr' => 'French',
-        'de' => 'German',
-        'el' => 'Greek',
-        'hu' => 'Hungarian',
-        'ga' => 'Irish',
-        'it' => 'Italian',
-        'lv' => 'Latvian',
-        'lt' => 'Lithuanian',
-        'pl' => 'Polish',
-        'pt' => 'Portuguese',
-        'ro' => 'Romanian',
-        'sk' => 'Slovak',
-        'sl' => 'Slovenian',
-        'es' => 'Spanish',
-        'sv' => 'Swedish',
-        'mt' => 'Maltese'
-    ];
+class LanguagesLkpTableSeeder extends Seeder {
+
+    protected $languagesRepository;
+
+    public function __construct(LanguageRepository $languagesRepository) {
+        $this->languagesRepository = $languagesRepository;
+    }
 
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        $languages = [];
+    public function run() {
+        // languages found here: https://en.wikipedia.org/wiki/Languages_of_the_European_Union
+        $languages = [
+            'bg' => 'Bulgarian',
+            'hr' => 'Croatian',
+            'cs' => 'Czech',
+            'da' => 'Danish',
+            'nl' => 'Dutch',
+            'en' => 'English',
+            'et' => 'Estonian',
+            'fi' => 'Finnish',
+            'fr' => 'French',
+            'de' => 'German',
+            'el' => 'Greek',
+            'hu' => 'Hungarian',
+            'ga' => 'Irish',
+            'it' => 'Italian',
+            'lv' => 'Latvian',
+            'lt' => 'Lithuanian',
+            'pl' => 'Polish',
+            'pt' => 'Portuguese',
+            'ro' => 'Romanian',
+            'sk' => 'Slovak',
+            'sl' => 'Slovenian',
+            'es' => 'Spanish',
+            'sv' => 'Swedish',
+            'mt' => 'Maltese',
+            'sq' => 'Albanian'
+        ];
         $idToInsert = 1;
-        foreach(self::$LANGUAGES as $code => $name) {
-            array_push($languages, [
-                'id' => $idToInsert,
-                'language_code' => $code,
-                'language_name' => $name,
-            ]);
+        foreach ($languages as $code => $name) {
+            $this->languagesRepository->updateOrCreate(['id' => $idToInsert],
+                [
+                    'id' => $idToInsert,
+                    'language_code' => $code,
+                    'language_name' => $name,
+                ]
+            );
             $idToInsert++;
         }
-        DB::table('languages_lkp')->delete();
-        DB::table('languages_lkp')->insert($languages);
     }
 }
