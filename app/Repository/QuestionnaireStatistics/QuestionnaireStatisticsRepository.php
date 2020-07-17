77d7b08a8e9cbdadaf7238ad625579326a436691
xstrtok <sweepy1982@gmail.com>
2020-07-17 12:33:10 +0300

    QuestionnaireStatisticsRepository update (see desc)
    - fix db query for radiogroup/checkbox questions
    - simplify db query on getStatisticsForFixedChoicesQuestion()
    - add title property to dummy data

--- app/Repository/QuestionnaireStatistics/QuestionnaireStatisticsRepository.php
+++ app/Repository/QuestionnaireStatistics/QuestionnaireStatisticsRepository.php
@@ -44,55 +44,40 @@ class QuestionnaireStatisticsRepository {
 
     // return both fixed and freetext
     public function getStatisticsPerQuestion($questionnaire) {
-        $questionIDsForQuestionnaire = DB::select('select id, type from questionnaire_questions
+        $questionIDsForQuestionnaire = DB::select('select id, type, question as title from questionnaire_questions
                                                  where questionnaire_id = ' . $questionnaire->id .
                                                  ' and deleted_at is null');
         $statisticsPerQuestionnaire = array();
-        foreach ($questionIDsForQuestionnaire as $key=>$question) {
+        foreach ($questionIDsForQuestionnaire as $question) {
             // separate for each type
+            $questionType = '';
             if ($question->type == 'radiogroup' || $question->type == 'checkbox') {
-                $fixedTypeStatistics = $this->getStatisticsForFixedChoicesQuestion($question->id);
-                $statisticsPerQuestionnaire[$key] = [
-                    'question_id' => $question->id,
-                    'question_title' => $fixedTypeStatistics[0]['title'],
-                    'question_type' => 'fixed_choices',
-                    'statistics' => array()
-                ];
-                // remove title/deleted_at and push to statistics for each answer
-                foreach ($fixedTypeStatistics as $key2=>$answers) {
-                    unset($answers['title']);
-                    unset($answers['deleted_at']);
-                    array_push($statisticsPerQuestionnaire[$key]['statistics'], $answers);
-                }
+                $questionType = 'fixed_choices';
+                $questionStatistics = $this->getStatisticsForFixedChoicesQuestion($question->id);
             } elseif ($question->type == 'text' || $question->type == 'comment') {
-                $freeTextTypeStatistics = $this->getStatisticsForFreeTextQuestion($question->id);
-                $statisticsPerQuestionnaire[$key] = [
+                $questionType = 'free_text';
+                $questionStatistics = $this->getStatisticsForFreeTextQuestion($question->id);
+            }
+            if (strlen($questionType) > 0) {
+                $statisticsPerQuestionnaire[] = [
                     'question_id' => $question->id,
-                    'question_title' => $fixedTypeStatistics[0]['title'],
-                    'question_type' => 'free_text',
-                    'statistics' => array()
+                    'question_title' => $questionStatistics[0]['title'],
+                    'question_type' => $questionType,
+                    'statistics' => $questionStatistics
                 ];
-                foreach ($freeTextTypeStatistics as $key2=>$answers) {
-                    array_push($statisticsPerQuestionnaire[$key]['statistics'], $answers);
-                }
             }
         }
 
-        // dd($statisticsPerQuestionnaire);
         return $statisticsPerQuestionnaire;
     }
 
     private function getStatisticsForFixedChoicesQuestion(int $questionId) {
         $query = DB::select('
-            select question as title, qpa.answer as answer_title, count(*) as num_responses, fixedchoices.deleted_at from
-                   (select question, id, deleted_at, answer_id from
-                           (select qra.question_id , qq.question, qq.id, qra.deleted_at, qra.answer_id from questionnaire_response_answers as qra
-                           inner join questionnaire_questions as qq on qra.question_id = qq.id
-                           order by qq.question)
-                    as qu)
-            as fixedchoices
-            inner join questionnaire_possible_answers as qpa on answer_id = qpa.id
-            where fixedchoices.id = ' . $questionId . ' and fixedchoices.deleted_at is null group by question, qpa.answer, fixedchoices.deleted_at;
+            select question as title, qpa.answer as answer_title, count(*) as num_responses from questionnaire_response_answers as qra
+                inner join questionnaire_questions as qq on qra.question_id = qq.id
+                inner join questionnaire_possible_answers as qpa on answer_id = qpa.id
+            where qq.id = ' . $questionId . ' and qra.deleted_at is null
+            group by question, qpa.answer
         ');
         $query = collect($query)->map(function($x) { return (array) $x;} )->toArray();
 
@@ -103,6 +88,7 @@ class QuestionnaireStatisticsRepository {
         // TODO
         $data = [
             [
+                'title' => 'Title free text',
                 'answer_text' => 'answer 5 (google translate)',
                 'is_translated' => true,
                 'answer_original_text' => 'answer 3',
@@ -112,6 +98,7 @@ class QuestionnaireStatisticsRepository {
                 ]
             ],
             [
+                'title' => 'Title free text',
                 'answer_text' => 'answer 6',
                 'is_translated' => false,
                 'answer_original_text' => 'answer 6',
@@ -121,6 +108,7 @@ class QuestionnaireStatisticsRepository {
                 ]
             ],
             [
+                'title' => 'Title free text',
                 'answer_text' => 'answer 6 French answer translated',
                 'is_translated' => true,
                 'answer_original_text' => 'answer 6 French answer',
