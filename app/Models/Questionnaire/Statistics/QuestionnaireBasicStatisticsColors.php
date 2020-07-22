7341c57fbeb525b1d19fb412395373b98ec915b3
Paul Isaris <paulisaris@gmail.com>
2020-07-22 14:28:24 +0300

    Added models and relationships for questionnaire statistics colors

---
+++ app/Models/Questionnaire/Statistics/QuestionnaireBasicStatisticsColors.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\Questionnaire\Statistics;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class QuestionnaireStatisticsBasicColors
+ * @package App\Models\Questionnaire\Statistics
+ * @property int $id
+ * @property int $questionnaire_id
+ * @property string $total_responses_color
+ * @property string $goal_responses_color
+ */
+class QuestionnaireBasicStatisticsColors extends Model
+{
+    protected $table = 'questionnaire_basic_statistics_colors';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'questionnaire_id', 'total_responses_color', 'goal_responses_color'
+    ];
+}
