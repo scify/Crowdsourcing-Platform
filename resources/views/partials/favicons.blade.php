fea0271dd71837df7e41b5081d059fb453001b5c
Paul Isaris <paulisaris@gmail.com>
2020-12-01 17:59:07 +0200

    Updated .env variable name

--- resources/views/partials/favicons.blade.php
+++ resources/views/partials/favicons.blade.php
@@ -1,27 +1,27 @@
-<link rel="shortcut icon" type="image/png" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/favicon.ico') }}"/>
-<link rel="shortcut icon" type="image/png" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/favicon_76.png') }}"/>
-<link rel="shortcut icon" type="image/png" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/favicon_57.png') }}"/>
+<link rel="shortcut icon" type="image/png" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/favicon.ico') }}"/>
+<link rel="shortcut icon" type="image/png" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/favicon_76.png') }}"/>
+<link rel="shortcut icon" type="image/png" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/favicon_57.png') }}"/>
 <link rel="apple-touch-icon-precomposed" sizes="76x76"
-      href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/favicon_76.png') }}">
+      href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/favicon_76.png') }}">
 <link rel="apple-touch-icon-precomposed" sizes="120x120"
-      href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/favicon_120.png') }}">
+      href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/favicon_120.png') }}">
 <link rel="apple-touch-icon-precomposed" sizes="114x114"
-      href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/favicon_114.png') }}">
+      href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/favicon_114.png') }}">
 
-<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/apple-icon-57x57.png') }}">
-<link rel="apple-touch-icon" sizes="60x60" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/apple-icon-60x60.png') }}">
-<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/apple-icon-72x72.png') }}">
-<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/apple-icon-76x76.png') }}">
-<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/apple-icon-114x114.png') }}">
-<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/apple-icon-120x120.png') }}">
-<link rel="apple-touch-icon" sizes="144x144" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/apple-icon-144x144.png') }}">
-<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/apple-icon-152x152.png') }}">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/apple-icon-180x180.png') }}">
-<link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/android-icon-192x192.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/favicon-96x96.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/favicon-16x16.png') }}">
-<link rel="manifest" href="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/manifest.json')}}">
+<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/apple-icon-57x57.png') }}">
+<link rel="apple-touch-icon" sizes="60x60" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/apple-icon-60x60.png') }}">
+<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/apple-icon-72x72.png') }}">
+<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/apple-icon-76x76.png') }}">
+<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/apple-icon-114x114.png') }}">
+<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/apple-icon-120x120.png') }}">
+<link rel="apple-touch-icon" sizes="144x144" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/apple-icon-144x144.png') }}">
+<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/apple-icon-152x152.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/apple-icon-180x180.png') }}">
+<link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/android-icon-192x192.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/favicon-96x96.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/favicon-16x16.png') }}">
+<link rel="manifest" href="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/manifest.json')}}">
 <meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="{{ asset('images/projects/' . config('app.project_resources_dir') . '/favicon/ms-icon-144x144.png') }}">
+<meta name="msapplication-TileImage" content="{{ asset('images/projects/' . config('app.installation_resources_dir') . '/favicon/ms-icon-144x144.png') }}">
 <meta name="theme-color" content="#ffffff">
