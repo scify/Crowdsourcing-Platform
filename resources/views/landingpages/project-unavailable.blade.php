10dad91daf2874b2a69dce8309a48a584bf47a0e
Paul Isaris <paulisaris@gmail.com>
2020-12-02 09:15:57 +0200

    Button

--- resources/views/landingpages/project-unavailable.blade.php
+++ resources/views/landingpages/project-unavailable.blade.php
@@ -13,13 +13,6 @@
                             {!! $viewModel->getProjectStatusMessage() !!}
                         </h1>
                         <div class="row">
-                            <div class="col-lg-2 col-md-4 col-sm-10 call-to-action mx-auto">
-                                <a href="https://ecas.org/" target="_blank" class="btn btn-primary"
-                                   style="color: {{ $viewModel->project->lp_questionnaire_btn_color }};
-                                           background-color: {{ $viewModel->project->lp_questionnaire_btn_bg_color }};">
-                                    Visit ECAS website
-                                </a>
-                            </div>
                             @if($viewModel->project->external_url)
                                 <div class="col-lg-2 col-md-4 col-sm-10 call-to-action mx-auto">
                                     <a href="{{ $viewModel->project->external_url }}" target="_blank" class="btn btn-primary"
