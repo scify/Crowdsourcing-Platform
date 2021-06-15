fcb0e81144eea3717361d4d152b96193ab84e2cf
Paul Isaris <paulisaris@gmail.com>
2021-06-15 14:15:47 +0300

    Reformatting

--- resources/views/admin/edit-user.blade.php
+++ resources/views/admin/edit-user.blade.php
@@ -17,7 +17,8 @@
                     <div class="card-body">
                         <div class="form-group has-feedback">
                             <label>Email</label>
-                            <input id="email" type="text" class="form-control" name="email" value="{{ $viewModel->user->email }}"
+                            <input id="email" type="text" class="form-control" name="email"
+                                   value="{{ $viewModel->user->email }}"
                                    required
                                    autofocus placeholder="Email" readonly>
                             @if ($errors->has('email'))
@@ -29,7 +30,8 @@
 
                         <div class="form-group has-feedback">
                             <label>Nickname</label>
-                            <input id="nickname" type="text" class="form-control" name="nickname" value="{{ $viewModel->user->nickname  }}"
+                            <input id="nickname" type="text" class="form-control" name="nickname"
+                                   value="{{ $viewModel->user->nickname  }}"
                                    required
                                    autofocus
                                    placeholder="Nickname" readonly>
@@ -47,7 +49,7 @@
                                 @foreach ($viewModel->allRoles as $role)
                                     <option
                                             @if ($viewModel->userRoleIds->contains($role->id))
-                                                selected
+                                            selected
                                             @endif
                                             value="{{ $role->id }}" name="roleVal[{{ $role->id }}]"
                                     >
