dbf63065b07914cbfa2dc476ae09f35df85cc744
Paul Isaris <paulisaris@gmail.com>
2021-06-24 16:17:46 +0300

    Correct file permissions script

---
+++ set-file-permissions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Default variables
+groupname="${1:-www-data}"
+username="${2:-$USER}"
+folder="${3:-.}"
+
+# Add user to webserver group
+usermod -a -G "$groupname" "$username"
+
+# Change storage ownership to normal user/ webserver group
+chown -R "$username":"$groupname" "$folder"/storage
+
+chmod 775 "$folder"/storage
+
+# Update permissions for all storage files
+find "$folder"/storage -type f -exec chmod 664 {} \;
+
+# Update permissions for all storage folders
+find "$folder"/storage -type d -exec chmod 775 {} \;
+
+# Change owner group to webserver group user for storage and bootstrap cache folder
+chgrp -R "$groupname" "$folder"/storage "$folder"/bootstrap/cache
