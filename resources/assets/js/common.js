7229eac241519d30c96bb6fd63a101e47aa33c37
Paul Isaris <paulisaris@gmail.com>
2021-07-07 15:58:09 +0300

    - Moved common method - Deleted unused file

--- resources/assets/js/common.js
+++ resources/assets/js/common.js
@@ -128,3 +128,8 @@ const app = new Vue({
     });
 })();
 
+export function arrayMove(arr, fromIndex, toIndex) {
+    const element = arr[fromIndex];
+    arr.splice(fromIndex, 1);
+    arr.splice(toIndex, 0, element);
+}
