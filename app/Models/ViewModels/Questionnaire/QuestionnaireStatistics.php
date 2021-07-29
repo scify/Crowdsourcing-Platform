7d10d6e7b21664f2ba77f63d3b93c153f241e851
Paul Isaris <paulisaris@gmail.com>
2021-07-29 10:28:32 +0300

    Removed project variable from questionnaire statistics page

--- app/Models/ViewModels/Questionnaire/QuestionnaireStatistics.php
+++ app/Models/ViewModels/Questionnaire/QuestionnaireStatistics.php
@@ -3,28 +3,22 @@
 
 namespace App\Models\ViewModels\Questionnaire;
 
-use App\Models\CrowdSourcingProject\CrowdSourcingProject;
 use App\Models\Questionnaire\Questionnaire;
 use App\Repository\Questionnaire\Statistics\QuestionnaireResponsesPerLanguage;
 use App\Repository\Questionnaire\Statistics\QuestionnaireResponseStatistics;
 
 class QuestionnaireStatistics {
 
-    public $project;
-    public $home_url;
     public $questionnaire;
     public $userCanPrintStatistics;
     public $questionnaireResponseStatistics;
     public $numberOfResponsesPerLanguage;
 
     public function __construct(Questionnaire $questionnaire,
-                                CrowdSourcingProject $project,
                                 QuestionnaireResponseStatistics $questionnaireResponseStatistics,
                                 QuestionnaireResponsesPerLanguage $numberOfResponsesPerLanguage,
                                 $userCanPrintStatistics) {
         $this->questionnaire = $questionnaire;
-        $this->project = $project;
-        $this->home_url = route('project.landing-page', $project->slug);
         $this->userCanPrintStatistics = $userCanPrintStatistics;
         $this->questionnaireResponseStatistics = $questionnaireResponseStatistics;
         $this->numberOfResponsesPerLanguage = $numberOfResponsesPerLanguage;
