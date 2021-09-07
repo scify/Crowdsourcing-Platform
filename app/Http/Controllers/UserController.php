eb5a92f04560387586155cf81dd505147b6a6730
Paul Isaris <paulisaris@gmail.com>
2021-09-07 15:44:19 +0300

    Removed debug line

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,35 +10,29 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
 
-class UserController extends Controller
-{
+class UserController extends Controller {
     private $userManager;
     private $questionnaireResponseManager;
     protected $userDashboardManager;
 
-    public function __construct(UserManager $userManager,
+    public function __construct(UserManager                  $userManager,
                                 QuestionnaireResponseManager $questionnaireResponseManager,
-                                UserDashboardManager $userDashboardManager)
-    {
+                                UserDashboardManager         $userDashboardManager) {
         $this->userManager = $userManager;
         $this->questionnaireResponseManager = $questionnaireResponseManager;
         $this->userDashboardManager = $userDashboardManager;
     }
 
-    public function home()
-    {
+    public function home() {
         return redirect('/my-dashboard');
     }
 
-    public function myDashboard()
-    {
-        dd("123");
+    public function myDashboard() {
         $dashboardViewModel = $this->userDashboardManager->getUserDashboardViewModel(Auth::user());
         return view('loggedin-environment.my-dashboard', ['viewModel' => $dashboardViewModel]);
     }
 
-    public function myAccount()
-    {
+    public function myAccount() {
         $userViewModel = $this->userManager->getUserProfile(Auth::user());
         return view('loggedin-environment.my-account', ['viewModel' => $userViewModel]);
     }
@@ -56,8 +50,7 @@ class UserController extends Controller
         return back();
     }
 
-    public function delete(Request $request)
-    {
+    public function delete(Request $request) {
         $this->userManager->deactivateUser($request->id);
         session()->flash('flash_message_success', 'User deleted.');
         return back();
@@ -69,23 +62,21 @@ class UserController extends Controller
         return redirect()->route('home');
     }
 
-    public function restore(Request $request)
-    {
+    public function restore(Request $request) {
         $this->userManager->reactivateUser($request->id);
         session()->flash('flash_message_success', 'User restored.');
         return back();
     }
 
-    public function showUsersByCriteria(Request $request)
-    {
+    public function showUsersByCriteria(Request $request) {
         $input = $request->all();
         $users = $this->userManager->getPlatformAdminUsersWithCriteria(UserManager::$USERS_PER_PAGE, $input);
         $users->setPath('#');
         if ($users->count() == 0) {
             $errorMessage = "No Users found";
-            return json_encode(new OperationResponse(config('app.OPERATION_FAIL'), (String)view('partials.ajax_error_message', compact('errorMessage'))));
+            return json_encode(new OperationResponse(config('app.OPERATION_FAIL'), (string)view('partials.ajax_error_message', compact('errorMessage'))));
         } else {
-            return json_encode(new OperationResponse(config('app.OPERATION_SUCCESS'), (String)view('admin.partials.users-list', compact('users'))));
+            return json_encode(new OperationResponse(config('app.OPERATION_SUCCESS'), (string)view('admin.partials.users-list', compact('users'))));
         }
     }
 
@@ -97,7 +88,7 @@ class UserController extends Controller
     public function downloadUserData() {
         $headers = array(
             "Content-type" => "text/csv",
-            "Content-Disposition" => "attachment; filename=file" . time() .".csv",
+            "Content-Disposition" => "attachment; filename=file" . time() . ".csv",
             "Pragma" => "no-cache",
             "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
             "Expires" => "0"
@@ -106,12 +97,11 @@ class UserController extends Controller
         $responses = $this->questionnaireResponseManager->getQuestionnaireResponsesForUser(Auth::user());
         $columns = array('Project name', 'Questionnaire title', 'Questionnaire description', 'Questionnaire JSON', 'Response JSON');
 
-        $callback = function() use ($responses, $columns)
-        {
+        $callback = function () use ($responses, $columns) {
             $file = fopen('php://output', 'w');
             fputcsv($file, $columns);
 
-            foreach($responses as $response) {
+            foreach ($responses as $response) {
                 fputcsv($file, array($response->name, $response->title, $response->questionnaire_description, $response->questionnaire_json, $response->response_json));
             }
             fclose($file);
