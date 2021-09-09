bee75ac1783402d1d03f4b5d807d1946bcb8abd5
Paul Isaris <paulisaris@gmail.com>
2021-09-09 14:54:27 +0300

    CEA-49 Added Google Authentication

--- config/services.php
+++ config/services.php
@@ -48,4 +48,10 @@ return [
         'redirect' => config('app.url') . '/login/social/twitter/callback',
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => config('app.url') . '/login/social/google/callback',
+    ],
+
 ];
