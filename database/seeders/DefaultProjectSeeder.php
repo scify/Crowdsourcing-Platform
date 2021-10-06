7340d1c64e8a8b655aa03445f64ea1a5ac3b1c48
Paul Isaris <paulisaris@gmail.com>
2021-10-06 14:54:20 +0300

    - Refactored application to have ECAS as an active installation - Removed EICON from default projects and application installation

--- database/seeders/DefaultProjectSeeder.php
+++ database/seeders/DefaultProjectSeeder.php
@@ -3,21 +3,26 @@
 namespace Database\Seeders;
 
 use App\BusinessLogicLayer\lkp\CrowdSourcingProjectStatusLkp;
+use App\Repository\CrowdSourcingProjectRepository;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
-class DefaultProjectSeeder extends Seeder
-{
+class DefaultProjectSeeder extends Seeder {
+
+    protected $projectRepository;
+
+    public function __construct(CrowdSourcingProjectRepository $crowdSourcingProjectRepository) {
+        $this->projectRepository = $crowdSourcingProjectRepository;
+    }
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        DB::table('crowd_sourcing_projects')->delete();
-        DB::table('crowd_sourcing_projects')->insert(array(
-            array('id' => 1,
+    public function run() {
+        $data = [
+            [
+                'id' => 1,
                 'name' => 'Crowdsourcing Demo Project',
                 'slug' => 'crowdsourcing-demo',
                 'logo_path' => '/images/projects/demo/logo.png',
@@ -42,14 +47,13 @@ class DefaultProjectSeeder extends Seeder
                 'user_creator_id' => 1,
                 'language_id' => 6,
                 'status_id' => CrowdSourcingProjectStatusLkp::DRAFT
-            ),
-        ));
-        DB::table('crowd_sourcing_projects')->insert(array(
-            array('id' => 2,
+            ],
+            [
+                'id' => 2,
                 'name' => 'FAIR EU',
                 'slug' => 'fair-eu',
-                'logo_path' => '/images/projects/fair-eu/fair-eu.png',
-                'img_path' => '/images/projects/fair-eu/fair-eu-bg.png',
+                'logo_path' => '/images/projects/ecas/fair-eu.png',
+                'img_path' => '/images/projects/ecas/fair-eu-bg.png',
                 'motto' => 'Please share with us your opinion on obstacles to free movement and <br>political participation in the EU. Your voice matters!',
                 'description' => '',
                 'about' => '<p>The FAIR EU (Fostering Awareness, Inclusion and Recognition of EU Mobile Citizens’ Political Rights)
@@ -72,41 +76,14 @@ class DefaultProjectSeeder extends Seeder
             </p>',
                 'user_creator_id' => 1,
                 'language_id' => 6,
-                'status_id' => CrowdSourcingProjectStatusLkp::DRAFT
-            ),
-        ));
-        DB::table('crowd_sourcing_projects')->insert(array(
-            array('id' => 3,
-                'name' => 'EICON Public Consultation',
-                'slug' => 'eicon',
-                'logo_path' => '/images/projects/eicon/logo.png',
-                'img_path' => '/images/projects/eicon/active_participation.png',
-                'motto' => 'The future is being created now. By us all.<br>So let’s use human-centred tech to make education more inclusive We are planning with teachers, and not for them',
-                'description' => '',
-                'about' => '<p>EICON’s main objective is to support organisations / institutions providing Vocational Education and Training (VET) 
-                            to become more inclusive. In particular, the aim is to put the organisations as a whole in the focus, and not just education and training practice. 
-                            Managers of these organisations / institutions are particularly in need for guidance on how to further develop their 
-                            organizations, as they often have to work towards multiple aims simultaneously, i.e. inclusion usually is one among other aims. 
-                            The typical organizational response to address inclusion is to provide teaching personnel with further qualification and training.
-                             <a href="http://www.eicon-project.eu/about.html">Learn more about our project.</a></p>',
-                'footer' => '<p style="font-size: 12px;">Copyright © EICON project 2019-2021&nbsp;|&nbsp;
-                <a href="http://www.eicon-project.eu/terms-of-use/" target="_blank" title="Read more">Terms of use</a>&nbsp;|&nbsp;
-                <a href="http://www.eicon-project.eu/privacy-policy/" target="_blank" title="Read more">Privacy Policy</a>&nbsp;|&nbsp;
-                <a href="http://www.eicon-project.eu/cookie-policy/" target="_blank" title="Read more">Cookie Policy</a>
-            </p>
-            <p style="font-size:13px; line-height:20px;"><img
-                        src="http://www.eicon-project.eu/img/erasmus.png" alt=""
-                        style="width:120px; float:left; margin:5px 30px 0 0;">
-                Project Co-funded by the JUSTICE, EQUALITY AND CITIZENSHIP PROGRAMME (2014-2020) OF THE EUROPEAN
-                UNION.<br>
-                The content of this website represents the views of the author only and is his/her sole responsibility.
-                The European Commission does not accept any responsibility for use that may be made of the information
-                it contains.
-            </p>',
-                'user_creator_id' => 1,
-                'language_id' => 6,
-                'status_id' => CrowdSourcingProjectStatusLkp::DRAFT
-            ),
-        ));
+                'status_id' => CrowdSourcingProjectStatusLkp::PUBLISHED
+            ]
+        ];
+
+        foreach ($data as $project) {
+            $project = $this->projectRepository->updateOrCreate(['id' => $project['id']],
+                $project);
+            echo "\nAdded Project: " . $project->name . " with slug: " . $project->slug . "\n";
+        }
     }
 }
