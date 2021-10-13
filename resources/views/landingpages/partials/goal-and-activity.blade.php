1b0f850cdd4d4236488ff47a49b41398f700f344
Paul Isaris <paulisaris@gmail.com>
2021-10-13 10:09:15 +0300

    Showing questionnaire contributors

--- resources/views/landingpages/partials/goal-and-activity.blade.php
+++ resources/views/landingpages/partials/goal-and-activity.blade.php
@@ -13,38 +13,33 @@
     </div>
 
     <div class="row activity-container wrapper-box">
-        @can("manage-platform")
-            <div class="col-md-6 col-sm-12 text-center">
-                @if ($viewModel->totalResponses ==0)
-                    <p class="no-activity-found-msg"
-                       style="color: {{ $viewModel->project->lp_questionnaire_goal_color }}">
-                        No recent activity found
-                    </p>
-                @elseif ($viewModel->totalResponses > 0)
-                    <div class="activity-title wrapper-title">
-                        <p style="color: {{ $viewModel->project->lp_questionnaire_goal_color }}">Latest contributors</p>
-                    </div>
-                    <div class="activity-content">
-                        @foreach($viewModel->allResponses as $response)
-                            @if($response->user)
-                                <div class="activity-item text-left">
-                                    <i class="fa fa-user-circle user-icon" aria-hidden="true"></i>
-                                    {{$response->user->nickname}}
-                                    @if($response->created_at)
-                                        responded at {{$response->created_at->toDayDateTimeString()}}
-                                    @endif
-                                </div>
-                            @endif
-                        @endforeach
-                    </div>
-                @endif
+        <div class="col-md-6 col-sm-12 text-center">
+            @if ($viewModel->totalResponses ==0)
+                <p class="no-activity-found-msg"
+                   style="color: {{ $viewModel->project->lp_questionnaire_goal_color }}">
+                    No recent activity found
+                </p>
+            @elseif ($viewModel->totalResponses > 0)
+                <div class="activity-title wrapper-title">
+                    <p style="color: {{ $viewModel->project->lp_questionnaire_goal_color }}">Latest contributors</p>
+                </div>
+                <div class="activity-content">
+                    @foreach($viewModel->allResponses as $response)
+                        @if($response->user)
+                            <div class="activity-item text-left">
+                                <i class="fa fa-user-circle user-icon" aria-hidden="true"></i>
+                                {{$response->user->nickname}}
+                                @if($response->created_at)
+                                    responded at {{$response->created_at->toDayDateTimeString()}}
+                                @endif
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+            @endif
 
-            </div>
-        @endcan
-        <?php $class = 'center' ?>
-        @can("manage-platform")
-            <?php $class = '' ?>
-        @endcan
+        </div>
+
         @if($viewModel->questionnaireGoalVM)
             <div class="col-md-6 col-sm-12 text-center {{ $class }}">
                 @include('landingpages.partials.project-goal', ['viewModel' => $viewModel->questionnaireGoalVM, 'projectId' => $viewModel->project->id])
