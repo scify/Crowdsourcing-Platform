80dbe8eab3c6445e525357c2a7277d579c6bfdd6
Paul Isaris <paulisaris@gmail.com>
2021-10-14 16:18:28 +0300

    Fixed bug on v bind

--- resources/views/admin/projects/create-edit/partials/statistics-colors.blade.php
+++ resources/views/admin/projects/create-edit/partials/statistics-colors.blade.php
@@ -6,7 +6,6 @@
                     <div class="row">
                         <div class="col">
                             <crowd-sourcing-project-colors
-                                    :project-id="{{ $viewModel->project->id }}"
                                     :color-data='@json( $viewModel->project->colors)'
                             ></crowd-sourcing-project-colors>
                         </div>
