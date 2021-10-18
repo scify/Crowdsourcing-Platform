8defbdf21ef7eace084973485a56a781eeb4b2b7
Paul Isaris <paulisaris@gmail.com>
2021-10-18 16:55:41 +0300

    Class name

--- resources/views/home/partials/together/motto.blade.php
+++ resources/views/home/partials/together/motto.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid h-100 w-100 px-0">
     <div class="row h-100 w-100 align-items-center mx-0"
          style="background-image: url({{asset('images/active_participation.png')}});">
-        <div class="gray-filter" style="top: @if (App::environment('staging')) 128.75px @else 93.75px @endif"></div>
+        <div class="overlay-filter" style="top: @if (App::environment('staging')) 128.75px @else 93.75px @endif"></div>
         <div class="col-lg-5 col-md-6 col-sm-11 mx-auto motto-content px-0">
             <div class="frosted"></div>
             <div id="project-motto">
