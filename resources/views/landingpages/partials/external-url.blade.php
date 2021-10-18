2299f1eabe792da0bad589fe7daa90e112d87073
Paul Isaris <paulisaris@gmail.com>
2021-10-18 16:28:36 +0300

    - External URL button is shown only if the questionnaire has been answered or its not available - Overlay background color in motto partial is now variable

--- resources/views/landingpages/partials/external-url.blade.php
+++ resources/views/landingpages/partials/external-url.blade.php
@@ -1,5 +1,9 @@
-<a href="{{$viewModel->project->external_url}}" target="_blank" class="btn btn-primary w-100"
-   style="color: {{ $viewModel->project->lp_external_url_btn_color }};
-           background-color: {{ $viewModel->project->lp_external_url_btn_bg_color }};">
-    Project Webpage
-</a>
+@if($viewModel->project->external_url)
+    <div class="col-5 mx-auto call-to-action">
+        <a href="{{$viewModel->project->external_url}}" target="_blank" class="btn btn-primary w-100"
+           style="color: {{ $viewModel->project->lp_external_url_btn_color }};
+                   background-color: {{ $viewModel->project->lp_external_url_btn_bg_color }};">
+            Project Webpage
+        </a>
+    </div>
+@endif
