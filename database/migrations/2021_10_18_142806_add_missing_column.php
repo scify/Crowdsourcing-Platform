b1bebb397b8b3ffc8e87c9992873226c31762ad8
Paul Isaris <paulisaris@gmail.com>
2021-10-18 14:33:24 +0300

    Added migration for missing column

---
+++ database/migrations/2021_10_18_142806_add_missing_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMissingColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('crowd_sourcing_projects', function (Blueprint $table) {
+            $table->string('lp_external_url_btn_bg_color')->nullable()->default('transparent')->after('lp_external_url_btn_color');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('crowd_sourcing_projects', function (Blueprint $table) {
+            $table->dropColumn(['lp_external_url_btn_bg_color']);
+        });
+    }
+}
