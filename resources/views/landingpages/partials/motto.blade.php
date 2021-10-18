12fef8d9faf5fe5080d236f10b029e5102a3eaed
Paul Isaris <paulisaris@gmail.com>
2021-10-18 16:28:36 +0300

    - External URL button is shown only if the questionnaire has been answered or its not available - Overlay background color in motto partial is now variable

--- resources/views/landingpages/partials/motto.blade.php
+++ resources/views/landingpages/partials/motto.blade.php
@@ -1,6 +1,6 @@
 <div class="row h-100 w-100 align-items-center mx-0 bg-img"
      style="background-image: url({{asset($viewModel->project->img_path)}});">
-    <div class="gray-filter"></div>
+    <div class="gray-filter" style="background-color: {{ $viewModel->project->lp_motto_overlay_color }}"></div>
     <div class="col-lg-5 col-md-6 col-sm-11 mx-auto motto-content px-0">
         <div class="frosted"></div>
         <div id="project-motto" class="container-fluid">
@@ -26,29 +26,16 @@
                                     <div class="col-md-5 col-sm-12 call-to-action mx-auto">
                                         @include("landingpages.partials.open-questionnaire-button")
                                     </div>
-                                    @if($viewModel->project->external_url)
-                                        <div class="col-md-5 col-sm-12 call-to-action mx-auto text-center">
-                                            @include('landingpages.partials.external-url')
-                                        </div>
-                                    @endif
                                 @else
                                     <div class="col-12">
                                         <h2 class="mt-3 text-center">You have already answered this questionnaire.<br>Thank
                                             you for
                                             your response!</h2>
                                     </div>
-                                    @if($viewModel->project->external_url)
-                                        <div class="col-5 mx-auto call-to-action">
-                                            @include('landingpages.partials.external-url')
-                                        </div>
-                                    @endif
+                                    @include('landingpages.partials.external-url')
                                 @endif
                             @else
-                                @if($viewModel->project->external_url)
-                                    <div class="col-5 mx-auto call-to-action">
-                                        @include('landingpages.partials.external-url')
-                                    </div>
-                                @endif
+                                @include('landingpages.partials.external-url')
                             @endif
                         </div>
                     </div>
