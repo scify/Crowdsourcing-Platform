8db1a2bd017c3b55953611730634b093aae7a1b4
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2021-10-19 13:11:59 +0300

    add the external url at the logo

--- resources/views/landingpages/partials/navbar.blade.php
+++ resources/views/landingpages/partials/navbar.blade.php
@@ -1,10 +1,18 @@
 <div @if (App::environment('staging')) class="header-margin-top" @endif>
-    <nav class="main-header navbar navbar-expand-lg navbar-white navbar-light fixed-top navbar-default navbar-fixed-top m-0" style="z-index: 100000;">
-        <a class="navbar-brand" href="{{ isset($viewModel->home_url) ? $viewModel->home_url : '#' }}">
-            @if(isset($viewModel->project))
+    <nav class="main-header navbar navbar-expand-lg navbar-white navbar-light fixed-top navbar-default navbar-fixed-top m-0"
+         style="z-index: 100000;">
+        @if(isset($viewModel->project) &&  isset($viewModel->project->external_url) && $viewModel->project->external_url !=null)
+            <a class="navbar-brand" target="_blank" href="{{ $viewModel->project->external_url}}">
                 <img loading="lazy" alt="{{$viewModel->project->name}}" src="{{asset($viewModel->project->logo_path)}}">
-            @endif
-        </a>
+            </a>
+        @else
+            <a class="navbar-brand" href="{{ isset($viewModel->home_url) ? $viewModel->home_url : '#' }}">
+                @if(isset($viewModel->project))
+                    <img loading="lazy" alt="{{$viewModel->project->name}}" src="{{asset($viewModel->project->logo_path)}}">
+                @endif
+            </a>
+        @endif
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top-menu-content"
                 aria-controls="top-menu-content" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
