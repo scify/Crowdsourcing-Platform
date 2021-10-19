9fd8fb9b97864c10a16ddd1e05982de2c80a3c2b
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2021-10-19 13:57:17 +0300

    change order of sections at landing page, change logo

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -7,10 +7,11 @@
     <section id="about" class="w-100">
         @include('home.partials.' . config('app.installation_resources_dir') . '.about-us')
     </section>
-    <section id="features" class="w-100">
-        @include('home.partials.' . config('app.installation_resources_dir') . '.features')
-    </section>
     <section id="projects" class="w-100">
         @include('home.partials.' . config('app.installation_resources_dir') . '.projects')
     </section>
+    <section id="features" class="w-100">
+        @include('home.partials.' . config('app.installation_resources_dir') . '.features')
+    </section>
+
 @endsection
