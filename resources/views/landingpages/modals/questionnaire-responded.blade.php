38811f25f25a03d7afc6a77da5c5ad89b0568aac
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2021-11-08 14:22:59 +0200

    change labels

--- resources/views/landingpages/modals/questionnaire-responded.blade.php
+++ resources/views/landingpages/modals/questionnaire-responded.blade.php
@@ -17,7 +17,7 @@
                     <p class="dashboard-message mt-4 mb-4 text-left w-100">Your answers have been saved anonymously. <br>
                         Please login to complete your submission!</p>
                     <a href="{{ route('register') }}" class="btn btn-lg btn-block btn-primary">Sign up / sign in</a>
-                    <p class="dashboard-message mt-4 text-left w-100">By signing-in<sup>*</sup> you
+                    <p class="dashboard-message mt-4 text-left w-100">By registering<sup>*</sup> you
                         <ul>
                             <li>- Help us <strong>filter out</strong> spammers, people who answer randomly.</li>
                             <li>- Can <strong>view</strong> your contribution, <strong>compare your answers</strong> with others.</li>
