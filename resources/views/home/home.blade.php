55fcb839680cd8b940b513bb0132797fdb9b6b60
Paul Isaris <paulisaris@gmail.com>
2021-11-11 16:38:48 +0200

    Home page UI fixes

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -4,13 +4,13 @@
     <section id="motto" class="w-100" style="height: 650px;">
         @include('home.partials.' . config('app.installation_resources_dir') . '.motto')
     </section>
-    <section id="about" class="w-100">
+    <section id="about" class="container-fluid w-100">
         @include('home.partials.' . config('app.installation_resources_dir') . '.about-us')
     </section>
     <section id="projects" class="w-100">
         @include('home.partials.' . config('app.installation_resources_dir') . '.projects')
     </section>
-    <section id="features" class="w-100">
+    <section id="features" class="container-fluid w-100">
         @include('home.partials.' . config('app.installation_resources_dir') . '.features')
     </section>
 
