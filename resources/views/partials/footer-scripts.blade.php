dc8fd919d23c6c10d84b8d752e2b23aa1c692d6d
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2021-11-11 20:51:40 +0200

    async tag at scripts

--- resources/views/partials/footer-scripts.blade.php
+++ resources/views/partials/footer-scripts.blade.php
@@ -1,4 +1,4 @@
-<script src="{{ mix('dist/js/manifest.js') }}"></script> {{-- The Webpack manifest runtime--}}
-<script src="{{ mix('dist/js/vendor.js') }}"></script> {{-- Vendor libraries like jQuery, bootstrap --}}
-<script src="{{ mix('dist/js/common.js')}}"></script> {{-- our application common code --}}
+<script async src="{{ mix('dist/js/manifest.js') }}"></script> {{-- The Webpack manifest runtime--}}
+<script async src="{{ mix('dist/js/vendor.js') }}"></script> {{-- Vendor libraries like jQuery, bootstrap --}}
+<script async src="{{ mix('dist/js/common.js')}}"></script> {{-- our application common code --}}
 @stack('scripts')
