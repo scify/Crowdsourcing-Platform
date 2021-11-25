e49d657441044d03e0ae6a923bf268c41973aaf6
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2021-11-25 11:54:22 +0200

    add language menu

---
+++ resources/views/partials/language-selector.blade.php
@@ -0,0 +1,19 @@
+<!--    TODO: LETS ADD THE LANGUAGE MENU HERE -->
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+       data-bs-toggle="dropdown" aria-expanded="false">
+        EN
+        <!--    TODO: LETS ADD THE SELECTED LANGUAGE HERE HERE -->
+    </a>
+
+    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <li><a class="dropdown-item" href="#">
+                English
+            </a>
+        </li>
+        <li><a class="dropdown-item" href="#">
+                Ελληνικά
+            </a>
+        </li>
+    </ul>
+</li>
