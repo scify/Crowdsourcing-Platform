dc3a35efc1ddadabe4ddc484fafc6f65f8dbf665
Paul Isaris <paulisaris@gmail.com>
2021-12-06 12:34:40 +0200

    CEA-156 A user can cast as many votes as the questionnaire suggests as maximum.

--- resources/assets/js/vue-components/questionnaire/QuestionnaireStatistics.vue
+++ resources/assets/js/vue-components/questionnaire/QuestionnaireStatistics.vue
@@ -43,7 +43,7 @@
           <div class="row justify-content-center">
             <div class="col-10 text-center mx-auto py-5">
               <h4 class="mt-0 p-0 mb-5 text-center message">This questionnaire allows up to
-                {{ questionnaire.max_votes_num }} votes.</h4>
+                <b>{{ questionnaire.max_votes_num }}</b> votes.</h4>
             </div>
           </div>
         </div>
