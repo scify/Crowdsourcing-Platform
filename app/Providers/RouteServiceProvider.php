5ef8e06217d7a716331f944d806cba8d8ef65c56
Paul Isaris <paulisaris@gmail.com>
2021-12-06 17:17:43 +0200

    Disabled rate limiting

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -74,7 +74,6 @@ class RouteServiceProvider extends ServiceProvider {
         });
 
         Route::prefix('api/v1')
-            ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
     }
