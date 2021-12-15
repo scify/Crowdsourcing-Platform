1dc87dcee2a8810248f0c56cd685364a80b628c6
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2021-12-15 22:33:32 +0200

    bug fix

--- resources/views/landingpages/partials/open-questionnaire-button.blade.php
+++ resources/views/landingpages/partials/open-questionnaire-button.blade.php
@@ -2,7 +2,7 @@
 <a href="javascript:void(0)"
    class="btn btn-primary w-100 respond-questionnaire call-to-action {{ !$viewModel->project->lp_show_speak_up_btn ? 'hidden' : '' }}"
    data-open-on-load={{$viewModel->openQuestionnaireWhenPageLoads?"1":"0"}}
-           data-questionnaire-id="{{$viewModel->feedbackQuestionnaire->id}}"
+           data-questionnaire-id="{{$viewModel->questionnaire->id}}"
    data-url="{{route('respond-questionnaire')}}"
    data-toggle="modal" data-target="#questionnaire-modal">
     {{$label}}
