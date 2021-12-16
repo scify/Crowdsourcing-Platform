f688427c2b3577f2ca36a20aed6f20335409166a
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2021-12-16 17:02:03 +0200

    on feedback questionnaires, user should give feedback by opening the project that she initially contributed at

--- app/BusinessLogicLayer/UserDashboardManager.php
+++ app/BusinessLogicLayer/UserDashboardManager.php
@@ -9,11 +9,14 @@ use App\BusinessLogicLayer\gamification\PlatformWideGamificationBadgesProvider;
 use App\BusinessLogicLayer\gamification\QuestionnaireBadgeProvider;
 use App\BusinessLogicLayer\questionnaire\QuestionnaireAccessManager;
 use App\BusinessLogicLayer\questionnaire\QuestionnaireGoalManager;
+use App\Models\CrowdSourcingProject\CrowdSourcingProject;
+use App\Models\Questionnaire\Questionnaire;
 use App\Models\ViewModels\GamificationNextStep;
 use App\Models\ViewModels\QuestionnaireSocialShareButtons;
 use App\Models\ViewModels\UserDashboardViewModel;
 use App\Repository\Questionnaire\QuestionnaireRepository;
 use App\Repository\Questionnaire\Responses\QuestionnaireResponseRepository;
+use Illuminate\Support\Collection;
 
 class UserDashboardManager
 {
@@ -52,26 +55,45 @@ class UserDashboardManager
             // IF:
             //a) they are not answered already
             $responseForThisFeedbackQuestionnaireDoesNotExist = $userResponses->first(function ($u) use ($questionnaire) {
-                return $u->questionnaire_id == $questionnaire->id;
-            }) == null;
+                    return $u->questionnaire_id == $questionnaire->id;
+                }) == null;
             //b) and user has responded to the main project questionnaire
             $responseForMainProjectQuestionnaireExists = $userResponses->first(function ($u) use ($questionnaire) {
-                return $u->questionnaire->type_id == 1 &&
-                    $questionnaire->projects->firstWhere("id", $u->project_id) != null;
-            })!=null;
+                    return $u->questionnaire->type_id == 1 &&
+                        $questionnaire->projects->firstWhere("id", $u->project_id) != null;
+                }) != null;
 
             return
-                $responseForThisFeedbackQuestionnaireDoesNotExist  &&
+                $responseForThisFeedbackQuestionnaireDoesNotExist &&
                 $responseForMainProjectQuestionnaireExists;
         }
         return true;
+    }
+
+    /**
+     * @param $questionnaire
+     * @param $userResponses
+     * @return Collection<CrowdSourcingProject>
+     */
+    private function evaluateProjectsThatUserCanContributeTo(Questionnaire $q, $userResponses): Collection
+    {
+        if ($q->type_id == 2) {
+            //This is a feedback questionnaire.
+            //Let's find the main project that the user answered. THis could be for example /gr/air-quality-athens
+            // We will only display this in the contribute (And we will skip all other projects related with the questionnaire)
+            $mainResponse = $userResponses->first(function ($u) use ($q) {
+                return $u->questionnaire->type_id == 1 &&
+                    $q->projects->firstWhere("id", $u->project_id) != null;
+            });
+            return collect([$q->projects->firstWhere("id",$mainResponse->project_id)]);
+        }
+        return $q->projects;
     }
 
     public function getUserDashboardViewModel($user): UserDashboardViewModel
     {
         $userResponses = $this->questionnaireResponseRepository->getQuestionnaireResponsesOfUser($user->id);
         $questionnaireIdsUserHasAnsweredTo = $userResponses->pluck('questionnaire_id')->toArray();
-        // should show only projects that are published and have at least 1 published questionnaire
         $questionnaires = $this->questionnaireRepository->getActiveQuestionnaires();
         $questionnairesToBeDisplayedInTheDashboard = collect([]);
         foreach ($questionnaires as $questionnaire) {
@@ -86,12 +108,14 @@ class UserDashboardManager
                 ->getNextUnlockableBadgeToShowForQuestionnaire($questionnaire, $user->id, $questionnaireIdsUserHasAnsweredTo);
             $questionnaire->userHasAccessToViewStatisticsPage = $this->questionnaireAccessManager
                 ->userHasAccessToViewQuestionnaireStatisticsPage($user, $questionnaire);
+
+            $projectsYouCanContributeTo = $this->evaluateProjectsThatUserCanContributeTo($questionnaire, $userResponses);
             $questionnaire->gamificationNextStepVM = new GamificationNextStep(
-                $questionnaire->projects,
+                $projectsYouCanContributeTo,
                 $nextUnlockableBadge->getNextStepMessage(),
                 $nextUnlockableBadge->imageFileName,
                 true,
-                new QuestionnaireSocialShareButtons($questionnaire->projects, $questionnaire, $user->id),
+                new QuestionnaireSocialShareButtons($projectsYouCanContributeTo, $questionnaire, $user->id),
                 in_array($questionnaire->id, $questionnaireIdsUserHasAnsweredTo));
 
             $questionnairesToBeDisplayedInTheDashboard->push($questionnaire);
