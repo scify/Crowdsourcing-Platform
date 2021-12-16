9a3558173ed16a19f279711a7f51bd40bdb5421b
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2021-12-16 17:32:23 +0200

    bug fix on landing page, open=1 query parameter was ignored for feedback, plus minor css improvements at the dashboard

--- resources/views/gamification/badge-single.blade.php
+++ resources/views/gamification/badge-single.blade.php
@@ -14,12 +14,12 @@
     <div class="col-md-12 mt-2">
         <h4 class="badgeName" style="color: {{ $badge->level == 0 ? 'black' : 'white' }}; text-align: center; margin: 0.5rem 0;">{{ $badge->badgeName }}</h4>
         @if($badge->level)
-            <h6 class="badgeLevel" style="color: white; text-align: center; margin: 0.5rem 0; ">Level: <span
+            <h6 class="badgeLevel" style="color: white; text-align: center;font-size: 1.3rem; margin: 0.5rem 0 1rem; ">Level: <span
                         class="points" style="font-weight: bold;">{{ $badge->level }}</span></h6>
-            <h5 class="badgeMessage" style="color: white; text-align: center; margin: 0.5rem 0;">{!! $badge->badgeMessage !!}</h5>
+            <h5 class="badgeMessage" style="color: white;font-size: 1rem; text-align: center; margin: 0.5rem 0;">{!! $badge->badgeMessage !!}</h5>
         @else
-            <h6 class="badgeLevel" style="color: black; text-align: center; margin: 0.5rem 0; ">Level: <span class="points" style="font-weight: bold;">0</span></h6>
-            <h5 class="badgeMessage" style="color: black; text-align: center; margin: 0.5rem 0;">You don't own this badge</h5>
+            <h6 class="badgeLevel" style="color: black; text-align: center;font-size: 1.3rem; margin: 0.5rem 0 1rem; ">Level: <span class="points" style="font-weight: bold;">0</span></h6>
+            <h5 class="badgeMessage" style="color: black; font-size: 1rem; text-align: center; margin: 0.5rem 0;">You don't own this badge</h5>
         @endif
     </div>
 </div>
