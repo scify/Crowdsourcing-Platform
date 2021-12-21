b0e25cefb28044e25a0c5c40a1babba6e1c583b9
Paul Isaris <paulisaris@gmail.com>
2021-12-21 14:16:41 +0200

    Solved bug with strings in welcome email

--- app/Notifications/UserRegistered.php
+++ app/Notifications/UserRegistered.php
@@ -29,11 +29,11 @@ class UserRegistered extends Notification {
 
         $message = (new MailMessage)
             ->subject('Crowdsourcing Platform | Welcome!')
-            ->greeting( __("notifications.thanks_message"))
+            ->greeting(__("notifications.thanks_message"))
             ->line('<div style="text-align:center; height: 200px;"><img class="badgeImg" style="height: 200px; margin-bottom: 0;" src=' . asset("images/active_participation.png") . '></div>');
 
-        $message->line('<br><h1 style="text-align: center; margin-bottom: 5px"><b>{{ __("notifications.make_an_impact")}}</b></h1>');
-        $message->line('<p style="text-align: center; margin-bottom: 5px"><b>{{ __("notifications.visit_your_dashboard_contribute}}</b></p>');
+        $message->line('<br><h1 style="text-align: center; margin-bottom: 5px"><b>' . __("notifications.make_an_impact") . '</b></h1>');
+        $message->line('<p style="text-align: center; margin-bottom: 5px"><b>' . __("notifications.visit_your_dashboard_contribute") . '</b></p>');
         $message->action(__("notifications.go_to_dashboard"), url('/my-dashboard'));
         return $message;
     }
