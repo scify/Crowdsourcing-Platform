8bfaed084c748dbfef6aba69176fe1a34131452d
antigonipoulou <antigonip@scify.org>
2021-12-22 09:31:28 +0200

    pt

--- resources/lang/pt/my-dashboard.php
+++ resources/lang/pt/my-dashboard.php
@@ -10,11 +10,11 @@ return [
     */
 
     'my_dashboard' => 'Meu painel',
-    'contribution' => 'Projetos para os quais você pode contribuir',
+    'contribution' => 'Projetos para os quais pode contribuir',
     'answers_left' => ' respostas para alcançar nosso objetivo', 
     'view_statistics'  => 'Ver estatísticas', 
-    'share_questionnaire' => 'Compartilhe o questionário:', 
-    'thank_you_for_sharing' => 'Obrigado por compartilhar o questionário! <br> Em breve você receberá um e-mail com seu crachá!'
+    'share_questionnaire' => 'Partilhe o questionário:', 
+    'thank_you_for_sharing' => 'Obrigado por partilhar o questionário! <br> m breve receberá um e-mail com seu crachá!'
 
 
 
--- resources/lang/pt/my-history.php
+++ resources/lang/pt/my-history.php
@@ -9,10 +9,10 @@ return [
 
     */
 
-    'my_history' => 'Minha história',
+    'my_history' => 'Meu histórico',
     'my_contributions' => 'Minhas contribuições',
     'number_of_questionnaires' => '{0} Contribuiu com 0 questionários |{1}  Contribuiu com 1 questionário |[2,*] Contribuiu com :count questionários"',
-    'no_questionnaires' => 'Você ainda não respondeu a nenhum questionário.',
+    'no_questionnaires' => 'Ainda não respondeu a nenhum questionário.',
     'project' => 'Projeto',
     'questionnaire_title' => 'Título do questionário',
     'questionnaire_description' => 'Descrição do questionário',
