6968118051cdb8c2e93a46b09451b4f5e794562f
Paul Isaris <paulisaris@gmail.com>
2022-01-10 10:01:41 +0200

    Social urls

--- resources/views/auth/partials/social-sign-in.blade.php
+++ resources/views/auth/partials/social-sign-in.blade.php
@@ -7,24 +7,26 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-2 p-0 text-center offset-2">
-                        <a class="socialSignIn" href="{{url('login/social/facebook')}}"><i
+                        <a class="socialSignIn" href="{{url(app()->getLocale() . '/login/social/facebook')}}"><i
                                     class="fab fa-facebook-square"></i></a>
                     </div>
                     <div class="col-2 p-0 text-center me-1">
-                        <a class="socialSignIn" href="{{url('login/social/twitter')}}"><i
+                        <a class="socialSignIn" href="{{url(app()->getLocale() . '/login/social/twitter')}}"><i
                                     class="fab fa-twitter-square"></i></a>
 
                     </div>
                     <div class="col-2 whole px-0 text-center me-1">
-                        <a class="socialSignIn google" href="{{url('login/social/google')}}"><i
+                        <a class="socialSignIn google" href="{{url(app()->getLocale() . '/login/social/google')}}"><i
                                     class="fab fa-google"></i></a>
                     </div>
                     <div class="col-2 whole px-0 text-center">
-                        <a class="socialSignIn microsoft" href="{{url('login/social/microsoft')}}"><i
+                        <a class="socialSignIn microsoft"
+                           href="{{url(app()->getLocale() . '/login/social/microsoft')}}"><i
                                     class="fab fa-windows"></i></a>
                     </div>
                     <div class="col-2 px-0 text-center">
-                        <a class="socialSignIn linkedin" href="{{url('login/social/linkedin')}}"><i
+                        <a class="socialSignIn linkedin"
+                           href="{{url(app()->getLocale() . '/login/social/linkedin')}}"><i
                                     class="fab fa-linkedin"></i></a>
                     </div>
                 </div>
