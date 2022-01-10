ed27443af4da83795af436288904d1e86b2c9d66
Paul Isaris <paulisaris@gmail.com>
2022-01-10 11:26:35 +0200

    Uncomment languages

--- resources/views/partials/language-selector.blade.php
+++ resources/views/partials/language-selector.blade.php
@@ -6,13 +6,13 @@
 
     <ul class="dropdown-menu">
         @foreach ($languages as $language)
-{{--            <li>--}}
-{{--                <a class="dropdown-item"--}}
-{{--                   href="{{ route( getNameOfRoute(\Illuminate\Support\Facades\Route::current()),--}}
-{{--                                                   SetParameterAndGetAll(\Illuminate\Support\Facades\Route::current(), "locale", $language->language_code)) . getRouteParameters()--}}
-{{--                                            }}"--}}
-{{--                   @if (app()->getLocale() == $language->language_code) style="font-weight: bold; text-decoration: underline" @endif>{{ strtoupper($language->language_code) . ", ".strtoupper($language->language_name) }}</a>--}}
-{{--            </li>--}}
+            <li>
+                <a class="dropdown-item"
+                   href="{{ route( getNameOfRoute(\Illuminate\Support\Facades\Route::current()),
+                                                   SetParameterAndGetAll(\Illuminate\Support\Facades\Route::current(), "locale", $language->language_code)) . getRouteParameters()
+                                            }}"
+                   @if (app()->getLocale() == $language->language_code) style="font-weight: bold; text-decoration: underline" @endif>{{ strtoupper($language->language_code) . ", ".strtoupper($language->language_name) }}</a>
+            </li>
         @endforeach
     </ul>
 </li>
