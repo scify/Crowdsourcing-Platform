92055d8b6124cebef6762cdf3823a521c6c162d6
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2022-01-10 11:00:43 +0200

    change character size of avatar to 2048

---
+++ database/migrations/2022_01_10_105732_alter_users_table_avatar.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTableAvatar extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //https://www.geeksforgeeks.org/maximum-length-of-a-url-in-different-browsers/
+            $table->string('avatar', 2048)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
