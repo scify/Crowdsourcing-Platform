a98654c0f0327b5bf62c0fe49870edfbc8f35dfc
Paul Isaris <paulisaris@gmail.com>
2022-01-11 15:00:11 +0200

    Column sizing

--- resources/views/auth/partials/social-sign-in.blade.php
+++ resources/views/auth/partials/social-sign-in.blade.php
@@ -6,16 +6,16 @@
         <div class="col-sm-8 align-left">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-3 p-0 text-center offset-2">
+                    <div class="col-2 p-0 text-center offset-4">
                         <a class="socialSignIn" href="{{url('login/social/facebook')}}"><i
                                     class="fab fa-facebook-square"></i></a>
                     </div>
-                    <div class="col-3 p-0 text-center me-1">
+                    <div class="col-2 p-0 text-center me-1">
                         <a class="socialSignIn" href="{{url('login/social/twitter')}}"><i
                                     class="fab fa-twitter-square"></i></a>
 
                     </div>
-                    <div class="col-3 whole px-0 text-center me-1">
+                    <div class="col-2 whole px-0 text-center me-1">
                         <a class="socialSignIn google" href="{{url('login/social/google')}}"><i
                                     class="fab fa-google"></i></a>
                     </div>
@@ -23,7 +23,7 @@
 {{--                        <a class="socialSignIn microsoft" href="{{url('login/social/microsoft')}}"><i--}}
 {{--                                    class="fab fa-windows"></i></a>--}}
 {{--                    </div>--}}
-                    <div class="col-3 px-0 text-center">
+                    <div class="col-2 px-0 text-center">
                         <a class="socialSignIn linkedin" href="{{url('login/social/linkedin')}}"><i
                                     class="fab fa-linkedin"></i></a>
                     </div>
