8b6f97fca435d8f0dea237dbe442b98e5953cdf1
antigonipoulou <antigonip@scify.org>
2022-01-11 15:46:55 +0200

    code of conduct

--- resources/views/code-of-conduct/en.blade.php
+++ resources/views/code-of-conduct/en.blade.php
@@ -15,20 +15,17 @@
                         <div class="row">
                             <div class="col">
                                 <p>As a crowdsourcing platform we welcome everyone, and encourage a friendly and positive environment.
-                                This code of conduct outlines our expectations for participants for successful participation, as well as steps to reporting unacceptable behavior. We are committed to providing a welcoming and inspiring environment for all and expect our code of conduct to be honored. Anyone who violates this code of conduct may be banned from the Platform.
+                                    This code of conduct outlines our expectations from participants to ascertain successful participation, as well as steps to reporting unacceptable behavior. We are committed to providing a welcoming and inspiring environment for all and expect our code of conduct to be honored. Anyone who violates this code of conduct may be banned from the Platform or their responses can be deleted.
                                 During your participation in crowdsourcing projects:
                                 </p>
                                 <ul>
                                     <li>
-                                        <strong>Be considerate:</strong> Your contribution, the answers you provide to the platform’s questionnaires will be used by other people. Try to be precise and explain your thoughts and arguments with clarity. This will facilitate the platform’s moderators work, where they will have to analyze all given answers, categorize them and produce valuable insights that can have a positive impact in the society.
+                                        <strong>Be clear and relevant to the topic:</strong> Try to be precise and explain your thoughts and arguments with clarity. All answers must be relevant to the topic of the questionnaire. This will facilitate the platform’s moderators work, where they will have to analyze all given answers, categorize them and produce valuable insights that can have a positive impact οn the society.
                                     </li>
-                                    <li><strong>Be respectful:</strong> Not all of us will agree all the time, but disagreement is no excuse for poor behavior and poor manners. We might all experience some frustration now and then, but we cannot allow that frustration to turn into a personal attack. It’s important to remember that even though the answers you provide are anonymous, an environment where people feel uncomfortable or threatened is not a productive one.
+                                    <li><strong>Be respectful and kind:</strong> Not all of us will agree all the time, but disagreement is no excuse for poor behavior and poor manners. It’s important to remember that even though the answers you provide are anonymous, an environment where people feel uncomfortable or threatened is not a productive one. Of course harassment, insults, posting (or threatening to post) other people’s personally identifying information (“doxing”), racist or sexist terms, and other exclusionary behavior aren’t acceptable.
+                                        If you just keep “respectful and kind” in mind, you can't go wrong.
                                     </li>
-                                    <li><strong>Be careful in the words that you choose: </strong>Be kind to others. Even though the answers you provide are anonymous harassment and other exclusionary behavior aren’t acceptable. This includes, but is not limited to: Violent threats or language directed against another person, Discriminatory jokes and language, Posting sexually explicit or violent material, Posting (or threatening to post) other people’s personally identifying information (“doxing”), Personal insults, especially those using racist or sexist terms.
-                                    </li>
-                                    <li><strong>Remember that your contribution is anonymous but public:</strong>Your email and any personal information is not revealed by the platform.  Though the textual answers you provide in the questionnaires, the answers to open-ended questions, may be publicly available. Do not include any personal information in your responses that you do not wish to be publicly available.
-
-
+                                    <li><strong>Remember that your contribution is public:</strong>Your email and any personal information is not revealed by the platform.  Though the textual answers you provide in the questionnaires, the answers to open-ended questions, may be publicly available. Do not include any personal information in your responses that you do not wish to be publicly available.
                                     </li>
                                 </ul>
                             </div>
@@ -44,6 +41,15 @@
                         </div>
                         <div class="row">
                             <div class="col-12">
+                                <h2 class="mt-4 mb-4">Excluding inappropriate content
+                                </h2>
+                            </div>
+                            <div class="col-12">
+                                Platform facilitators will periodically review contributions posted by participants. Facilitators can exclude inappropriate content from the platform, and explain reasons for exclusion to the author of the excluded content.
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
                                 <h2 class="mt-4 mb-4">Reporting Issues
                                 </h2>
                             </div>
@@ -52,7 +58,7 @@
                                 <br><br>
                                 All reports will be handled with discretion. In your report please include:<br>
                                 - Your contact information.<br>
-                                - The questionnaire response that you wish to report.Provide the name of the project, the question and the answer’s text that you want to report.
+                                - The questionnaire response that you wish to report. Provide the name of the project, the question and the answer’s text that you want to report.
                                 <br><br>
                                 After filing a report, a representative will contact you personally, review the incident, follow up with any additional questions, and make a decision as to how to respond.
 
@@ -63,13 +69,9 @@
                                 <h2 class="mt-4 mb-4">Attribution & Acknowledgements</h2>
                             </div>
                             <div class="col-12">
-                                This code of conduct is based on the Code of Conduct from <a href="https://openlifesci.org/code-of-conduct"> Open Life Science </a>
+                                This code of conduct has used elements of the Code of Conduct from the <a href="https://openlifesci.org/code-of-conduct"> Open Life Science </a>
                             </div>
                         </div>
-
-
-
-
                     </div>
                 </div>
             </div>
