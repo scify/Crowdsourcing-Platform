affc20e591b4bd2060f9bc112d74c464ee7ff44d
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2022-01-12 15:29:25 +0200

    at the code of conduct page, redirect back and open the questionnaire if the refferer was the questionnaire page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,33 +4,65 @@ namespace App\Http\Controllers;
 
 
 use App\BusinessLogicLayer\CrowdSourcingProject\CrowdSourcingProjectManager;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
-class HomeController extends Controller {
+
+class HomeController extends Controller
+{
     private $crowdSourcingProjectManager;
 
-    public function __construct(CrowdSourcingProjectManager $crowdSourcingProjectManager) {
+    public function __construct(CrowdSourcingProjectManager $crowdSourcingProjectManager)
+    {
         $this->crowdSourcingProjectManager = $crowdSourcingProjectManager;
     }
 
-    public function showHomePage() {
+    public function showHomePage()
+    {
         $projects = $this->crowdSourcingProjectManager->getCrowdSourcingProjectsForHomePage();
         $pastProjects = $this->crowdSourcingProjectManager->getPastCrowdSourcingProjectsForHomePage();
         return view('home.home')->with(['projects' => $projects, 'pastProjects' => $pastProjects]);
     }
 
-    public function showTermsAndPrivacyPage() {
+    public function showTermsAndPrivacyPage()
+    {
         $locale = app()->getLocale();
-        return view("privacy-policy.".$locale);
+        return view("privacy-policy." . $locale);
     }
-    public function showCodeOfConductPage(){
 
-        $referrer =  request()->headers->get('referer');
-        //if refferer is the questionnaire page, we will allow to redirect back.
-        if ($referrer)
-            $goBackUrl = $referrer . "?open=1";
+    public function showCodeOfConductPage()
+    {
+        $goBackUrl = $this->getRedirectBackUrl();
 
         $locale = app()->getLocale();
-        return view("code-of-conduct.".$locale)
-                ->with(['goBackUrl' => $goBackUrl]);
+        return view("code-of-conduct." . $locale)
+            ->with(['goBackUrl' => $goBackUrl]);
+    }
+
+    /*
+     * If referrer URL belongs to the application and is the questionnaire page,
+     * provide the option to redirect back to the questionnaire.
+     */
+    private function getRedirectBackUrl()
+    {
+        $referrer = request()->headers->get('referer');
+        //if referer is the questionnaire page, we will allow to redirect back.
+        $goBackUrl = null;
+        if ($referrer) {
+            $host = parse_url($referrer, PHP_URL_HOST);
+            $current_host = parse_url(env('APP_URL'), PHP_URL_HOST);
+            if ($host == $current_host) {
+                $route = collect(Route::getRoutes())->first(function ($route) use ($referrer) {
+                    return $route->matches(request()->create($referrer));
+                });
+                if ($route != null && $route->getName() == "project.landing-page") {
+                    $goBackUrl = $referrer;
+                    if (!Str::contains($referrer, '?open'))
+                        $goBackUrl .= "?open=1"; //so user can go back and open the questionnaire
+                }
+            }
+        }
+        return $goBackUrl;
     }
 }
