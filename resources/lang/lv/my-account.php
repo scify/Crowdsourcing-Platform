ec4e790cbf840215e78be2f08914d6076e5eb520
antigonipoulou <antigonip@scify.org>
2022-01-14 13:12:51 +0200

    sign out key

--- resources/lang/lv/my-account.php
+++ resources/lang/lv/my-account.php
@@ -11,7 +11,7 @@ return [
 
     'my_account' => 'Mans profils',
     'personal_details' => 'Personīgā informācija', 
-    'update' => 'Atjauninājums', 
+    'update' => 'Atjaunināt', 
     'my_data' => 'Mani dati', 
     'download_my_data' => 'Lejupielādēt manus datus', 
     'account_deactivation' => 'Konta deaktivizēšana', 
@@ -20,7 +20,7 @@ return [
     'warning_for_deactivation' => '<b>Brīdinājums:</b> Šī darbība dzēsīs tavu kontu.<br><br> Tu zaudēsi visas savas nozīmītes un vairs nevarēsi piedalīties platformā.',
     'deactivate_my_account_2' => 'Es saprotu, lūdzu, deaktivizējiet manu kontu',
     'current_password' => 'Pašreizējā parole', 
-    'new_password' => 'Jauna parole', 
+    'new_password' => 'Jaunā parole', 
     're_enter_password' => 'Paroles atkārtota ievadīšana'
 
 
