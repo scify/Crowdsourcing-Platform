50ece0f1ecc9a8c9a4eaf71afd1eb539796f3c40
antigonipoulou <antigonip@scify.org>
2022-01-19 15:50:59 +0200

    montengrin code of conduct

--- resources/views/code-of-conduct/sr.blade.php
+++ resources/views/code-of-conduct/sr.blade.php
@@ -6,7 +6,7 @@
         <div class="mb-5">
             <div class="row">
                 <div class="col-12 mx-auto mb-3 text-center">
-                    <h1>CODE OF CONDUCT</h1>
+                    <h1>Kodeks ponašanja</h1>
                 </div>
             </div>
             <div class="row">
@@ -14,62 +14,61 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col">
-                                <p>As a crowdsourcing platform we welcome everyone, and encourage a friendly and positive environment.
-                                    This code of conduct outlines our expectations from participants to ascertain successful participation, as well as steps to reporting unacceptable behavior. We are committed to providing a welcoming and inspiring environment for all and expect our code of conduct to be honored. Anyone who violates this code of conduct may be banned from the Platform or their responses can be deleted.
-                                During your participation in crowdsourcing projects:
+                                <p>Kao platforma za crowdsourcing, pozdravljamo sve i podstičemo prijateljsko i pozitivno okruženje.
+                                    Ovaj kodeks ponašanja opisuje naša očekivanja od učesnika kako bi se osiguralo uspješno učešće, kao i korake za prijavljivanje neprihvatljivog ponašanja. Posvećeni smo pružanju prijatnog i inspirativnog okruženja za sve i očekujemo da se poštuje naš kodeks ponašanja. Svakom ko prekrši ovaj kodeks ponašanja može biti zabranjen pristup Platformi ili njihovi odgovori mogu biti izbrisani.
+                                    Tokom Vašeg učešća u crowdsourcing projektima:                                    
                                 </p>
                                 <ul>
                                     <li>
-                                        <strong>Be clear and relevant to the topic:</strong> Try to be precise and explain your thoughts and arguments with clarity. All answers must be relevant to the topic of the questionnaire. This will facilitate the platform’s moderators work, where they will have to analyze all given answers, categorize them and produce valuable insights that can have a positive impact οn the society.
+                                        <strong>Budite jasni i relevantni u vezi teme:</strong> Pokušajte da budete precizni i jasno objasnite svoje misli i argumente. Svi odgovori moraju biti relevantni za temu upitnika. Ovo će olakšati rad moderatora platforme, gdje će morati da analiziraju sve date odgovore, kategorišu ih i proizvedu vrijedne zaključke koji mogu imati pozitivan uticaj na društvo.
                                     </li>
-                                    <li><strong>Be respectful and kind:</strong> Not all of us will agree all the time, but disagreement is no excuse for poor behavior and poor manners. It’s important to remember that even though the answers you provide are anonymous, an environment where people feel uncomfortable or threatened is not a productive one. Of course harassment, insults, posting (or threatening to post) other people’s personally identifying information (“doxing”), racist or sexist terms, and other exclusionary behavior aren’t acceptable.
-                                        If you just keep “respectful and kind” in mind, you can't go wrong.
+                                    <li><strong>Budite učtivi i ljubazni:</strong> Nećemo se svi složiti stalno, ali neslaganje nije izgovor za loše ponašanje i loše manire. Važno je zapamtiti da iako su odgovori koje dajete anonimni, okruženje u kojem se ljudi osećaju neprijatno ili ugroženo nije produktivno. Naravno, uznemiravanje, uvrede, objavljivanje (ili pretnje objavljivanjem) ličnih informacija drugih ljudi („doxing“), rasistički ili seksistički izrazi i drugo isključivo ponašanje nijesu prihvatljivi. Ako samo imate na umu „poštovanje i ljubaznost“, ne možete pogriješiti.
                                     </li>
-                                    <li><strong>Remember that your contribution is public:</strong>Your email and any personal information is not revealed by the platform.  Though the textual answers you provide in the questionnaires, the answers to open-ended questions, may be publicly available. Do not include any personal information in your responses that you do not wish to be publicly available.
+                                    <li><strong>Zapamtite da je Vaš doprinos javan:</strong> Platforma ne otkriva Vaš imejl niti bilo koje lične podatke. Sa druge strane, tekstualni odgovori koje dajete u upitnicima, odgovori na otvorena pitanja mogu biti javno dostupni. Nemojte uključivati nikakve lične podatke u svoje odgovore za koje ne želite da budu javno dostupni.
                                     </li>
                                 </ul>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-12">
-                                <h2 class="mt-4 mb-4">Diversity Statement
+                                <h2 class="mt-4 mb-4">Izjava o različitosti
                                 </h2>
                             </div>
                             <div class="col-12">
-                                We encourage everyone to participate and are committed to building a community for all. Although this list cannot be exhaustive, we explicitly honor diversity in age, gender, gender identity or expression, culture, ethnicity, language, national origin, political beliefs, profession, race, religion, sexual orientation, socioeconomic status, and technical ability. We will not tolerate discrimination based on any of the protected characteristics above, including participants with disabilities.
+                                Podstičemo sve da učestvuju i budu posvećeni izgradnji zajednice za sve. Iako ovaj spisak ne može biti potpun, mi izričito poštujemo različitost u godinama, polu, rodnom identitetu ili izražavanju, kulturi, etničkoj pripadnosti, jeziku, nacionalnom porijeklu, političkim uvjerenjima, profesiji, rasi, religiji, seksualnoj orijentaciji, socioekonomskom statusu i tehničkim sposobnostima. Nećemo tolerisati diskriminaciju na osnovu bilo koje od gore navedenih zaštićenih karakteristika, uključujući učesnike sa invaliditetom.
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-12">
-                                <h2 class="mt-4 mb-4">Excluding inappropriate content
+                                <h2 class="mt-4 mb-4">Uklanjanje neprikladnog sadržaja
                                 </h2>
                             </div>
                             <div class="col-12">
-                                Platform facilitators will periodically review contributions posted by participants. Facilitators can exclude inappropriate content from the platform, and explain reasons for exclusion to the author of the excluded content.
+                                Fasilitatori platforme će periodično pregledati unose koje su unijeli učesnici. Fasilitatori mogu ukloniti neprikladan sadržaj sa platforme i objasniti razloge za uklanjanje autoru uklonjenog sadržaja.
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-12">
-                                <h2 class="mt-4 mb-4">Reporting Issues
+                                <h2 class="mt-4 mb-4">Prijava
                                 </h2>
                             </div>
                             <div class="col-12">
-                                If you experience or witness unacceptable behavior, or have any other concerns, please report it by contacting the organisers at info@scify.org
+                                Ako iskusite ili ste svjedoci neprihvatljivog ponašanja, ili imate bilo kakve druge zabrinutosti, prijavite to kontaktiranjem organizatora na info@scifi.org.
                                 <br><br>
-                                All reports will be handled with discretion. In your report please include:<br>
-                                - Your contact information.<br>
-                                - The questionnaire response that you wish to report. Provide the name of the project, the question and the answer’s text that you want to report.
+                                Sa svim prijavama će se postupati diskretno. Molimo Vas da u prijavu uključite:<br>
+                                - Vaše kontakt informacije.<br>
+                                - Odgovor na upitnik koji želite da prijavite. Navedite naziv projekta, pitanje i tekst odgovora koji želite da prijavite.
                                 <br><br>
-                                After filing a report, a representative will contact you personally, review the incident, follow up with any additional questions, and make a decision as to how to respond.
+                                Nakon podnošenja prijave, predstavnik će Vas lično kontaktirati, pregledati slučaj, postaviti dodatna pitanja i donijeti odluku o tome kako da odgovori.
 
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-12">
-                                <h2 class="mt-4 mb-4">Attribution & Acknowledgements</h2>
+                                <h2 class="mt-4 mb-4">Atribucija & Priznanja</h2>
                             </div>
                             <div class="col-12">
-                                This code of conduct has used elements of the Code of Conduct from the <a href="https://openlifesci.org/code-of-conduct"> Open Life Science </a>
+                                Ovaj kodeks ponašanja koristi elemente Kodeksa ponašanja sa <a href="https://openlifesci.org/code-of-conduct"> Open Life Science. </a>
                             </div>
                         </div>
                     </div>
@@ -78,7 +77,7 @@
             @if ($goBackUrl)
                 <div class=" mt-5 col-md-4 col-sm-12 mx-auto">
                     <a href="{{$goBackUrl}}" class="btn call-to-action go-back"><i
-                                class="fas fa-long-arrow-alt-left"></i> Back to the questionnaire</a>
+                                class="fas fa-long-arrow-alt-left"></i> Nazad na upitnik</a>
                 </div>
             @endif
         </div>
