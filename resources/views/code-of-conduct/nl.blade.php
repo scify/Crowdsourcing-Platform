734b1586297d5bb243a91e160d838c2b73af7496
antigonipoulou <antigonip@scify.org>
2022-01-21 10:55:30 +0200

    conduct dutch

--- resources/views/code-of-conduct/nl.blade.php
+++ resources/views/code-of-conduct/nl.blade.php
@@ -6,7 +6,7 @@
         <div class="mb-5">
             <div class="row">
                 <div class="col-12 mx-auto mb-3 text-center">
-                    <h1>CODE OF CONDUCT</h1>
+                    <h1>Gedragscode</h1>
                 </div>
             </div>
             <div class="row">
@@ -14,62 +14,61 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col">
-                                <p>As a crowdsourcing platform we welcome everyone, and encourage a friendly and positive environment.
-                                    This code of conduct outlines our expectations from participants to ascertain successful participation, as well as steps to reporting unacceptable behavior. We are committed to providing a welcoming and inspiring environment for all and expect our code of conduct to be honored. Anyone who violates this code of conduct may be banned from the Platform or their responses can be deleted.
-                                During your participation in crowdsourcing projects:
+                                <p>We heten iedereen welkom op dit crowdsource platform en moedigen een vriendelijk en positieve omgeving aan.
+                                    In deze gedragscode geven we aan wat we verwachten van deelnemers om een succesvolle deelname te bevorderen. Ook geven we de stappen aan hoe je onacceptabel gedrag kunt melden. We zetten ons in om iedereen een welkome en inspirerende omgeving te bieden en we verwachten dat onze gedragscode zal worden nageleefd. Mensen die zich niet aan de gedragscode houden kunnen van het platform verbannen worden en hun reacties kunnen worden verwijderd.
+                                    Denk aan het volgende bij jouw deelname aan de crowdsource projecten:
                                 </p>
                                 <ul>
                                     <li>
-                                        <strong>Be clear and relevant to the topic:</strong> Try to be precise and explain your thoughts and arguments with clarity. All answers must be relevant to the topic of the questionnaire. This will facilitate the platform’s moderators work, where they will have to analyze all given answers, categorize them and produce valuable insights that can have a positive impact οn the society.
+                                        <strong>Wees helder en richt je op het onderwerp:</strong> Probeer precies te zijn en leg je gedachten en argumenten helder uit. Zorg dat je antwoorden gaan over het onderwerp van de vragenlijst. Dit draagt bij aan de uitvoering van het werk van de moderatoren. Zij zullen alle antwoorden analyseren en categoriseren om tot waardevolle inzichten te komen.
                                     </li>
-                                    <li><strong>Be respectful and kind:</strong> Not all of us will agree all the time, but disagreement is no excuse for poor behavior and poor manners. It’s important to remember that even though the answers you provide are anonymous, an environment where people feel uncomfortable or threatened is not a productive one. Of course harassment, insults, posting (or threatening to post) other people’s personally identifying information (“doxing”), racist or sexist terms, and other exclusionary behavior aren’t acceptable.
-                                        If you just keep “respectful and kind” in mind, you can't go wrong.
+                                    <li><strong>Wees vriendelijk en respectvol:</strong> We zijn het niet altijd met elkaar eens maar dit is geen excuus om onvriendelijk te worden. Wees je ervan bewust dat een omgeving waarin mensen zich ongemakkelijk of bedreigd voelen, vaak niet tot goede resultaten leidt. Ook al zijn de antwoorden anoniem. Beledigingen, intimidatie, racistische of seksistische termen en ander uitsluitend gedrag worden niet geaccepteerd. Ook het plaatsen van persoonlijke gegevens van anderen (doxing), of daarmee dreigen zijn niet toegestaan. Blijf gewoon vriendelijk en respectvol en dan gaat het altijd goed.
                                     </li>
-                                    <li><strong>Remember that your contribution is public:</strong>Your email and any personal information is not revealed by the platform.  Though the textual answers you provide in the questionnaires, the answers to open-ended questions, may be publicly available. Do not include any personal information in your responses that you do not wish to be publicly available.
+                                    <li><strong>Denk er aan dat je bijdrage openbaar is:</strong> Je persoonlijke gegevens zoals je emailadres worden niet op het platform getoont. Maar de tekstuele antwoorden die je geeft in de vragenlijsten, kunnen wel openbaar inzichtelijk zijn. Zet daarom geen persoonlijke gegevens in je antwoorden waarvan je niet wilt dat deze publiekelijk zichtbaar zijn.
                                     </li>
                                 </ul>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-12">
-                                <h2 class="mt-4 mb-4">Diversity Statement
+                                <h2 class="mt-4 mb-4">Diversiteitsverklaring
                                 </h2>
                             </div>
                             <div class="col-12">
-                                We encourage everyone to participate and are committed to building a community for all. Although this list cannot be exhaustive, we explicitly honor diversity in age, gender, gender identity or expression, culture, ethnicity, language, national origin, political beliefs, profession, race, religion, sexual orientation, socioeconomic status, and technical ability. We will not tolerate discrimination based on any of the protected characteristics above, including participants with disabilities.
+                                We moedigen iedereen aan om deel te nemen. En wij zetten ons in om een community te creëren waarin iedereen zich thuis voelt. Hoewel deze lijst niet volledig kan zijn, zijn wij expliciet voor diversiteit in leeftijd, gender, gender identiteit of uiting, cultuur, etniciteit, taal, nationaliteit, politieke overtuigingen, professie, ras, religie, seksuele oriëntatie, socioeconomische status en technische capaciteiten. We accepteren geen discriminatie op een van de beschermde karakteristieken die hierboven genoemd zijn, inclusief deelnemers met een fysieke beperking.
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-12">
-                                <h2 class="mt-4 mb-4">Excluding inappropriate content
+                                <h2 class="mt-4 mb-4">Uitsluiting van ongepaste bijdragen
                                 </h2>
                             </div>
                             <div class="col-12">
-                                Platform facilitators will periodically review contributions posted by participants. Facilitators can exclude inappropriate content from the platform, and explain reasons for exclusion to the author of the excluded content.
+                                De beheerders van het platform zullen regelmatig de bijdragen van deelnemers bekijken. Beheerders kunnen ongepaste bijdragen verwijderen van het platform. Ze zullen de reden voor de verwijdering uitleggen aan de schrijver van de verwijderde content.
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-12">
-                                <h2 class="mt-4 mb-4">Reporting Issues
+                                <h2 class="mt-4 mb-4">Melden van problemen
                                 </h2>
                             </div>
                             <div class="col-12">
-                                If you experience or witness unacceptable behavior, or have any other concerns, please report it by contacting the organisers at info@scify.org
+                                Als je onacceptabel gedrag ervaart of waarneemt of als je andere zorgen hebt, meldt deze dan alsjeblieft bij de organisatoren via info@scify.org.
                                 <br><br>
-                                All reports will be handled with discretion. In your report please include:<br>
-                                - Your contact information.<br>
-                                - The questionnaire response that you wish to report. Provide the name of the project, the question and the answer’s text that you want to report.
+                                Alle meldingen zullen met tact worden behandeld. Als je een melding maakt voeg daar dan de volgende zaken aan toe:<br>
+                                - Jouw 	contactgegevens.<br>
+                                - Het inhoudelijke deel van de vragenlijst waarover je een melding wilt maken. Geef daarbij aan de naam van het project, de vraag en het antwoord waarvan je melding wilt maken.
                                 <br><br>
-                                After filing a report, a representative will contact you personally, review the incident, follow up with any additional questions, and make a decision as to how to respond.
+                                Nadat je een melding hebt gemaakt zal een vertegenwoordiger van het platform contact met je opnemen. De vertegenwoordiger zal zal de melding beoordelen en als dit nodig is nog extra vragen stellen. Vervolgens wordt een besluit genomen over hoe er mee zal worden omgegaan.
 
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-12">
-                                <h2 class="mt-4 mb-4">Attribution & Acknowledgements</h2>
+                                <h2 class="mt-4 mb-4">Naamsvermelding en dankbetuiging</h2>
                             </div>
                             <div class="col-12">
-                                This code of conduct has used elements of the Code of Conduct from the <a href="https://openlifesci.org/code-of-conduct"> Open Life Science </a>
+                                Deze gedragscode heeft elementen gebruikt van de Gedragscode van de <a href="https://openlifesci.org/code-of-conduct"> Open Life Science </a>
                             </div>
                         </div>
                     </div>
