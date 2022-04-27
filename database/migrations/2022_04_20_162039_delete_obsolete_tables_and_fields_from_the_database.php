6518c66dec8360986a45d55046904cccf5381a7b
Paul Isaris <paulisaris@gmail.com>
2022-04-27 15:57:32 +0300

    Cleared obsolete classes, DB tables, and DB fields

---
+++ database/migrations/2022_04_20_162039_delete_obsolete_tables_and_fields_from_the_database.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DeleteObsoleteTablesAndFieldsFromTheDatabase extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::dropIfExists('questionnaire_html');
+        Schema::dropIfExists('questionnaire_possible_answers');
+        Schema::dropIfExists('questionnaire_questions');
+        Schema::dropIfExists('questionnaire_response_answers');
+        Schema::dropIfExists('questionnaire_response_answer_texts');
+        Schema::dropIfExists('questionnaire_translation_html');
+        Schema::dropIfExists('questionnaire_translation_possible_answers');
+        Schema::dropIfExists('questionnaire_translation_questions');
+        Schema::table('questionnaires', function (Blueprint $table) {
+            $table->dropColumn(['title', 'description']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+
+    }
+}
