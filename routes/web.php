9c3700b426b558201ab55950b4c4390a79b41de4
Paul Isaris <paulisaris@gmail.com>
2022-05-19 12:36:54 +0200

    Improved the locale search in order to fix urls that don't make sense for the app (eg /123).

--- routes/web.php
+++ routes/web.php
@@ -148,6 +148,7 @@ Route::get('/questionnaire/answers-admin-analysis-statuses-get/', [Questionnaire
 Route::group($localeInfo, function () {
     Route::get('/{project_slug}', 'CrowdSourcingProjectController@showLandingPage')->name('project.landing-page');
 });
+Route::get('/{project_slug}', 'CrowdSourcingProjectController@showLandingPage')->name('project.landing-page');
 
 
 
