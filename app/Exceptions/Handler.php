4e15bd58c996c07a699d78b06959fa2921a893b7
Paul Isaris <paulisaris@gmail.com>
2022-05-26 14:43:23 +0300

    Added a handler for 404 errors, in order to set the locale part of the URL correctly.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,14 @@
 
 namespace App\Exceptions;
 
+use Http\Discovery\Exception\NotFoundException;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\App;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler {
@@ -47,5 +53,26 @@ class Handler extends ExceptionHandler {
         if (App::environment('local'))
             return false;
         return parent::shouldReport($e);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Throwable $exception
+     * @return Application|RedirectResponse|Response|Redirector
+     * @throws Throwable
+     */
+    public function render($request, Throwable $exception) {
+
+        if ($exception instanceof NotFoundHttpException) {
+            $urlVars = explode('/', url()->current());
+            $locale = $urlVars[3];
+            $lastPart = $urlVars[4];
+            if(app()->getLocale() !== $locale)
+                return redirect("/" . app()->getLocale() . '/' . $lastPart);
+        }
+
+        return parent::render($request, $exception);
     }
 }
