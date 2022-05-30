74ef561097692ef4b66e14a572e02cb6a2946101
Paul Isaris <paulisaris@gmail.com>
2022-05-30 09:33:26 +0300

    Added a child class to Auth middleware, in order to override the `redirectTo` method, so that we ensure that we always redirect to the `/login` route with a locale.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\AuthMiddleware;
 use App\Http\Middleware\CheckQuestionnairePageVisibilitySettings;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -55,7 +56,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        'auth' => AuthMiddleware::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
