fe45da25784b1b49b9c6487b12c959bf5bf76938
antigonipoulou <andigoni.poulou@gmail.com>
2022-06-14 13:42:34 +0300

    sk translation

---
+++ resources/lang/sk/login-register.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Login-register elements
+    |--------------------------------------------------------------------------
+
+    */
+
+
+    'moto_login_page'             => 'Definujme našu budúcnosť!', 
+    'login_message'               => 'Prihláste sa pomocou svojho účtu',  
+    'remember_me'                 => 'Zapamätajte si ma', 
+    'login_with'                  => 'Alebo sa prihláste pomocou:', 
+    'register_option'             => 'Chcem sa zaregistrovať', 
+    'i_forgot_my_password'        => 'Zabudol som heslo',  
+    'create_your_account'         => 'Vytvorte si účet',  
+    'nickname'                    => 'Prezývka',  
+    'email'                       => 'E-mail',  
+    'password'                    => 'Heslo',  
+    'retype_password'             => 'Znovu zadajte heslo',  
+    'register'                    => 'Vytvoriť účet', 
+    'i_already_have_a_membership' => 'Už mám účet',   
+    'password_reset_message'      => 'Obnoviť heslo', 
+    'send_password_reset_link'    => 'Odoslať odkaz na obnovenie hesla',  
+    'nickname_field_required'     => 'Pole prezývky je povinné.',
+    'email_field_required'        => 'Pole e-mailu je povinné..',
+    'password_field_required'     => 'Pole hesla je povinné.',
+    'password_reset_link'         => 'Poslali sme vám e-mailom odkaz na obnovenie hesla!',
+    'email_already_using'         => 'E-mail už bol prijatý.',
+    'password_not_match'          => 'Potvrdenie hesla sa nezhoduje.',
+    'password_min_characters'     => 'Heslo musí mať aspoň 8 znakov.',
+    'credentials_not_match'       => 'Tieto poverenia sa nezhodujú s našimi záznamami.',
+    'one_step_away'               => 'Ste o krok ďalej! ',
+    'avoid_duplicate_submissions' => 'Aby sa predišlo duplicitným odoslaniam, môžu prispievať iba prihlásení používatelia. Ak chcete pokračovať, prihláste sa'
+
+
+
+];
---
+++ resources/lang/sk/my-dashboard.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | My dashboard elements
+    |--------------------------------------------------------------------------
+
+    */
+
+    'my_dashboard' => 'Môj Dashboard',
+    'contribution' => 'Projekty, do ktorých môžete prispieť',
+    'answers_left' => ' odpovedí na dosiahnutie nášho cieľa', 
+    'view_statistics'  => 'Zobraziť štatistiky', 
+    'share_questionnaire' => 'Zdieľať dotazník:', 
+    'thank_you_for_sharing' => 'Ďakujeme za zdieľanie dotazníka! <br> Čoskoro dostanete e-mail s vaším odznakom!'
+
+
+
+
+
+
+];
