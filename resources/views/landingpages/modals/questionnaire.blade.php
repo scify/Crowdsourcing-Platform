1a663018f1bf3c37a52a21671fbfed638cfc4334
Paul Isaris <paulisaris@gmail.com>
2022-06-16 09:47:13 +0300

    Added aria tags

--- resources/views/landingpages/modals/questionnaire.blade.php
+++ resources/views/landingpages/modals/questionnaire.blade.php
@@ -1,8 +1,8 @@
-<div class="modal fade questionnaire-modal " id="questionnaire-modal" tabindex="-1" role="dialog">
+<div class="modal fade questionnaire-modal " id="questionnaire-modal" tabindex="-1" aria-labelledby="questionnaireModalTitle" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">{{$viewModel->questionnaire->fieldsTranslation->title}}</h4>
+                <h4 class="modal-title" id="questionnaireModalTitle">{{$viewModel->questionnaire->fieldsTranslation->title}}</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
