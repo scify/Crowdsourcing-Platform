c92c9a403f6538f157c83fedfeff8a3c3f992f67
Paul Isaris <paulisaris@gmail.com>
2022-09-29 12:07:01 +0300

    Updated Motto Subtitle column to be bigger

---
+++ database/migrations/2022_09_29_120559_change_motto_subtitle_in_crowd_sourcing_project_translations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeMottoSubtitleInCrowdSourcingProjectTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('crowd_sourcing_project_translations', function (Blueprint $table) {
+            $table->text('motto_subtitle')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('crowd_sourcing_project_translations', function (Blueprint $table) {
+            //
+        });
+    }
+}
