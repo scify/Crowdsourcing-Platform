95f28f8d4c59368fb05d833882ea9217808e0684
Paul Isaris <paulisaris@gmail.com>
2022-10-10 15:20:29 +0300

    Added mix asset url

--- config/app.php
+++ config/app.php
@@ -59,7 +59,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
-
+    'mix_url' => env('MIX_ASSET_URL', null),
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
