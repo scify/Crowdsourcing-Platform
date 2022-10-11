2e4e33588cb3018b170f597de087227a8bbdcb26
Paul Isaris <paulisaris@gmail.com>
2022-10-11 09:06:51 +0300

    Removed unclosed comments

--- resources/views/partials/footer-scripts.blade.php
+++ resources/views/partials/footer-scripts.blade.php
@@ -7,13 +7,13 @@
 </script>
 <script type="module" src="{{ mix('dist/js/manifest.js') }}"></script> {{-- The Webpack manifest runtime--}}
 <script type="module" src="{{ mix('dist/js/vendor.js') }}"></script> {{-- Vendor libraries like jQuery, bootstrap --}}
-<script src="{{ mix('dist/js/common.js')}}"></script>--> {{-- our application common code --}}
+<script src="{{ mix('dist/js/common.js')}}"></script> {{-- our application common code --}}
 @if (isset($includeBackofficeCommonJs) && $includeBackofficeCommonJs)
-    <script src="{{ mix('dist/js/common-backoffice.js')}}"></script>--> {{-- backend common code --}}
+    <script src="{{ mix('dist/js/common-backoffice.js')}}"></script> {{-- backend common code --}}
 @endif
 @if(config('app.user_way_id'))
     <script>(function () {
-            var s = document.createElement("script");
+            const s = document.createElement("script");
             s.setAttribute("data-account", '{{ config('app.user_way_id') }}');
             s.setAttribute("src", "https://cdn.userway.org/widget.js");
             document.body.appendChild(s);
