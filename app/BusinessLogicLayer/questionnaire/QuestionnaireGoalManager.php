25f58c3f8790590da765c4b89e1eb79ccc9c6432
Paul Isaris <paulisaris@gmail.com>
2022-10-11 12:43:34 +0300

    Fixed possible division by zero

--- app/BusinessLogicLayer/questionnaire/QuestionnaireGoalManager.php
+++ app/BusinessLogicLayer/questionnaire/QuestionnaireGoalManager.php
@@ -11,7 +11,10 @@ class QuestionnaireGoalManager {
 
     public function getQuestionnaireGoalViewModel(Questionnaire $questionnaire, int $responses_count): QuestionnaireProjectGoal {
         $responsesNeededToReachGoal = $questionnaire->goal - $responses_count;
-        $targetAchievedPercentage = round($responses_count / $questionnaire->goal * 100, 1);
+        if ($questionnaire->goal == 0)
+            $targetAchievedPercentage = 0;
+        else
+            $targetAchievedPercentage = round($responses_count / $questionnaire->goal * 100, 1);
         $goal = $questionnaire->goal;
 
         return new QuestionnaireProjectGoal($responsesNeededToReachGoal, $targetAchievedPercentage, $goal);
