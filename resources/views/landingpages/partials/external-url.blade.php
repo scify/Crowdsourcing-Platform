a3a3cf291721e2c2340fedd5273d39a4238172bd
Paul Isaris <paulisaris@gmail.com>
2022-10-19 16:27:40 +0300

    Added margin top class

--- resources/views/landingpages/partials/external-url.blade.php
+++ resources/views/landingpages/partials/external-url.blade.php
@@ -1,5 +1,5 @@
 @if($viewModel->project->external_url)
-    <div class="col-md-5 col-sm-12 mx-auto">
+    <div class="col-md-5 col-sm-12 mx-auto mt-5">
         <a href="{{$viewModel->project->external_url}}" target="_blank" class="btn btn-outline-dark call-to-action">
             {{ __("questionnaire.visit_projects_site") }}
         </a>
