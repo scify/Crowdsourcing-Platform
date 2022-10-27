a72a94ce76a7bb0ac0fb2de2b33b58852c1dd95c
Paul Isaris <paulisaris@gmail.com>
2022-10-27 13:58:33 +0300

    - Added AWS S3 library - Refactored General FileUploader to store files to S3

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,7 +66,7 @@ return [
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
+            'bucket' => env('AWS_BUCKET')
         ],
 
     ],
