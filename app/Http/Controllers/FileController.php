0ddc8ee60cdf54ca2453c317ec61606c0b26bcef
Paul Isaris <paulisaris@gmail.com>
2022-10-27 13:58:33 +0300

    - Added AWS S3 library - Refactored General FileUploader to store files to S3

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,23 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Utils\FileUploader;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller {
     public function uploadFiles(Request $request): JsonResponse {
         $request->validate([
             'files.*' => 'required|file|max:52428800|mimetypes:image/*,audio/,video/*,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/pdf',
         ]);
-        $dirToStoreFileInUploads = $request->directory ?? 'default';
         $responseFilePaths = [];
         foreach ($request->files as $fileObject) {
             $symfonyFile = $fileObject[0];
             $uploadedFile = UploadedFile::createFromBase($symfonyFile);
             $originalFileName = $uploadedFile->getClientOriginalName();
-            $uploadedFilePath = config('app.url') . FileUploader::uploadAndGetPath($uploadedFile, $dirToStoreFileInUploads);
+            $path = Storage::disk('s3')->put('uploads', $uploadedFile);
+            $uploadedFilePath = Storage::disk('s3')->url($path);
             $responseFilePaths[$originalFileName] = $uploadedFilePath;
         }
 
