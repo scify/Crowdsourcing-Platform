08cfef970f68f5a2b48f925e1a7281ec5f00d10b
Paul Isaris <paulisaris@gmail.com>
2022-11-21 15:55:33 +0200

    - Added plugin for cookies to be GDPR compliant

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -1,36 +1,39 @@
-<!-- Google Analytics -->
-@if(config('app.google_analytics_id'))
-    <script defer async>
-        (function (i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function () {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+@if(isset($_COOKIE[config('cookies_consent.cookie_prefix')
+. 'cookies_consent_targeting']))
+    <!-- Google Analytics -->
+    @if(config('app.google_analytics_id'))
+        <script defer async>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                    m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-        window.ga('create', '{{ config('app.google_analytics_id') }}', 'auto');
-        window.ga('set', 'anonymizeIp', true);
-        window.ga('send', 'pageview');
+            window.ga('create', '{{ config('app.google_analytics_id') }}', 'auto');
+            window.ga('set', 'anonymizeIp', true);
+            window.ga('send', 'pageview');
 
-    </script>
+        </script>
+    @endif
+    <!-- End Google Analytics -->
+    <!-- Google Tag Manager -->
+    <script defer async>(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push(
+                {'gtm.start': new Date().getTime(), event: 'gtm.js'}
+            );
+            const f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l !== 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', '{{ config('app.google_tag_manager_id') }}');</script>
+    <!-- End Google Tag Manager -->
 @endif
-<!-- End Google Analytics -->
-<!-- Google Tag Manager -->
-<script defer async>(function (w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push(
-            {'gtm.start': new Date().getTime(), event: 'gtm.js'}
-        );
-        const f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s), dl = l !== 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', '{{ config('app.google_tag_manager_id') }}');</script>
-<!-- End Google Tag Manager -->
