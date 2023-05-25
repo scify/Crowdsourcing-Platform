c4d752dbc9c1297abda5e93f838cf10b2d509779
Paul Isaris <paulisaris@gmail.com>
2023-05-25 10:26:49 +0300

    Refactor AnalyticsLogger to use ES6 syntax

--- resources/assets/js/analytics-logger.js
+++ resources/assets/js/analytics-logger.js
@@ -1,20 +1,20 @@
 class AnalyticsLogger {
 
-    static logEvent(eventType, category, action, label, value) {
-        if (this.isGoogleAnalyticsLoaded()) {
-            console.log("gtag enabled");
-            window.gtag("event", eventType, {
-                eventCategory: category,
-                eventAction: action,
-                eventLabel: label,
-                eventValue: value
-            });
-        }
-    }
+	static logEvent(eventType, category, action, label, value) {
+		if (this.isGoogleAnalyticsLoaded()) {
+			console.log("gtag enabled");
+			window.gtag("event", eventType, {
+				eventCategory: category,
+				eventAction: action,
+				eventLabel: label,
+				eventValue: value
+			});
+		}
+	}
 
-    static isGoogleAnalyticsLoaded() {
-        return window.gtag !== undefined;
-    }
+	static isGoogleAnalyticsLoaded() {
+		return window.gtag !== undefined;
+	}
 
 }
 
