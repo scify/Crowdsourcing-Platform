ea8eca9d7fd995d3b2c5959c8ce0a91ca7531628
Paul Isaris <paulisaris@gmail.com>
2024-06-28 11:48:50 +0300

    Improved accessibility

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     @include('partials.favicons')
     <title>@yield('title_prefix', config('app.name')) @yield('title_postfix', '')</title>
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=10, user-scalable=yes" name="viewport">
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,300italic,400italic,600italic">
     @vite('resources/assets/sass/common.scss')
