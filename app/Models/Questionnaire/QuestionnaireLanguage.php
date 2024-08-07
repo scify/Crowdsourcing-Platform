c4ee0d8353b2e51e5b26b8751c5abbcafdfd2898
Paul Isaris <paulisaris@gmail.com>
2024-08-07 16:20:38 +0300

    Added more tests for QuestionnaireControllerTest.php

--- app/Models/Questionnaire/QuestionnaireLanguage.php
+++ app/Models/Questionnaire/QuestionnaireLanguage.php
@@ -4,6 +4,7 @@ namespace App\Models\Questionnaire;
 
 use App\Models\Language;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -21,6 +22,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property-read Language $language
  */
 class QuestionnaireLanguage extends Model {
+    use HasFactory;
     use SoftDeletes;
 
     protected $table = 'questionnaire_languages';
