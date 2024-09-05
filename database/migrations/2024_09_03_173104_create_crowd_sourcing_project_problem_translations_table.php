e7d7d763dae096329896fcc7141c8741fe23decb
Kostas Papandreou <papandrk@gmail.com>
2024-09-05 11:29:12 +0300

    added db migrations for project_problems, problem_solutions, etc.

---
+++ database/migrations/2024_09_03_173104_create_crowd_sourcing_project_problem_translations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('crowd_sourcing_project_problem_translations', function (Blueprint $table) {
+            $table->unsignedBigInteger('problem_id');
+            $table->foreign('problem_id')->references('id')->on('crowd_sourcing_project_problems');
+            
+            $table->unsignedInteger('language_id');
+            $table->foreign('language_id')->references('id')->on('languages_lkp');
+            
+            $table->primary(['problem_id', 'language_id'], 'crowd_sourcing_project_problem_translations_primary');
+
+            $table->string('title', 250);
+            $table->text('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('crowd_sourcing_project_problem_translations');
+    }
+};
---
+++ database/migrations/2024_09_03_173415_create_crowd_sourcing_project_problem_solution_translations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('crowd_sourcing_project_problem_solution_translations', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('solution_id');
+            $table->foreign('solution_id', 'csp_problem_solution_translations_solution_id_foreign')->references('id')->on('crowd_sourcing_project_problem_solutions');
+            
+            $table->unsignedInteger('language_id');
+            $table->foreign('language_id', 'csp_problem_solution_translations_language_id_foreign')->references('id')->on('languages_lkp');
+            
+            $table->primary(['solution_id', 'language_id'], 'crowd_sourcing_project_problem_solution_translations_primary');
+
+            $table->string('title', 250);
+            $table->text('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('crowd_sourcing_project_problem_solution_translations');
+    }
+};
