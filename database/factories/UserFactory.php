bc3efd76db372e9c1ff2fbc86e5ae554de3d4c48
Paul Isaris <paulisaris@gmail.com>
2024-10-04 17:24:41 +0300

    Fixed User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,12 +5,17 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Random\RandomException;
 
 class UserFactory extends Factory {
     protected $model = User::class;
 
+    /**
+     * @throws RandomException
+     */
     public function definition() {
         return [
+            'id' => $this->faker->unique()->numberBetween(1, 10000),
             'nickname' => $this->faker->name,
             'avatar' => $this->faker->imageUrl(),
             'email' => $this->faker->unique()->safeEmail,
--- tests/Feature/Controllers/CrowdSourcingProjectControllerTest.php
+++ tests/Feature/Controllers/CrowdSourcingProjectControllerTest.php
@@ -11,13 +11,9 @@ use App\Models\User;
 use App\Models\UserRole;
 use App\Repository\CrowdSourcingProject\CrowdSourcingProjectRepository;
 use Faker\Factory as Faker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class CrowdSourcingProjectControllerTest extends TestCase {
-    use RefreshDatabase;
-
-    protected $seed = true;
     protected CrowdSourcingProjectRepository $crowdSourcingProjectRepository;
 
     protected function setUp(): void {
@@ -299,9 +295,9 @@ class CrowdSourcingProjectControllerTest extends TestCase {
     /**
      * @test
      */
-    public function authenticatedUserCannotStoreProject() {
+    public function nonAdminUserCannotStoreProject() {
         $user = User::factory()->create();
-        $this->actingAs($user);
+        $this->be($user);
 
         $response = $this->withoutMiddleware(VerifyCsrfToken::class) // Disable CSRF only
             ->post(route('projects.store'), [
