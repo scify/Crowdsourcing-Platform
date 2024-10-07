f22f4f4073fa1d4b3509e0bf45882aa2f9364a31
Paul Isaris <paulisaris@gmail.com>
2024-10-07 12:03:53 +0300

    Added a separate seeder for the Air Quality project

--- database/seeders/DefaultProjectSeeder.php
+++ database/seeders/DefaultProjectSeeder.php
@@ -75,22 +75,6 @@ class DefaultProjectSeeder extends Seeder {
                 'display_landing_page_banner' => 1,
                 'status_id' => CrowdSourcingProjectStatusLkp::DRAFT,
             ],
-            [
-                'id' => 4,
-                'slug' => 'air-quality-europe',
-                'external_url' => 'https://www.scify.gr/site/en/',
-                'img_path' => '/images/projects/air-quality-europe/logo-bg.webp',
-                'logo_path' => '/images/projects/air-quality-europe/logo.webp',
-                'user_creator_id' => 1,
-                'language_id' => 6,
-                'should_send_email_after_questionnaire_response' => 1,
-                'lp_primary_color' => '#707070',
-                'lp_questionnaire_image_path' => '/images/projects/air-quality-europe/logo.webp',
-                'lp_show_speak_up_btn' => 1,
-                'sm_featured_img_path' => '/images/projects/air-quality-europe/logo.webp',
-                'display_landing_page_banner' => 1,
-                'status_id' => CrowdSourcingProjectStatusLkp::PUBLISHED,
-            ],
         ];
 
         $project_translations = [
@@ -190,44 +174,6 @@ class DefaultProjectSeeder extends Seeder {
                 'sm_keywords' => 'Social Media Keywords',
                 'questionnaire_response_email_intro_text' => '<p>Thanks to your contribution we are one step closer to understanding the problem.<br></p>',
                 'questionnaire_response_email_outro_text' => '<p>Thank you for your time and effort.<br></p>',
-            ],
-            [
-                'id' => 6,
-                'language_id' => 6,
-                'project_id' => 4,
-                'name' => 'Air Quality in Europe',
-                'motto_title' => 'Please share with us your opinion on the air quality in Europe. Your voice matters!',
-                'motto_subtitle' => 'You can make an impact! Share your opinion with us!',
-                'description' => 'Lorem ipsum dolor site amet',
-                'about' => '<p>Contribute to solving air quality problems in Athens and across Europe. Write points for and against the proposed solutions. Your contributions will be a valuable contribution to official policy to improve air quality in Athens and across Europe.</p>',
-                'footer' => '<p style="font-size: 12px;">© SCIFY ' . now()->year . '&nbsp;|&nbsp;
-                    <a href="https://www.scify.gr/site/en/" target="_blank" title="Read more">Terms of use</a>&nbsp;|&nbsp;
-                    <a href="https://www.scify.gr/site/en/" target="_blank" title="Read more">Privacy Policy</a>&nbsp;|&nbsp;
-                    <a href="https://www.scify.gr/site/en/" target="_blank" title="Read more">Cookie Policy</a>',
-                'sm_title' => 'Air Quality in Europe',
-                'sm_description' => 'Please share with us your opinion on the air quality in Europe. Your voice matters!',
-                'sm_keywords' => 'Social Media Keywords',
-                'questionnaire_response_email_intro_text' => '<p>Thanks to your contribution we are one step closer to understanding the problem.<br></p>',
-                'questionnaire_response_email_outro_text' => '<p>Thank you for your time and effort.<br></p>',
-            ],
-            [
-                'id' => 7,
-                'language_id' => 12,
-                'project_id' => 4,
-                'name' => 'Η Ποιότητα του Αέρα στην Ευρώπη',
-                'motto_title' => 'Παρακαλούμε μοιραστείτε μαζί μας την άποψή σας για την ποιότητα του αέρα στην Ευρώπη. Η φωνή σας μετράει!',
-                'motto_subtitle' => 'Μπορείς να κάνεις διαφορά! Μοιραστείτε την άποψή σας μαζί μας!',
-                'description' => 'Lorem ipsum dolor site amet',
-                'about' => '<p>Συμβάλλετε στην επίλυση των προβλημάτων ποιότητας του αέρα στην Αθήνα και σε όλη την Ευρώπη. Γράψτε επιχειρήματα υπέρ και κατά των προτεινόμενων λύσεων. Η συμβολή σας θα είναι πολύτιμη για τη διαμόρφωση επίσημης πολιτικής για τη βελτίωση της ποιότητας του αέρα στην Αθήνα και σε όλη την Ευρώπη.</p>',
-                'footer' => '<p style="font-size: 12px;">© SCIFY ' . now()->year . '&nbsp;|&nbsp;
-                    <a href="https://www.scify.gr/site/en/" target="_blank" title="Read more">Πολιτική Ορθής Χρήσης</a>&nbsp;|&nbsp;
-                    <a href="https://www.scify.gr/site/en/" target="_blank" title="Read more">Πολιτική Ιδιωτικότητας</a>&nbsp;|&nbsp;
-                    <a href="https://www.scify.gr/site/en/" target="_blank" title="Read more">Πολιτική Cookies</a>',
-                'sm_title' => 'Η Ποιότητα του Αέρα στην Ευρώπη',
-                'sm_description' => 'Παρακαλούμε μοιραστείτε μαζί μας την άποψή σας για την ποιότητα του αέρα στην Ευρώπη. Η φωνή σας μετράει!',
-                'sm_keywords' => 'Social Media Keywords',
-                'questionnaire_response_email_intro_text' => '<p>Χάρη στη συνεισφορά σας είμαστε ένα βήμα πιο κοντά στην κατανόηση του προβλήματος.<br></p>',
-                'questionnaire_response_email_outro_text' => '<p>Σας ευχαριστούμε για το χρόνο και την προσπάθειά σας.<br></p>',
             ],
         ];
 
