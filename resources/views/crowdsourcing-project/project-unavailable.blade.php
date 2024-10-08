9006304835a8b7a2ab027058800bc3582389e308
Kostas Papandreou <papandrk@gmail.com>
2024-10-08 15:28:59 +0300

    IP-107: Implement the Mockup changes in the UI (only what is strictly needed for now)

--- resources/views/crowdsourcing-project/project-unavailable.blade.php
+++ resources/views/crowdsourcing-project/project-unavailable.blade.php
@@ -14,8 +14,7 @@
             <div id="project-motto-container" class="row h-100 w-100 align-items-center mx-0 bg-img"
                  style="background-image: url({{asset($viewModel->project->img_path)}});">
                 <div class="overlay-filter"
-                     style="background-color: {{ $viewModel->project->lp_primary_color }};
-                 top: @if (App::environment('staging')) 128.75px @else 93.75px @endif"></div>
+                     style="top: @if (App::environment('staging')) 128.75px @else 93.75px @endif"></div>
                 <div class="col-lg-7 col-md-8 col-sm-11 mx-auto motto-content px-0">
                     <div class="frosted"></div>
                     <div id="project-motto" class="container-fluid">
