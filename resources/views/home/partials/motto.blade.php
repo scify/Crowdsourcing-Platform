9b6830798744eba2f0e7f94d1bc328a8c8419862
Paul Isaris <paulisaris@gmail.com>
2024-10-09 10:32:37 +0300

    Divided common CSS into smaller files

--- resources/views/home/partials/motto.blade.php
+++ resources/views/home/partials/motto.blade.php
@@ -2,12 +2,23 @@
     <div class="row h-100 w-100 align-items-center mx-0"
          style="background-image: url({{asset('images/active_participation.webp')}});">
         <div class="overlay-filter" style="top: @if (App::environment('staging')) 128.75px @else 93.75px @endif"></div>
-        <div class="col-lg-5 col-md-10 col-sm-11 mx-auto motto-content px-0">
+        <div class="col-lg-6 col-md-10 col-sm-11 mx-auto motto-content px-2 h-75">
             <div class="frosted"></div>
-            <div id="project-motto">
-                <h1>Let's crowdsource!<br>Taking decisions with citizens and not for them</h1>
-                <div class="w-50 mx-auto">
-                    <a href="#projects" class="btn btn-block call-to-action smooth-goto">Share your ideas</a>
+            <div id="project-motto" class="h-100">
+                <div class="container-fluid h-100">
+                    <div class="row h-50">
+                        <div class="col">
+                            <h1>Let's crowdsource!<br>Taking decisions with citizens and not for them</h1>
+                        </div>
+                    </div>
+                    <div class="row h-50">
+                        <div class="col-lg-6 col-md-10 col-sm-12 mx-auto">
+                            <div class="w-100 mx-auto">
+                                <a href="#projects" class="btn btn-block btn-primary call-to-action smooth-goto">Share
+                                    your ideas</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
