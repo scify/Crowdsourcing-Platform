200c7eb7fc5e2baaf6a7865c25ba1525a43d3fb4
Paul Isaris <paulisaris@gmail.com>
2024-11-21 11:08:26 +0200

    Solution relationships

--- app/Models/Solution/Solution.php
+++ app/Models/Solution/Solution.php
@@ -4,6 +4,8 @@ namespace App\Models\Solution;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Solution extends Model {
@@ -11,4 +13,19 @@ class Solution extends Model {
 
     protected $table = 'solutions';
     protected $fillable = ['id', 'problem_id', 'user_creator_id', 'slug', 'status_id', 'img_url'];
+    protected $with = ['defaultTranslation'];
+
+    // problem relationship (a solution belongs to a problem)
+    public function problem(): BelongsTo {
+        return $this->belongsTo('App\Models\Problem\Problem', 'problem_id');
+    }
+
+    // default translation relationship
+    // the solution has the same default translation as the problem it belongs to.
+    // but the records are stored in different tables (solution_translations and problem_translations)
+    // so we need to take the default_language_id from the problem and use it to get the translation
+    public function defaultTranslation(): HasOne {
+        return $this->hasOne('App\Models\Solution\SolutionTranslation', 'solution_id')
+            ->where('language_id', $this->problem->default_language_id);
+    }
 }
