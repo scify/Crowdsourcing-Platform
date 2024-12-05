d8bee6d8379cabba75d0bf04a679cf3c5bdd5f3d
Kostas Papandreou <papandrk@gmail.com>
2024-12-05 10:45:04 +0200

    added relationships to solution models

--- app/Models/Solution/SolutionTranslation.php
+++ app/Models/Solution/SolutionTranslation.php
@@ -3,13 +3,24 @@
 namespace App\Models\Solution;
 
 use App\Models\CompositeKeysModel;
+use App\Models\Language;
+use Awobaz\Compoships\Compoships;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SolutionTranslation extends CompositeKeysModel {
-    use HasFactory;
+    use Compoships, HasFactory;
 
     protected $table = 'solution_translations';
     protected $fillable = ['solution_id', 'language_id', 'title', 'description'];
     protected $primaryKey = ['solution_id', 'language_id'];
     public $incrementing = false;
+
+    public function solution(): BelongsTo {
+        return $this->belongsTo(Solution::class, 'solution_id', 'id');
+    }
+
+    public function language(): BelongsTo {
+        return $this->belongsTo(Language::class, 'language_id', 'id');
+    }
 }
