aa44395cd433bb2f4564a03efaffc5a26486db18
Kostas Papandreou <papandrk@gmail.com>
2024-12-05 18:06:37 +0200

    problems front display page text changes

--- resources/lang/en/project-problems.php
+++ resources/lang/en/project-problems.php
@@ -9,4 +9,7 @@ return [
     'solutions_for' => 'Solutions for',
     'project_landing_page_problems_action_button' => 'See all problems',
     'suggest_solution' => 'Suggest a solution',
+    'temp_str_1' => 'Contribute to solving the problems for topic:',
+    'temp_str_2' => 'Select a problem from the list below and vote for an existing solution, or suggest a new candidate solution. Your contributions will have a valuable impact on official policy across Europe.',
+    'temp_str_3' => 'Select a problem from the list below to begin',
 ];
