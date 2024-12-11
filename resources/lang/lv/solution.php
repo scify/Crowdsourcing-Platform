1d945ada9cf4d202374a402ee3bb9d053552d400
Paul Isaris <paulisaris@gmail.com>
2024-12-11 17:30:02 +0200

    Added translations

---
+++ resources/lang/lv/solution.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'propose_solution' => 'Ierosināt risinājumu',
+    'proposal_submitted_title' => 'Priekšlikums iesniegts',
+    'thanks_for_proposal_title' => 'Paldies par jūsu priekšlikumu! Tas mums daudz nozīmē.',
+    'thanks_for_proposal_message' => 'Jūsu priekšlikums ir veiksmīgi iesniegts! Mūsu komanda to rūpīgi pārskatīs. Mēs sazināsimies ar jums pēc iespējas ātrāk. <br><br><b>Ar jūsu palīdzību</b> mēs varam padarīt pasauli labāku!',
+    'solution_title' => 'Risinājuma nosaukums',
+    'solution_description' => 'Risinājuma apraksts',
+    'help_us_more_message' => 'Palīdziet mums vairāk, iesniedzot vairāk risinājumu!',
+];
