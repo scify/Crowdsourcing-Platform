9bcaee8de768e2465ce4796e760e7b72172361e4
Paul Isaris <paulisaris@gmail.com>
2024-12-12 12:12:01 +0200

    IP-312: The max num of votes is passed in the problem page view model and passed in the vue component as a prop

--- resources/views/problem/partials/solutions-list.blade.php
+++ resources/views/problem/partials/solutions-list.blade.php
@@ -10,6 +10,7 @@
             :problem-id='{{ $viewModel->problem->id }}'
             :problem-slug='@json($viewModel->problem->slug)'
             :project-slug='@json($viewModel->project->slug)'
+            :max-votes-per-user-for-solutions='@json($viewModel->project->max_votes_per_user_for_solutions)'
             :button-text-color-theme='@json($viewModel->project->lp_btn_text_color_theme)'>
     </solutions>
 
