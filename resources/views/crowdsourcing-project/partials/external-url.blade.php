bb1d94f84f6ce80e704c544d6402f9fe63dcc3cc
Paul Isaris <paulisaris@gmail.com>
2024-12-12 12:07:37 +0200

    Changed some texts

--- resources/views/crowdsourcing-project/partials/external-url.blade.php
+++ resources/views/crowdsourcing-project/partials/external-url.blade.php
@@ -1,7 +1,7 @@
 @if($viewModel->projectHasExternalURL())
     <div class="col-md-5 col-sm-12 mx-auto text-center mt-5">
         <a href="{{$viewModel->project->external_url}}" target="_blank" class="btn call-to-action">
-            {{ __("questionnaire.visit_projects_site") }}
+            {{ __("project.visit_project_webpage_link_text") }}
         </a>
     </div>
 @endif
