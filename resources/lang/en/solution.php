cef3324f2be7fe9829aa0c361d99b5720f81a3a2
Kostas Papandreou <papandrk@gmail.com>
2024-12-12 12:27:38 +0200

    IP-79, IP-83: finished propose-solution functionality

--- resources/lang/en/solution.php
+++ resources/lang/en/solution.php
@@ -8,4 +8,8 @@ return [
     'solution_title' => 'Solution Title',
     'solution_description' => 'Solution Description',
     'help_us_more_message' => 'Help us more by submitting more solutions!',
+    'submission_form' => 'Submission Form',
+    'share_your_solution' => 'Share Your Solution',
+    'do_you_have_an_idea' => 'Do you have an idea that can help solve a problem? Submit your solution below and join our community of problem-solvers! Your contribution can make a real difference. Fill in the form with your best solution and help us tackle challenges together.',
+    'about_the_problem' => 'About the Problem',
 ];
---
+++ resources/views/solution/partials/propose-solution-overview.blade.php
@@ -0,0 +1,36 @@
+<div class="container px-sm-0">
+
+    <div class="row">
+        <div class="col-12 my-4 my-lg-5 pt-4">
+            <x-go-back-link href="/{{ app()->getLocale() .'/'. $viewModel->project->slug . '/problems/' . $viewModel->problem->slug }}"
+                            class="d-none d-lg-block">{{ __("project-problems.back") }}</x-go-back-link>
+        </div>
+    </div>
+
+    <div class="row propose-solution-description">
+
+        <div class="col-12">
+            <h1 class="section-title text-center">{{ __('solution.share_your_solution') }}</h1>
+            <div class="section-body pb-5 pb-lg-0">
+                <p>{{ __('solution.do_you_have_an_idea') }}</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row propose-solution-description">
+
+        <div class="col-12 col-lg-7">
+            <h2 class="section-title">{{ __("solution.about_the_problem") }}</h2>
+            <div class="section-body pb-5 pb-lg-0">
+                <p>{!! $viewModel->problem->currentTranslation->description !!}</p>
+            </div>
+        </div>
+
+        <div class="col-12 col-lg-4 offset-lg-1 align-self-end text-center">
+            <img src="/images/problems/problem-page-intro-top-question@2x.png" alt="" width="382" height="238" class="img-fluid">
+        </div>
+
+    </div>
+
+</div>
