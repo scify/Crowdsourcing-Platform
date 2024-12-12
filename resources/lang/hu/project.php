6075ef774a60182cbdd9c14a76460097bece88ac
Paul Isaris <paulisaris@gmail.com>
2024-12-12 11:54:14 +0200

    Refactoring & translations

---
+++ resources/lang/hu/project.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'project_finalized_message' => 'Ez a kampány befejeződött.<br>Köszönjük a hozzájárulását!',
+    'project_unpublished_message' => 'Ez a kampány még nincs közzétéve.',
+    'project_archived_message' => 'Ez a kampány archiválva van.',
+    'project_unavailable_message' => 'Ez a kampány jelenleg nem elérhető.',
+    'visit_project_webpage_link_text' => 'Látogassa meg a kampány weboldalát',
+];
---
+++ resources/lang/it/project.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'project_finalized_message' => 'Questa campagna è terminata.<br>Grazie per il vostro contributo!',
+    'project_unpublished_message' => 'Questa campagna non è ancora pubblicata.',
+    'project_archived_message' => 'Questa campagna è archiviata.',
+    'project_unavailable_message' => 'Questa campagna non è attualmente disponibile.',
+    'visit_project_webpage_link_text' => 'Visita la pagina della campagna',
+];
