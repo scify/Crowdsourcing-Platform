021e5d71f0641d1c4adcd7bfb67cd5ea9ba921cf
Kostas Papandreou <papandrk@gmail.com>
2024-12-17 14:47:13 +0200

    IP-331: Complete the platform translation in Croatian

--- resources/lang/hr/project-problems.php
+++ resources/lang/hr/project-problems.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'back' => 'Back',
-    'the_topic' => 'The topic:',
-    'list_of_problems' => 'List of Problems',
-    'list_of_solutions' => 'List of Solutions',
-    'see_all_problems' => 'See all problems',
-    'solutions_for' => 'Solutions for',
-    'project_landing_page_problems_action_button' => 'See all problems',
-    'suggest_solution' => 'Suggest a solution',
-    'temp_str_1' => 'Contribute to solving the problems for topic:',
-    'temp_str_2' => 'Select a problem from the list below and vote for an existing solution, or suggest a new candidate solution. Your contributions will have a valuable impact on official policy across Europe.',
-    'temp_str_3' => 'Select a problem from the list below to begin',
+    'back' => 'Natrag',
+    'the_topic' => 'Tema:',
+    'list_of_problems' => 'Popis problema',
+    'list_of_solutions' => 'Popis rješenja',
+    'see_all_problems' => 'Pogledajte sve probleme',
+    'solutions_for' => 'Rješenja za',
+    'project_landing_page_problems_action_button' => 'Pogledajte sve probleme',
+    'suggest_solution' => 'Predložite rješenje',
+    'temp_str_1' => 'Doprinosite rješavanju problema za temu:',
+    'temp_str_2' => 'Odaberite problem s popisa u nastavku i glasujte za postojeće rješenje ili predložite novo kandidatsko rješenje. Vaš doprinos imat će značajan utjecaj na službenu politiku širom Europe.',
+    'temp_str_3' => 'Odaberite problem s popisa u nastavku da biste započeli',
 ];
