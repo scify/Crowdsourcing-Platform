789b3f9589e0a8157d80c5a41f4c56bf955b8916
Kostas Papandreou <papandrk@gmail.com>
2025-01-14 18:43:40 +0200

    IP-342: "User registration & profile update page -> the user should also be able to enter their sex, date of birth, and country (not required)"

---
+++ app/BusinessLogicLayer/enums/GenderEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\BusinessLogicLayer\enums;
+
+abstract class GenderEnum {
+    public const MALE = 'Male';
+    public const FEMALE = 'Female';
+    public const OTHER = 'Other';
+}
