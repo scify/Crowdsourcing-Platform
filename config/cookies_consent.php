74b9e850d5307a1f2321e9fb1f9b599e2170184f
Paul Isaris <paulisaris@gmail.com>
2025-01-30 16:11:12 +0200

    cookie name

--- config/cookies_consent.php
+++ config/cookies_consent.php
@@ -31,7 +31,7 @@ return [
     'cookies' => [
         'strictly_necessary' => [
             [
-                'name' => 'cookieConsent',
+                'name' => 'crowdsourcing_app_cookies_consent',
                 'description' => 'This cookie is set by the GDPR Cookie Consent plugin and is used to store whether or not user has consented to the use of cookies. It does not store any personal data.',
                 'duration' => '2 years',
                 'policy_external_link' => null,
