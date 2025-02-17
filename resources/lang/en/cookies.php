976f9a868bdd1cb9248f35567e31b5fdade625d4
Paul Isaris <paulisaris@gmail.com>
2025-02-17 14:21:16 +0200

    upgraded cookies plugin

---
+++ resources/lang/en/cookies.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'google_analytics' => [
+        '_ga_description' => 'This cookie is installed by Google Analytics. The cookie is used to calculate visitor, session, campaign data and keep track of site usage for the site\'s analytics report. The cookies store information anonymously and assign a randomly generated number to identify unique visitors.',
+        '_gat_description' => 'This cookies is installed by Google Universal Analytics to throttle the request rate to limit the collection of data on high traffic sites.',
+    ],
+    'linkedin' => [
+        'bcookie_description' => 'This cookie is set by LinkedIn. The purpose of the cookie is to enable LinkedIn functionalities on the page.',
+        'li_gc_description' => 'This cookie is set by LinkedIn. This cookie is used to store consent of guests regarding the use of cookies for non-essential purposes.',
+        'lidc_description' => 'This cookie is set by LinkedIn and used for routing.',
+    ],
+    'facebook' => [
+        '_fbp_description' => 'This cookie is set by Facebook to deliver advertisement when they are on Facebook or a digital platform powered by Facebook advertising after visiting this website.',
+    ],
+    'twitter' => [
+        'guest_id_description' => 'This cookie is set by Twitter. This cookie is used to identify and track the website visitor.',
+        'guest_id_ads_description' => 'This cookie is set by Twitter. This cookie is used to identify and track the website visitor.',
+        'guest_id_marketing_description' => 'This cookie is set by Twitter. This cookie is used to identify and track the website visitor.',
+        'muc_ads_description' => 'This cookie is set by Twitter. This cookie is used to identify and track the website visitor.',
+        'personalization_id_description' => 'This cookie is set by Twitter. This cookie is used to identify and track the website visitor.',
+    ],
+    'google_ads' => [
+        '_gcl_au_description' => 'This cookie is used by Google Analytics to understand user interaction with the website.',
+    ],
+];
