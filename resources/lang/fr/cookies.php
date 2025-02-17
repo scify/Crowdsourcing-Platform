45f66bd49b1e7293384b1dd96b5d144734e43da1
Paul Isaris <paulisaris@gmail.com>
2025-02-17 14:38:03 +0200

    added more translations for cookies

---
+++ resources/lang/fr/cookies.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'google_analytics' => [
+        '_ga_description' => 'Ce cookie est installé par Google Analytics. Le cookie est utilisé pour calculer les données des visiteurs, des sessions et des campagnes et pour suivre l\'utilisation du site pour le rapport analytique du site. Les cookies stockent les informations de manière anonyme et attribuent un numéro généré aléatoirement pour identifier les visiteurs uniques.',
+        '_gid_description' => 'Ce cookie est installé par Google Analytics. Le cookie est utilisé pour stocker des informations sur la façon dont les visiteurs utilisent un site Web et aide à créer un rapport analytique sur le fonctionnement du site Web. Les données collectées incluent le nombre de visiteurs, la source d\'origine et les pages visitées sous forme anonyme.',
+        '_gat_description' => 'Ce cookie est installé par Google Universal Analytics pour limiter le taux de requêtes et limiter la collecte de données sur les sites à fort trafic.',
+    ],
+    'linkedin' => [
+        'bcookie_description' => 'Ce cookie est défini par LinkedIn. Le but du cookie est d\'activer les fonctionnalités de LinkedIn sur la page.',
+        'li_gc_description' => 'Ce cookie est défini par LinkedIn. Ce cookie est utilisé pour stocker le consentement des invités concernant l\'utilisation de cookies à des fins non essentielles.',
+        'lidc_description' => 'Ce cookie est défini par LinkedIn et utilisé pour le routage.',
+    ],
+    'facebook' => [
+        '_fbp_description' => 'Ce cookie est défini par Facebook pour diffuser des publicités lorsqu\'ils sont sur Facebook ou sur une plateforme numérique alimentée par la publicité Facebook après avoir visité ce site Web.',
+    ],
+    'twitter' => [
+        'guest_id_description' => 'Ce cookie est défini par Twitter. Ce cookie est utilisé pour identifier et suivre le visiteur du site Web.',
+        'guest_id_ads_description' => 'Ce cookie est défini par Twitter. Ce cookie est utilisé pour identifier et suivre le visiteur du site Web.',
+        'guest_id_marketing_description' => 'Ce cookie est défini par Twitter. Ce cookie est utilisé pour identifier et suivre le visiteur du site Web.',
+        'muc_ads_description' => 'Ce cookie est défini par Twitter. Ce cookie est utilisé pour identifier et suivre le visiteur du site Web.',
+        'personalization_id_description' => 'Ce cookie est défini par Twitter. Ce cookie est utilisé pour identifier et suivre le visiteur du site Web.',
+    ],
+    'google_ads' => [
+        '_gcl_au_description' => 'Ce cookie est utilisé par Google Analytics pour comprendre l\'interaction de l\'utilisateur avec le site Web.',
+    ],
+];
